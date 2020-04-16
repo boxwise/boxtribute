63526bbcf2d977b0133568be4a588d67c6c44040
mcgnly <katie@mcgnly.com>
2020-04-16 15:07:32 +0200

    add component to scan a box. Put this in after the routing PR

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,23 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import ScanBox from './ScanBox';
 
 function Home() {
-  const [data, setData] = useState({})
-
-  useEffect(() => {
-    async function fetchData(){
-    const result = await axios(
-      'http://localhost:5000/api/public',
-    );
-    setData(result.data);
-  }
-  fetchData();
-  }, []);
-
   return (
     <div>
-      <h2>Welcome to boxwise, please log in</h2>
-      <p>{data.message}</p>
+      <h2 className ='w-screen flex justify-center p-2 bg-blue-500' >Welcome to boxwise!</h2>
+      <ScanBox />
     </div>
   )
 }
--- src/views/ScanBox.js
+++ src/views/ScanBox.js
@@ -1,31 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
+import QrReader from 'react-qr-reader';
+// import {Link} from "react-router-dom";
 
-function ScanBox({authObject}) {
-  const [data, setData] = useState({})
-
-  useEffect(() => {
-    async function fetchData(){
-      // Auth0 is pretty full-service, so we only have to worry about sending the auth token, and it will take care of the rest
-      // no refresh token cookie needed
-    const result = await axios(
-      'http://localhost:5000/api/private',
-      {
-        headers: {
-          Authorization: `Bearer ${authObject.access_token}`
-        }
-      }
-    );
-    setData(result.data);
-  }
-  fetchData();
-  }, [authObject]);
+function ScanBox() {
+  const [data, setData] = useState('')
 
   return (
     <div>
       <h2>Scan a box now:</h2>
-      <p>{data.message}</p>
+      {data ? 
+      <div>
+        <a href={data}>{data}</a> 
+        <br />
+        <button 
+        onClick={()=>setData('')}
+        className="m-1 bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button"
+        >Scan again</button>
+      </div>
+      :
+      <QrReader
+          delay={300}
+          onError={(err) => console.log(err)}
+          onScan={setData}
+          style={{ width: '100%' }}
+        />}
 
+        {/* <Link 
+        to='/'
+        className="m-1 leading-loose bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button"
+        >
+          Go Home
+        </Link> */}
     </div>
   )
 }
