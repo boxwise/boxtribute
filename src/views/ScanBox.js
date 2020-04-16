e0bc699d99639ce3622aae60cd871414d74e7b7e
mcgnly <katie@mcgnly.com>
2020-04-16 12:10:51 +0200

    make scan box a placeholder because it will go in for real in scan-box branch

--- src/views/ScanBox.js
+++ src/views/ScanBox.js
@@ -1,40 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import QrReader from 'react-qr-reader';
+import React from 'react';
 
 function ScanBox({authObject}) {
-  const [data, setData] = useState({})
-
-  // useEffect(() => {
-  //   async function fetchData(){
-  //     // Auth0 is pretty full-service, so we only have to worry about sending the auth token, and it will take care of the rest
-  //     // no refresh token cookie needed
-  //   const result = await axios(
-  //     'http://localhost:5000/api/private',
-  //     {
-  //       headers: {
-  //         Authorization: `Bearer ${authObject.access_token}`
-  //       }
-  //     }
-  //   );
-  //   setData(result.data);
-  // }
-  // fetchData();
-  // }, [authObject]);
-
-
-
   return (
     <div>
       <h2>Scan a box now:</h2>
-      <QrReader
-          delay={300}
-          onError={(err) => console.log(err)}
-          onScan={setData}
-          style={{ width: '100%' }}
-        />
-        <p>{data.result}</p>
-
+      <p>I'm a placeholder</p>
     </div>
   )
 }
