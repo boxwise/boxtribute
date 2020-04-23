8801356e830f41054c82cb96acf8f1d9a2b03733
mcgnly <katie@mcgnly.com>
2020-04-23 19:51:32 +0200

    add fallback message for error case

--- src/views/ScanBox.js
+++ src/views/ScanBox.js
@@ -4,28 +4,45 @@ import QrReader from 'react-qr-reader';
 
 function ScanBox() {
   const [data, setData] = useState('')
+  const [qrError, setQrError] = useState('')
+
+  const displayReader = () => {
+    if (data) {
+      return (
+        <div>
+          <a href={data}>{data}</a>
+          <br />
+          <button
+            onClick={() => setData('')}
+            className="m-1 bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button"
+          >Scan again</button>
+        </div>
+      )
+    }
+    if (qrError) {
+      return (
+        <div>
+          <h2>Oh no!</h2>
+        <p>There seems to be a problem! Your device or browser may not be compatible with scanning a QR code here. Please open your camera or other QR-reader and use that instead. If you are on iOS, you can also try using Safari. </p>
+        </div>
+      )
+    }
+    return (
+      <QrReader
+        delay={300}
+        onError={(err) => setQrError(err)}
+        onScan={setData}
+        style={{ width: '100%' }}
+      />
+    );
+  }
 
   return (
     <div>
       <h2>Scan a box now:</h2>
-      {data ? 
-      <div>
-        <a href={data}>{data}</a> 
-        <br />
-        <button 
-        onClick={()=>setData('')}
-        className="m-1 bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button"
-        >Scan again</button>
-      </div>
-      :
-      <QrReader
-          delay={300}
-          onError={(err) => console.log(err)}
-          onScan={setData}
-          style={{ width: '100%' }}
-        />}
+      {displayReader()}
 
-        {/* <Link 
+      {/* <Link 
         to='/'
         className="m-1 leading-loose bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button"
         >
