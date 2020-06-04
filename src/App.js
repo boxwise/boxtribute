a4ac47261b91c776d2f7c8319b7ad189ae4d07e1
Katie McGinley <katie@mcgnly.com>
2020-06-04 17:56:36 +0200

    make query to protected gql route with apollo, one time all the way through (woooohoooo!)

--- src/App.js
+++ src/App.js
@@ -1,108 +1,112 @@
-import React, { useEffect, useState } from "react"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import { ApolloProvider } from "@apollo/react-hooks"
-import ApolloClient from "apollo-boost"
-import PrivateRoute from "./PrivateRoute"
-import Auth0 from "./Auth0"
-import Home from "./views/Home"
-import OrgTopLevel from "./views/Organization"
-import PdfGenerator from "./views/Labels/PdfGenerator"
-import Labels from "./views/Labels/Labels"
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
+import PrivateRoute from "./PrivateRoute";
+import Auth0 from "./Auth0";
+import Home from "./views/Home";
+import OrgTopLevel from "./views/Organization";
+import PdfGenerator from "./views/Labels/PdfGenerator";
+import Labels from "./views/Labels/Labels";
 
-const { REACT_APP_GRAPHQL_SERVER } = process.env
-
-const client = new ApolloClient({
-  uri: REACT_APP_GRAPHQL_SERVER,
-})
+const { REACT_APP_GRAPHQL_SERVER } = process.env;
 
 export default function App() {
-  const AuthContext = React.createContext({})
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [authObject, setAuthObject] = useState({});
 
-  const [loggedIn, setLoggedIn] = useState(false)
-  const [authObject, setAuthObject] = useState({})
+  const client = new ApolloClient({
+    uri: REACT_APP_GRAPHQL_SERVER,
+    request: (operation) => {
+      operation.setContext({
+        headers: {
+          Authorization: `Bearer ${authObject.accessToken}`,
+          "X-Boxwise": authObject.idTokenPayload.email,
+          "X-Clacks-Overhead": "GNU Terry Pratchett",
+        },
+      });
+    },
+  });
 
   function handleLogIn() {
     // if the login was successful, there will be a hash in the url, so you can do all the parsing work in the Auth0 file
     Auth0.handleAuthentication()
       .then((authTokens) => {
-        setAuthObject(authTokens)
-        !!authTokens ? setLoggedIn(true) : setLoggedIn(false)
+        setAuthObject(authTokens);
+        !!authTokens ? setLoggedIn(true) : setLoggedIn(false);
       })
       .catch((err) => {
         // TODO: better logging and error handling
-        console.log(err)
-      })
+        console.log(err);
+      });
   }
 
   useEffect(() => {
     // on page load, see if I'm in a freshly-logged-in state back from the redirect
-    handleLogIn()
-  }, [])
+    handleLogIn();
+  }, []);
 
   function handleLogOut() {
-    window.location.hash = ""
-    setLoggedIn(false)
-    setAuthObject({})
+    window.location.hash = "";
+    setLoggedIn(false);
+    setAuthObject({});
+    client.resetStore();
   }
 
   return (
-    <AuthContext.Provider value={authObject}>
-      <ApolloProvider client={client}>
-        <Router>
-          <div>
-            {/* "Nav-bar" */}
-
-            {/* NOTE! 
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          {/* NOTE! 
         This works like a normal switch, so you have to put the specific routes the highest,
         and work your way down to least-specific */}
-            <Switch>
-              <PrivateRoute
-                path="/org"
-                pathNameRedirect="/"
-                isLoggedIn={loggedIn}
-              >
-                <OrgTopLevel authObject={authObject} />
-              </PrivateRoute>
-
-              <PrivateRoute
-                path="/generateLabel"
-                pathNameRedirect="/"
-                isLoggedIn={loggedIn}
-              >
-                <PdfGenerator authObject={authObject} />
-              </PrivateRoute>
-
-              <PrivateRoute
-                path="/pdf"
-                pathNameRedirect="/"
-                isLoggedIn={loggedIn}
-              >
-                <Labels authObject={authObject} />
-              </PrivateRoute>
-
-              <Route path="/">
-                <Home />
-              </Route>
-            </Switch>
-          </div>
-          {loggedIn ? (
-            // eslint-disable-next-line react/button-has-type
-            <button onClick={() => handleLogOut()} className="log-in">
-              Log Out
-            </button>
-          ) : (
-            <button
-              onClick={() => {
-                Auth0.login()
-              }}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
+          <Switch>
+            <PrivateRoute
+              path="/org"
+              pathNameRedirect="/"
+              isLoggedIn={loggedIn}
             >
-              Sign In
-            </button>
-          )}
-        </Router>
-      </ApolloProvider>
-    </AuthContext.Provider>
-  )
+              <OrgTopLevel />
+            </PrivateRoute>
+
+            <PrivateRoute
+              path="/generateLabel"
+              pathNameRedirect="/"
+              isLoggedIn={loggedIn}
+            >
+              <PdfGenerator authObject={authObject} />
+            </PrivateRoute>
+
+            <PrivateRoute
+              path="/pdf"
+              pathNameRedirect="/"
+              isLoggedIn={loggedIn}
+            >
+              <Labels authObject={authObject} />
+            </PrivateRoute>
+
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+        {loggedIn ? (
+          // eslint-disable-next-line react/button-has-type
+          <button onClick={() => handleLogOut()} className="log-in">
+            Log Out
+          </button>
+        ) : (
+          <button
+            onClick={() => {
+              Auth0.login();
+            }}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            Sign In
+          </button>
+        )}
+      </Router>
+    </ApolloProvider>
+  );
 }
--- src/views/Organization/SingleOrg.js
+++ src/views/Organization/SingleOrg.js
@@ -1,32 +1,37 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import { useParams, Link } from "react-router-dom"
-import axios from "axios"
+import { useQuery } from "@apollo/react-hooks"
+import { gql } from "apollo-boost"
 
-export default function SingleOrg({ authObject }) {
-  const [data, setData] = useState({})
-  useEffect(() => {
-    async function fetchData() {
-      // Send the access token to gain access to the API at all, send the id token to gain access to your specific org
-      // TODO I think if we added roles and org to the user profile in auth0, we could skip that step and restrict routes on the frontend
-      const result = await axios("http://localhost:5000/api/private", {
-        headers: {
-          Authorization: `Bearer ${authObject.accessToken}`,
-          "X-Clacks-Overhead": "GNU Terry Pratchett",
-          "X-Boxwise": authObject.idTokenPayload.email
-        },
-      })
-      setData(result.data)
+const CAMPS = gql`
+  {
+    allCamps {
+      id
+      organisation_id
+      name
     }
-    fetchData()
-  }, [authObject])
+  }
+`
 
+export default function SingleOrg() {
   const { orgId } = useParams()
+  const { loading, error, data } = useQuery(CAMPS)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
+
   return (
     <div>
       <h3>Requested organization: {orgId}</h3>
       <h3>Bases in this org:</h3>
       <ul>
-        <p>{data.message}</p>
+        {data.allCamps.map(({ id, organisation_id, name }) => (
+          <div key={id}>
+            <li>
+              {organisation_id}: {name}
+            </li>
+          </div>
+        ))}
       </ul>
       <Link to="/">Go Home</Link>
     </div>
