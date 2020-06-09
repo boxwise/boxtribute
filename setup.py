73fdd2c9472c05177a399260147df9fb57bcf80f
pylipp <philipp.metzner@tum.de>
2020-06-09 20:43:01 +0200

    Set up Python code acc. to conventional structure

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as f:
+    REQUIREMENTS = f.readlines()
+
+setup(
+    name="boxwise-flask",
+    use_scm_version=True,
+    description="""Boxwise is a web application that makes it easy for
+    organisations to source, store and distribute donated goods to people in
+    need in a fair and dignified way.""",
+    url="https://github.com/boxwise/boxwise-flask",
+    author="boxwise.co",
+    author_email="hello@boxwise.co",
+    license="Apache 2.0",
+    packages=find_packages(exclude=["test"]),
+    setup_requires=["setuptools_scm"],
+    install_requires=REQUIREMENTS,
+)
