e66511ee9dc8d3632cbec153257913e85f56ff59
haguesto <hans@boxwise.co>
2020-06-24 16:48:52 +0200

    use url to connect to db

--- boxwise_flask/main.py
+++ boxwise_flask/main.py
@@ -4,15 +4,23 @@ import os
 from .app import db
 from .routes import app
 
-app.config["DATABASE"] = {
-    "name": os.getenv("MYSQL_DB"),
-    "engine": "peewee.MySQLDatabase",
-    "user": os.getenv("MYSQL_USER"),
-    "host": os.getenv("MYSQL_HOST"),
-    "password": os.getenv("MYSQL_PASSWORD"),
-    # int, otherwise: TypeError: %d format: a number is required, not str from
-    # pymysql.connections
-    "port": int(os.getenv("MYSQL_PORT", 0)),
-}
+# load data for database connection
+db_user = os.getenv("MYSQL_USER")
+db_password = os.getenv("MYSQL_PASSWORD")
+db_name = os.getenv("MYSQL_DB")
+db_host = os.getenv("MYSQL_HOST")
+# int, otherwise: TypeError: %d format: a number is required, not str from
+# pymysql.connections
+db_port = int(os.getenv("MYSQL_PORT", 0))
+cloud_sql_connection_name = os.getenv("CLOUD_SQL_CONNECTION_NAME", False)
+
+if cloud_sql_connection_name:
+    app.config["DATABASE"] = "mysql://{}:{}@/{}?unix_socket=/cloudsql/{}".format(
+        db_user, db_password, db_name, cloud_sql_connection_name
+    )
+else:
+    app.config["DATABASE"] = "mysql://{}:{}@{}:{}/{}".format(
+        db_user, db_password, db_host, db_port, db_name
+    )
 
 db.init_app(app)
