f998452673a66addbc78c965f5631f28c876da70
mc18g13 <matthewcaswell@hotmail.co.uk>
2020-08-15 18:49:26 +0100

    amend mock decorator function

--- test/endpoint_tests/__init__.py
+++ test/endpoint_tests/__init__.py
@@ -2,17 +2,14 @@ from functools import wraps
 from unittest.mock import patch
 
 
-def mock_decorator(*args, **kwargs):
+def mock_decorator(f):
     """Fake decorator for mocking other decorators."""
 
-    def decorator(f):
-        @wraps(f)
-        def decorated_function(*args, **kwargs):
-            return f(*args, **kwargs)
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        return f(*args, **kwargs)
 
-        return decorated_function
-
-    return decorator
+    return decorated_function
 
 
 def mock_auth_test(test_for, **kwargs):
@@ -20,5 +17,5 @@ def mock_auth_test(test_for, **kwargs):
     return True
 
 
-patch("boxwise_flask.auth_helper.requires_auth", mock_decorator()).start()
+patch("boxwise_flask.auth_helper.requires_auth", mock_decorator).start()
 patch("boxwise_flask.auth_helper.authorization_test", mock_auth_test).start()
