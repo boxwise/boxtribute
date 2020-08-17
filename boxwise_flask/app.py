37c7d212a2ac4805b9489e449a3a9b52a83d3ac6
haguesto <hans@boxwise.co>
2020-08-17 17:09:34 +0200

    Seperate blueprint into frontend and api

--- boxwise_flask/app.py
+++ boxwise_flask/app.py
@@ -1,21 +1,16 @@
 """Configuration and instantiation of flask app and peewee-managed database"""
-import os
-
 from flask import Flask
 from flask_cors import CORS
 
-from .routes import main_page_blueprint
+from .routes import api_bp, frontend_bp
 
 
 def create_app():
     # Not able to change the static folder variables after app is initialized
-    app = Flask(
-        __name__,
-        static_folder=os.getenv("FLASK_STATIC_FOLDER", "static"),
-        static_url_path=os.getenv("FLASK_STATIC_URL_PATH", "/static"),
-    )
+    app = Flask(__name__)
 
     CORS(app)
 
-    app.register_blueprint(main_page_blueprint)
+    app.register_blueprint(frontend_bp)
+    app.register_blueprint(api_bp)
     return app
