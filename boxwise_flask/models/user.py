6b2d7ebadace43e509dfba3d02116e3ff08f4891
mc18g13 <matthewcaswell@hotmail.co.uk>
2020-09-25 10:48:37 +0100

    update camps in comment to bases

--- boxwise_flask/models/user.py
+++ boxwise_flask/models/user.py
@@ -28,9 +28,9 @@ class User(db.Model):
     def get_user(email):
         user = User.select().where(User.email == email).get()
         base_ids = UsergroupBaseAccess.get_all_base_id_for(user.usergroup_id)
-        # camps is a peewee ModelSelect (so, many objects).
+        # base_ids is a peewee ModelSelect (so, many objects).
         # convert to dict 1 at a time,
-        # and pull the camp_id from that dict, and put in a list
+        # and pull the base_id from that dict, and put in a list
         user.base_id = [model_to_dict(item)["base_id"] for item in base_ids]
 
         return user
