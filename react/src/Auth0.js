4d37d4a3635d7140f1f024d2471b29774581ca58
Luis Ferreira <luis.guilherme.ferreira@hotmail.com>
2020-11-06 18:06:20 +0000

    Current state

--- react/src/Auth0.js
+++ react/src/Auth0.js
@@ -1,4 +1,4 @@
-import * as Auth0 from "auth0-js";
+import createAuth0Client from '@auth0/auth0-spa-js';
 
 // get your own by making an account at Auth0 and putting these in the .env file
 // unless you change the CRA properties, both the REACT_APP_REDIRECT and REACT_APP_LOGOUT_URL=http://localhost:3000
@@ -11,38 +11,30 @@ const {
 } = process.env;
 
 class Auth {
-  auth0 = new Auth0.WebAuth({
-    domain: REACT_APP_AUTH0_DOMAIN,
-    clientID: REACT_APP_AUTH0_CLIENT_ID,
-  });
+ 
+  async getAuth0 () {
+     
+    return await createAuth0Client({
+        domain: REACT_APP_AUTH0_DOMAIN,
+        clientID: REACT_APP_AUTH0_CLIENT_ID,
+        redirectUri: REACT_APP_REDIRECT,
+  })};
 
-  login() {
-    this.auth0.authorize({
+  auth0 = this.getAuth0();
+
+
+
+  async login() { 
+     return await this.auth0.loginWithRedirect({
       audience: REACT_APP_AUTH0_AUDIENCE,
       scope: "openid profile email",
       responseType: "id_token token",
-      redirectUri: REACT_APP_REDIRECT,
     });
   }
 
-  handleAuthentication() {
-    return new Promise((resolve, reject) => {
-      // Auth0 returns the token in the params of the URL after successful login
-      // and redirect. Parse it to get the tokens.
-      this.auth0.parseHash((err, authResult) => {
-        if (err) reject(err);
-        if (!authResult || !authResult.idToken) {
-          reject(err);
-        }
-
-        // authResult contains both the accessToken
-        // (I am a recognized user who has logged in) and the idToken
-        // (here is some info about me as a specific user)
-        // because the only useful info we have about the user is the email,
-        // we need to send this to the backend and check there for access to org-specific info
-        resolve(authResult);
-      });
-    });
+  async handleAuthentication() {
+    return await this.auth0.handleRedirectCallback();
+    
   }
 
   logout() {
