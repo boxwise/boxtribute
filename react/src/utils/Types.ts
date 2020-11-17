dd7a216f7ed353ca223d21c83f0e9acfb9d282ff
HaGuesto <hans@boxwise.co>
2020-11-17 18:23:38 +0100

    Clean up old Auth0 implementation

--- react/src/utils/Types.ts
+++ react/src/utils/Types.ts
@@ -1,30 +1,3 @@
-export interface AuthObjectType {
-  accessToken: "";
-  idToken: "";
-  idTokenPayload: {
-    at_hash: "";
-    aud: "";
-    email: "";
-    email_verified: false;
-    exp: null;
-    iat: null;
-    iss: "";
-    name: "";
-    nickname: "";
-    nonce: "";
-    picture: "";
-    sub: "";
-    updated_at: "";
-  };
-  appState: null;
-  refreshToken: null;
-  state: "";
-  expiresIn: null;
-  //   this won't change
-  tokenType: "Bearer";
-  scope: "";
-}
-
 export interface UserObjectType {
   user: {
     base_id: [number];
