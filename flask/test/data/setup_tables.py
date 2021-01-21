a6fa50a3e2092631de74cebb7315c1c18a280d0b
Matt Rose <matthewrose1799@gmail.com>
2021-01-21 17:49:28 -0500

    added backend tests for getting box from QR code

--- flask/test/data/setup_tables.py
+++ flask/test/data/setup_tables.py
@@ -7,6 +7,7 @@ from data.product import create_default_product
 from data.product_category import create_default_product_category
 from data.product_gender import create_default_product_gender
 from data.qr_code import create_default_qr_code
+from data.qr_code import create_qr_code_without_box
 from data.size_range import create_default_size_range
 from data.user import create_default_users
 from data.usergroup import create_default_usergroup
@@ -21,6 +22,7 @@ def setup_tables():
     create_default_location()
     create_default_organisation()
     create_default_qr_code()
+    create_qr_code_without_box()
     create_default_users()
     create_default_usergroup()
     create_default_usergroup_access_level()
