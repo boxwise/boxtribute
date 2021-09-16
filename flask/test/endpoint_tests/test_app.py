f0e6c33b71361e5110c8464373d843472ae96d00
Philipp Metzner <beth.aleph@yahoo.de>
2021-09-16 20:29:37 +0200

    Refine endpoint tests
    - add fixture to obtain test client for app connected to MySQL database
    - give meaningful names to queries
    - combine get_boxes_by_* tests to reduce overhead for setting up app
    - add test for invalid case of sending two parameters with getBoxDetails

--- flask/test/endpoint_tests/test_app.py
+++ flask/test/endpoint_tests/test_app.py
@@ -1,54 +1,25 @@
 import os
 
 import pytest
-from boxwise_flask.app import create_app
-from boxwise_flask.db import db
 
 
 @pytest.mark.skipif("CIRCLECI" not in os.environ, reason="only functional in CircleCI")
-def test_backend_connection():
-    """Verify that database connection is established and operational.
-
-    Follow the setup-proceduce of the main module, instantiate a test client,
-    and query a box via the GraphQL endpoint.
-    """
-    app = create_app()
-    app.testing = True
-
-    # cf. main.py but inserting values from docker-compose.yml
-    app.config["DATABASE"] = "mysql://root:dropapp_root@127.0.0.1:3306/dropapp_dev"
-
-    db.init_app(app)
-
-    client = app.test_client()
-
+def test_get_box_details(mysql_app_client):
     data = {
-        "query": """query Box {
+        "query": """query BoxIdAndItems {
                 getBoxDetails(qrCode: "ffdd7f7243d74a663b417562df0ebeb") {
                     ID
                     items
                 }
             }"""
     }
-    response = client.post("/graphql", json=data)
+    response = mysql_app_client.post("/graphql", json=data)
     queried_box = response.json["data"]["getBoxDetails"]
     assert response.status_code == 200
     assert queried_box == {"ID": "436898", "items": 87}
 
-    db.close_db(None)
-
-
-@pytest.mark.skipif("CIRCLECI" not in os.environ, reason="only functional in CircleCI")
-def test_get_box_details():
-    app = create_app()
-    app.testing = True
-    app.config["DATABASE"] = "mysql://root:dropapp_root@127.0.0.1:3306/dropapp_dev"
-
-    db.init_app(app)
-    client = app.test_client()
-
     data = {
-        "query": """query Box {
+        "query": """query SomeBoxDetails {
                 getBoxDetails(boxId: 996559) {
                     qrCode {
                         ID
@@ -61,7 +32,7 @@ def test_get_box_details():
                 }
             }"""
     }
-    response = client.post("/graphql", json=data)
+    response = mysql_app_client.post("/graphql", json=data)
     queried_box = response.json["data"]["getBoxDetails"]
     assert response.status_code == 200
     assert queried_box == {
@@ -74,42 +45,35 @@ def test_get_box_details():
         },
         "size": "53 S",
     }
-    db.close_db(None)
+
+    data = {
+        "query": """query BoxLookupWithTwoParameters {
+                getBoxDetails(boxId: 996559, qrCode: "deadbeef") {
+                    ID
+                }
+            }"""
+    }
+    response = mysql_app_client.post("/graphql", json=data)
+    queried_box = response.json["data"]["getBoxDetails"]
+    assert response.status_code == 200
+    assert queried_box is None
 
 
 @pytest.mark.skipif("CIRCLECI" not in os.environ, reason="only functional in CircleCI")
-def test_get_boxes_by_location():
-    app = create_app()
-    app.testing = True
-    app.config["DATABASE"] = "mysql://root:dropapp_root@127.0.0.1:3306/dropapp_dev"
-
-    db.init_app(app)
-    client = app.test_client()
-
+def test_get_boxes(mysql_app_client):
     data = {
-        "query": """query Box {
+        "query": """query CommentsOfLostBoxes {
                 getBoxesByLocation(locationId: 14) {
                     comment
                 }
             }"""
     }
-    response = client.post("/graphql", json=data)
+    response = mysql_app_client.post("/graphql", json=data)
     queried_boxes = response.json["data"]["getBoxesByLocation"]
     assert response.status_code == 200
     assert len(queried_boxes) == 78
     # There are no comments currently. Verify by creating a set
     assert {box["comment"] for box in queried_boxes} == {""}
-    db.close_db(None)
-
-
-@pytest.mark.skipif("CIRCLECI" not in os.environ, reason="only functional in CircleCI")
-def test_get_boxes_by_gender():
-    app = create_app()
-    app.testing = True
-    app.config["DATABASE"] = "mysql://root:dropapp_root@127.0.0.1:3306/dropapp_dev"
-
-    db.init_app(app)
-    client = app.test_client()
 
     data = {
         "query": """query BoxesWithUnisexAdultProducts {
@@ -118,11 +82,10 @@ def test_get_boxes_by_gender():
                 }
             }"""
     }
-    response = client.post("/graphql", json=data)
+    response = mysql_app_client.post("/graphql", json=data)
     queried_boxes = response.json["data"]["getBoxesByGender"]
     assert response.status_code == 200
     assert len(queried_boxes) == 47
     # IDs are six-digit numbers
     for box in queried_boxes:
         assert 99999 < int(box["ID"]) < 1000000
-    db.close_db(None)
