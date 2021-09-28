a3603a8c6007c5f52076fb67885633fbb4337731
Philipp Metzner <beth.aleph@yahoo.de>
2021-09-28 12:42:36 +0200

    Run prettier

--- react/src/views/BoxInfo.tsx
+++ react/src/views/BoxInfo.tsx
@@ -33,20 +33,26 @@ function BoxInfo(props) {
     });
   }, [id, getBoxQuery]);
 
-  return <>{boxData === null ? <p>Fetching box now...</p> :
-    <div>
-      <h2>Box Found!</h2>
-      <p>Box ID: {boxData.box_id}</p>
-      <p>Location Name: {boxData.location_label}</p>
-      <p>Product Name: {boxData.product_name}</p>
-      <p>Product Size: XXXXXX</p>
-      <p># of Items: {boxData.no_of_items}</p>
-      <p>Product Gender: {boxData.product_gender} </p>
-      <p>Comments: {boxData.product_gender} </p>
-      <p>Box Status: XXXXXX</p>
-      <p>QR Code: {boxData.qr_code}</p>
-    </div>
-  }</>;
+  return (
+    <>
+      {boxData === null ? (
+        <p>Fetching box now...</p>
+      ) : (
+        <div>
+          <h2>Box Found!</h2>
+          <p>Box ID: {boxData.box_id}</p>
+          <p>Location Name: {boxData.location_label}</p>
+          <p>Product Name: {boxData.product_name}</p>
+          <p>Product Size: XXXXXX</p>
+          <p># of Items: {boxData.no_of_items}</p>
+          <p>Product Gender: {boxData.product_gender} </p>
+          <p>Comments: {boxData.product_gender} </p>
+          <p>Box Status: XXXXXX</p>
+          <p>QR Code: {boxData.qr_code}</p>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default BoxInfo;
