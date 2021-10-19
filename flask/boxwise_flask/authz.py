fd733d5807f46da77464774d942a9e1b4095dffa
Philipp Metzner <beth.aleph@yahoo.de>
2021-10-19 09:12:17 +0200

    Enable unit testing of authorize function
    - optionally pass current user data as argument
    - remove user_can_access_base function since superfluous
    - add unit tests for verifying access to base/organisation/user
      resources

--- flask/boxwise_flask/authz.py
+++ flask/boxwise_flask/authz.py
@@ -4,34 +4,44 @@ from flask import g
 from .exceptions import Forbidden, UnknownResource
 
 
-def authorize(*, user_id=None, organisation_id=None, base_id=None, permission=None):
-    """Check whether the current user is authorized to access the specified
-    resource.
+def authorize(
+    current_user=None,
+    *,
+    user_id=None,
+    organisation_id=None,
+    base_id=None,
+    permission=None,
+):
+    """Check whether the current user (default: `g.user`) is authorized to access the
+    specified resource.
     This function is supposed to be used in resolver functions. It may raise an
     UnknownResource or Forbidden exception which ariadne handles by extending the
     'errors' field of the response.
     There are no HTTP 4xx status codes associated with the error since a GraphQL
     response is returned as 200 acc. to specification.
     """
+    if current_user is None:
+        current_user = g.user
+
     if permission == "qr:create":
         # For the front-end, base-specific distinction when creating QR codes is
         # relevant but not for the back-end (there is no data relationship between
         # QR code and base). The permission is of form 'base_x:qr:create'.
-        authorized = any("qr:create" in p for p in g.user["permissions"])
+        authorized = any("qr:create" in p for p in current_user["permissions"])
 
     elif permission is not None:
-        authorized = permission in g.user["permissions"]
+        authorized = permission in current_user["permissions"]
 
         if not authorized and base_id is not None:
             # Handle base-specific permission of form 'base_x:...'
             permission = f"base_{base_id}:{permission}"
-            authorized = permission in g.user["permissions"]
+            authorized = permission in current_user["permissions"]
     elif base_id is not None:
-        authorized = user_can_access_base(g.user, base_id)
+        authorized = base_id in current_user["base_ids"]
     elif organisation_id is not None:
-        authorized = organisation_id == g.user["organisation_id"]
+        authorized = organisation_id == current_user["organisation_id"]
     elif user_id is not None:
-        authorized = user_id == g.user["id"]
+        authorized = user_id == current_user["id"]
     else:
         raise UnknownResource()
 
@@ -44,8 +54,4 @@ def authorize(*, user_id=None, organisation_id=None, base_id=None, permission=No
         ):
             if value is not None:
                 break
-        raise Forbidden(resource, value, g.user)
-
-
-def user_can_access_base(requesting_user, base_id):
-    return base_id in requesting_user.get("base_ids", [])
+        raise Forbidden(resource, value, current_user)
--- flask/test/auth_tests/test_authz.py
+++ flask/test/auth_tests/test_authz.py
@@ -1,23 +1,37 @@
 import pytest
-from boxwise_flask.authz import authorize, user_can_access_base
-from boxwise_flask.exceptions import UnknownResource
+from boxwise_flask.authz import authorize
+from boxwise_flask.exceptions import Forbidden, UnknownResource
 
 
-def test_user_can_access_base_valid_user():
-    user = {"base_ids": [1]}
-    assert user_can_access_base(user, 1)
+def test_authorized_user():
+    user = {"base_ids": [1], "organisation_id": 2, "id": 3}
+    assert authorize(user, base_id=1)
+    assert authorize(user, organisation_id=2)
+    assert authorize(user, user_id=3)
 
 
-def test_user_can_access_base_no_base_ids():
-    user = {}
-    assert not user_can_access_base(user, 1)
+def test_user_unauthorized_for_base():
+    user = {"base_ids": []}
+    with pytest.raises(Forbidden):
+        authorize(user, base_id=1)
 
-
-def test_user_can_access_base_invalid_base_ids():
     user = {"base_ids": [2]}
-    assert not user_can_access_base(user, 1)
+    with pytest.raises(Forbidden):
+        authorize(user, base_id=1)
+
+
+def test_user_unauthorized_for_organisation():
+    user = {"organisation_id": 1}
+    with pytest.raises(Forbidden):
+        authorize(user, organisation_id=2)
+
+
+def test_user_unauthorized_for_user():
+    user = {"id": 1}
+    with pytest.raises(Forbidden):
+        authorize(user, user_id=2)
 
 
 def test_invalid_authorization_resource():
     with pytest.raises(UnknownResource):
-        authorize()
+        authorize(current_user={})
