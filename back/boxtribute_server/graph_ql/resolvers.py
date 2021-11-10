aea9f50f31499d2ae3f265294d659f162b6ee9fc
Philipp Metzner <beth.aleph@yahoo.de>
2021-11-10 15:48:04 +0100

    Extend GraphQL enums
    - align with values from ProductGender/BoxState model
    - gender "11" (UnisexKid) merged with UnisexChild in production (DB dump
      to be updated), see also boxwise/dropapp#431
    - gender "10" without name, used to indicate "None"

--- back/boxtribute_server/graph_ql/resolvers.py
+++ back/boxtribute_server/graph_ql/resolvers.py
@@ -334,8 +334,14 @@ def resolve_organisation_bases(organisation_obj, info):
 
 
 @product.field("gender")
-def resolve_product_gender(obj, info):
-    return obj.id
+def resolve_product_gender(product_obj, info):
+    # Instead of a ProductGender instance return an integer for EnumType conversion
+    gender_id = product_obj.gender.id
+    if gender_id == 11:  # pragma: no cover
+        # In dropapp, UnisexChild (6) and UnisexKid (11) are merged.
+        # This shall be removed when a recent database dump is added.
+        gender_id = 6
+    return gender_id
 
 
 @product.field("sizes")
