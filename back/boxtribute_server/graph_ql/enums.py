913189e12b1d8bf35f8f889683aadb2feec2667c
Philipp Metzner <beth.aleph@yahoo.de>
2021-12-08 10:09:22 +0100

    Group GraphQL enum definitions

--- back/boxtribute_server/graph_ql/enums.py
+++ back/boxtribute_server/graph_ql/enums.py
@@ -2,13 +2,9 @@ from ariadne import EnumType
 
 from ..models.enums import BoxState, HumanGender, Language, ProductGender
 
-# Translate GraphQL enum into id field of database table
-product_gender_enum = EnumType("ProductGender", ProductGender)
-
-
-box_state_enum = EnumType("BoxState", BoxState)
-
-
-gender_enum = EnumType("HumanGender", HumanGender)
-
-language_enum = EnumType("Language", Language)
+enum_types = [
+    EnumType("ProductGender", ProductGender),
+    EnumType("BoxState", BoxState),
+    EnumType("HumanGender", HumanGender),
+    EnumType("Language", Language),
+]
