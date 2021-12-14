b4c11b20e3e5ffd8882e5148e8e6679f126b2d17
Philipp Metzner <beth.aleph@yahoo.de>
2021-12-14 19:35:01 +0100

    Update User endpoint test
    - delete unused function
    - update variable names
    - verify more fields
    - combine tests into single function

--- back/test/endpoint_tests/test_user.py
+++ back/test/endpoint_tests/test_user.py
@@ -1,17 +1,8 @@
-import pytest
-
-
-def get_user_from_list_of_dicts(id, list_of_dicts):
-    return [x for x in list_of_dicts if x["id"] == id][0]
-
-
-@pytest.mark.usefixtures("default_users")
-@pytest.mark.usefixtures("default_organisation")
-def test_user_query_from_email(client, default_users, default_organisation):
+def test_user(client, default_users, default_organisation):
     test_id = 8
     expected_user = default_users[test_id]
 
-    graph_ql_query_string = f"""query User {{
+    query = f"""query User {{
                 user(id: {test_id}) {{
                     id
                     name
@@ -20,7 +11,6 @@ def test_user_query_from_email(client, default_users, default_organisation):
                     validLastDay
                     bases {{
                         id
-                        name
                     }}
                     organisation {{
                         id
@@ -30,28 +20,26 @@ def test_user_query_from_email(client, default_users, default_organisation):
                 }}
             }}"""
 
-    data = {"query": graph_ql_query_string}
+    data = {"query": query}
     response_data = client.post("/graphql", json=data)
     queried_user = response_data.json["data"]["user"]
     assert response_data.status_code == 200
     assert int(queried_user["id"]) == test_id
+    assert queried_user["name"] == expected_user["name"]
+    assert queried_user["email"] == expected_user["email"]
     assert queried_user["validFirstDay"] == expected_user["valid_first_day"].isoformat()
     assert queried_user["lastLogin"][:-6] == expected_user["last_login"].isoformat()
     assert [int(b["id"]) for b in queried_user["bases"]] == [1]
-    assert queried_user["bases"][0]["name"] == "the best name"
     assert int(queried_user["organisation"]["id"]) == default_organisation["id"]
 
-
-@pytest.mark.usefixtures("default_users")
-def test_users(client, default_users):
-    graph_ql_query_string = """query {
+    query = """query {
                 users {
                     id
                     name
                 }
         }"""
 
-    data = {"query": graph_ql_query_string}
+    data = {"query": query}
     response_data = client.post("/graphql", json=data)
 
     assert response_data.status_code == 200
