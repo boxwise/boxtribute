2b8a33d717b23f4c2b312427a9f6b49bc067152b
Philipp Metzner <beth.aleph@yahoo.de>
2021-12-14 15:15:56 +0100

    Verify all fields for product query
    Group tests for faster test execution (only data-read access)

--- back/test/data/product.py
+++ back/test/data/product.py
@@ -4,10 +4,11 @@ from data.base import data as base_data
 from data.product_category import default_product_category_data
 from data.product_gender import default_product_gender_data
 from data.size_range import default_size_range_data
+from data.user import default_user_data
 
 
 def default_product_data():
-    mock_product = {
+    return {
         "id": 1,
         "base": base_data()[0]["id"],
         "category": default_product_category_data()["id"],
@@ -16,8 +17,8 @@ def default_product_data():
         "size_range": default_size_range_data()["id"],
         "in_shop": 0,
         "price": 1,
+        "created_by": default_user_data()["id"],
     }
-    return mock_product
 
 
 @pytest.fixture()
--- back/test/endpoint_tests/test_product.py
+++ back/test/endpoint_tests/test_product.py
@@ -1,39 +1,48 @@
-import pytest
-
-
-@pytest.mark.usefixtures("default_product")
 def test_product(client, default_product):
-    graph_ql_query_string = f"""query {{
+    query = f"""query {{
                 product(id: {default_product['id']}) {{
+                    id
                     name
-                    price
-                    gender
-                    sizes
                     category {{
                         hasGender
                     }}
+                    sizeRange {{
+                        id
+                    }}
+                    sizes
+                    base {{
+                        id
+                    }}
+                    price
+                    gender
+                    createdBy {{
+                        id
+                    }}
                 }}
             }}"""
-    data = {"query": graph_ql_query_string}
+    data = {"query": query}
     response_data = client.post("/graphql", json=data)
     queried_product = response_data.json["data"]["product"]
-    assert queried_product["name"] == default_product["name"]
-    assert queried_product["price"] == default_product["price"]
-    assert queried_product["gender"] == "Women"
-    assert queried_product["sizes"] == []
-    assert queried_product["category"]["hasGender"]
+    assert queried_product == {
+        "id": str(default_product["id"]),
+        "name": default_product["name"],
+        "category": {"hasGender": True},
+        "sizeRange": {"id": str(default_product["size_range"])},
+        "sizes": [],
+        "base": {"id": str(default_product["base"])},
+        "price": default_product["price"],
+        "gender": "Women",
+        "createdBy": {"id": str(default_product["created_by"])},
+    }
 
-
-@pytest.mark.usefixtures("default_product")
-def test_products(client, default_product):
-    graph_ql_query_string = """query {
+    query = """query {
                 products {
                     elements {
                         name
                     }
                 }
             }"""
-    data = {"query": graph_ql_query_string}
+    data = {"query": query}
     response_data = client.post("/graphql", json=data)
     queried_product = response_data.json["data"]["products"]["elements"][0]
     assert queried_product["name"] == default_product["name"]
