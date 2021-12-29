2749cdeab72b266d4c02bad98ac4756ca3183bbb
Daniel Spaude <daniel@spaude.de>
2021-12-29 20:32:10 +0100

    work on header menu

--- react/src/components/HeaderMenu.tsx
+++ react/src/components/HeaderMenu.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Box, Button, ListItem, UnorderedList, Text, Link, Stack } from "@chakra-ui/react";
+import { Box, Button, ListItem, UnorderedList, Text, Link, Stack, Flex } from "@chakra-ui/react";
 
 const OpenIcon = () => <Box>Open</Box>;
 const CloseIcon = () => <Box>Close</Box>;
@@ -12,8 +12,8 @@ const MenuToggle = ({ toggle, isOpen }) => (
   </Box>
 );
 
-const Logo = () => (
-  <Box>
+const Logo = (props) => (
+  <Box {...props}>
     <Text fontSize="lg" fontWeight="bold">
       Logo
     </Text>
@@ -29,39 +29,68 @@ const LoginLogoutButton = () => {
   const { isAuthenticated, logout, loginWithRedirect } = useAuth0();
   return (
     <Link>
-      {isAuthenticated && logout != null && <Text display="block" onClick={() => logout()}>Logout</Text>}
-      {!isAuthenticated && <Text display="block" onClick={() => loginWithRedirect()}>Login</Text>}
+      {isAuthenticated && logout != null && (
+        <Text display="block" onClick={() => logout()}>
+          Logout
+        </Text>
+      )}
+      {!isAuthenticated && (
+        <Text display="block" onClick={() => loginWithRedirect()}>
+          Login
+        </Text>
+      )}
     </Link>
   );
 };
 
 const MenuLinks = ({ isOpen }) => {
   return (
-    <Stack
-      spacing={8}
-      align="center"
-      justify={["center", "space-between", "flex-end", "flex-end"]}
-      direction={["column", "row", "row", "row"]}
-      pt={[4, 4, 0, 0]}
+    <Box
+      display={{ base: isOpen ? "block" : "none", md: "block" }}
+      flexBasis={{ base: "100%", md: "auto" }}
     >
-      <LoginLogoutButton />
-      <MenuItem to="/" text="Home" />
-      <MenuItem to="/locations" text="Locations" />
-      <MenuItem to="/boxes" text="Boxes" />
-    </Stack>
+      <Stack
+        spacing={8}
+        align="center"
+        justify={["center", "space-between", "flex-end", "flex-end"]}
+        direction={["column", "row", "row", "row"]}
+        pt={[4, 4, 0, 0]}
+      >
+        <LoginLogoutButton />
+        <MenuItem to="/" text="Home" />
+        <MenuItem to="/locations" text="Locations" />
+        <MenuItem to="/boxes" text="Boxes" />
+      </Stack>
+    </Box>
   );
 };
+
+const NavBarContainer = ({ children }) => (
+  <Flex
+    as="nav"
+    align="center"
+    justify="space-between"
+    wrap="wrap"
+    w="100%"
+    mb={8}
+    p={8}
+    bg={["primary.500", "primary.500", "transparent", "transparent"]}
+    color={["white", "white", "primary.700", "primary.700"]}
+  >
+    {children}
+  </Flex>
+);
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const toggle = () => setIsMenuOpen(!isMenuOpen);
 
   return (
-    <Box>
-      <Logo />
+    <NavBarContainer>
+      <Logo w="100px" color={["white", "white", "primary.500", "primary.500"]} />
       <MenuToggle toggle={toggle} isOpen={isMenuOpen} />
       <MenuLinks isOpen={isMenuOpen} />
-    </Box>
+    </NavBarContainer>
   );
 };
 
