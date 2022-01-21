0e63e57e136d10057b345164b4b159c3341de129
Daniel Spaude <daniel@spaude.de>
2022-01-21 09:29:26 +0100

    work on Box search form

--- react/src/views/boxes/Boxes.tsx
+++ react/src/views/boxes/Boxes.tsx
@@ -1,16 +1,46 @@
-import { Button, Heading, Input, VStack } from "@chakra-ui/react";
-import React, { useState } from "react";
+import React from "react";
+import { Button, FormControl, FormErrorMessage, Heading, Input, VStack } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
 
 const Boxes = () => {
-  const [boxNumberInput, setBoxNumberInput] = useState<string>();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting, isDirty, isValid },
+  } = useForm({
+    mode: "onChange"
+  });
+
+  const onSubmit = (values) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        alert(JSON.stringify(values, null, 2));
+        resolve(values);
+      }, 1000);
+    });
+  };
+
   return (
-    <VStack align="left">
+    <form onSubmit={handleSubmit(onSubmit)}>
       <VStack align="left">
         <Heading as="h2">Search for a box</Heading>
-        <Input placeholder="Enter box id (only numbers allowed)" type="number" onChange={event => setBoxNumberInput(event.target.value)}/>
-        <Button disabled={!boxNumberInput}>Search</Button>
+        <FormControl isInvalid={errors.boxNumber}>
+          <Input
+            id="boxNumber"
+            placeholder="Enter box id (only numbers allowed)"
+            {...register("boxNumber", {
+              required: "Box number is required",
+              minLength: { value: 4, message: "A Boxnumber has at least 4 digits" },
+            })}
+            type="number"
+          />
+          <FormErrorMessage>{errors.boxNumber && errors.boxNumber.message}</FormErrorMessage>
+        </FormControl>
+        <Button isLoading={isSubmitting} type="submit" disabled={!isDirty || !isValid || isSubmitting} >
+          Search
+        </Button>
       </VStack>
-    </VStack>
+    </form>
   );
 };
 
