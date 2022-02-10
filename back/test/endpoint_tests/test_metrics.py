50bed2f9e5879f92192a813d78a9e42d9c36ae83
Philipp Metzner <beth.aleph@yahoo.de>
2022-02-10 11:54:57 +0100

    Implement computation of sales incl. filtering
    Should (Transaction.tokens > 0) be used for selection?

--- back/test/endpoint_tests/test_metrics.py
+++ back/test/endpoint_tests/test_metrics.py
@@ -3,7 +3,7 @@ from datetime import date
 from utils import assert_successful_request
 
 
-def test_metrics_query(read_only_client):
+def test_metrics_query(read_only_client, default_transaction):
     query = "query { metrics { numberOfFamiliesServed } }"
     response = assert_successful_request(read_only_client, query, field="metrics")
     assert response == {"numberOfFamiliesServed": 1}
@@ -14,3 +14,13 @@ def test_metrics_query(read_only_client):
                 numberOfFamiliesServed(after: "{after}") }} }}"""
     response = assert_successful_request(read_only_client, query, field="metrics")
     assert response == {"numberOfFamiliesServed": 0}
+
+    query = "query { metrics { numberOfSales } }"
+    response = assert_successful_request(read_only_client, query, field="metrics")
+    assert response == {"numberOfSales": default_transaction["count"]}
+
+    # Expect no transactions to have been performed in the future
+    after = f"{date.today().year + 1}-01-01"
+    query = f"""query {{ metrics {{ numberOfSales(after: "{after}") }} }}"""
+    response = assert_successful_request(read_only_client, query, field="metrics")
+    assert response == {"numberOfSales": 0}
