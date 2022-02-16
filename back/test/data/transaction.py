b7fef3f6d6bf15941e84e22b08c8a7b2615c8b87
Philipp Metzner <beth.aleph@yahoo.de>
2022-02-16 10:29:12 +0100

    Extend transaction test data

--- back/test/data/transaction.py
+++ back/test/data/transaction.py
@@ -1,11 +1,11 @@
 import pytest
 from boxtribute_server.models.definitions.transaction import Transaction
 from boxtribute_server.models.utils import utcnow
-from data.beneficiary import default_beneficiary_data
+from data.beneficiary import another_beneficiary_data, default_beneficiary_data
 from data.product import default_product_data
 from data.user import default_user_data
 
-TIME = utcnow().replace(tzinfo=None)
+TIME = utcnow().replace(year=2021, tzinfo=None)
 
 
 def default_transaction_data():
@@ -21,6 +21,14 @@ def default_transaction_data():
     }
 
 
+def another_transaction_data():
+    data = default_transaction_data()
+    data["id"] = 3
+    data["beneficiary"] = another_beneficiary_data()["id"]
+    data["created_on"] = TIME.replace(year=2020)
+    return data
+
+
 @pytest.fixture()
 def default_transaction():
     return default_transaction_data()
@@ -28,3 +36,4 @@ def default_transaction():
 
 def create():
     Transaction.create(**default_transaction_data())
+    Transaction.create(**another_transaction_data())
