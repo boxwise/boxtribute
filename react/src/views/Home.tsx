62f20d5aef0c3adf9cb0b0a6ddb39b9ff1443ee3
flisowna <ewa.agnieszka.flis@gmail.com>
2022-03-14 12:52:32 +0100

    undo unneeded change

--- react/src/views/Home.tsx
+++ react/src/views/Home.tsx
@@ -2,11 +2,7 @@ import { Heading } from "@chakra-ui/react";
 import React from "react";
 
 const Home = () => {
-  return (
-    <div>
-      <Heading>not home</Heading>
-    </div>
-  );
+  return <Heading>Home</Heading>;
 };
 
 export default Home;
