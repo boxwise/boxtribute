c61880e56108c84039eecae2ba5f37be58394717
Daniel Spaude <daniel@spaude.de>
2022-06-20 11:46:00 +0200

    work on DistroSpotsView

--- react/src/views/DistroSpotsView/components/DistroSpots.stories.tsx
+++ react/src/views/DistroSpotsView/components/DistroSpots.stories.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import DistroSpots from './DistroSpots';
+import DistroSpots, { DistroSpot } from './DistroSpots';
 
+
+
+const mockedDistroSpots: DistroSpot[] = [
+  {
+    id: "1",
+    name: "Horgosz"
+  },
+  {
+    id: "2",
+    name: "Subotica",
+    nextDistroEventDate: new Date("2022/08/22")
+  },
+  {
+    id: "3",
+    name: "Bihac",
+    nextDistroEventDate: new Date("2022/09/15"), 
+    comment: "This distro spot is currently having issues regarding accessibility for our 2nd vehicle."
+  }
+];
 
 export default {
   title: 'Mobile Distro Events/Distro Spots',
@@ -11,11 +30,11 @@ export default {
   },
 } as ComponentMeta<typeof DistroSpots>;
 
-const Template: ComponentStory<typeof DistroSpots> = (args) => <DistroSpots />;
+const Template: ComponentStory<typeof DistroSpots> = (args) => <DistroSpots {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-    distroSpots: []
+    distroSpots: mockedDistroSpots
 }
 
 export const NoData = Template.bind({});
--- react/src/views/DistroSpotsView/components/DistroSpots.tsx
+++ react/src/views/DistroSpotsView/components/DistroSpots.tsx
@@ -1,7 +1,62 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  Box,
+  AccordionIcon,
+  AccordionPanel,
+  VStack,
+} from "@chakra-ui/react";
 import React from "react";
 
-const DistroSpots = () => {
-    return <>Distro Spots Placehoder</>
+interface GeoData {
+  lat: number;
+  long: number;
 }
+
+export interface DistroSpot {
+  id: string;
+  name: string;
+  geoData?: GeoData;
+  nextDistroEventDate?: Date;
+  comment?: string
+}
+
+interface DistroSpotsProps {
+  distroSpots: DistroSpot[];
+}
+
+const DistroSpots = ({ distroSpots }: DistroSpotsProps) => {
+  return (
+    <Accordion allowToggle>
+      {distroSpots.map((distroSpot) => {
+        return (
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                {/* <Box flex="1" textAlign="left"> */}
+                <VStack flex="1" textAlign="left">
+                  <Box fontWeight="bold">
+                    {distroSpot.name}
+                  </Box>
+                  <Box>
+                    {distroSpot.nextDistroEventDate?.toDateString()}
+                  </Box>
+                </VStack>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </AccordionPanel>
+          </AccordionItem>
+        );
+      })}
+    </Accordion>
+  );
+};
 
 export default DistroSpots;
