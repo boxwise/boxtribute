d07f18be720353e2841e4a2ac8c16c9d8c7605a9
Daniel Spaude <daniel@spaude.de>
2022-06-20 22:19:43 +0200

    refactor Header Menu (WIP)

--- react/src/components/HeaderMenu/HeaderMenu.stories.tsx
+++ react/src/components/HeaderMenu/HeaderMenu.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import HeaderMenu from "./HeaderMenu";
+import { action } from '@storybook/addon-actions';
 
 export default {
     title: "General/Header Menu", 
@@ -15,7 +16,17 @@ const Template: ComponentStory<typeof HeaderMenu> = (args) => <HeaderMenu {...ar
 
 export const Default = Template.bind({});
 Default.args = {
-    
+    loginWithRedirect: action("loginWithRedirect was clicked")
+}
+
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+    ...Default.args,
+    isAuthenticated: true,
+    user: {
+        picture: "/some/picture/path.jpg",
+        email: "emil@ngo.org"
+    }
 }
 
 // const { globalPreferences } = useContext(GlobalPreferencesContext);
