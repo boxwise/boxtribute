0ac3bade391a74aa7237b3e6cdea4afa7874a62a
Daniel Spaude <daniel@spaude.de>
2022-06-22 13:57:10 +0200

    distro spots view (WIP)

---
+++ react/src/views/DistroSpotsView/components/DistroSpotsContainer.tsx
@@ -0,0 +1,32 @@
+import { gql, useQuery } from "@apollo/client";
+import { GlobalPreferencesContext } from "providers/GlobalPreferencesProvider";
+import { useCallback, useContext } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useGlobalSiteState } from "utils/hooks";
+import DistroSpots from "./DistroSpots";
+
+export interface DistroSpotsContainerProps {
+  onGoToDistroEventView: (distroEventId: string) => void;
+}
+
+const DistroSpotsContainer = ({onGoToDistroEventView}: DistroSpotsContainerProps) => {
+
+  const {currentBaseId} = useGlobalSiteState();
+
+  const DISTRO_SPOTS_FOR_BASE_ID = gql``;
+
+  const { loading, error, data } = useQuery(DISTRO_SPOTS_FOR_BASE_ID, {
+  variables: {
+    baseId: currentBaseId,
+  },
+});
+
+  return (
+    <DistroSpots
+      distroSpots={data}
+      onDistroEventClick={onGoToDistroEventView}
+    />
+  );
+};
+
+export default DistroSpotsContainer;
---
+++ react/src/views/DistroSpotsView/components/DistroSpotsView.tsx
@@ -0,0 +1,22 @@
+import { useCallback } from "react";
+import { useGlobalSiteState } from "utils/hooks";
+import DistroSpotsContainer from "./DistroSpotsContainer";
+
+const DistroSpotsView = () => {
+
+  const {currentBaseId, navigate} = useGlobalSiteState();
+
+  const onGoToDistroEventViewHandler = useCallback(
+    (distroEventId: string) =>
+      navigate(`/bases/${currentBaseId}/distributions/events/${distroEventId}`),
+    [currentBaseId, navigate]
+  );
+
+  return (
+    <DistroSpotsContainer
+      onGoToDistroEventView={onGoToDistroEventViewHandler}
+    />
+  );
+};
+
+export default DistroSpotsView;
