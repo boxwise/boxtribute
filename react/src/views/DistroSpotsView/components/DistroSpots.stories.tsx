1c0dc9c83662a522764c4fc18955c05907527877
Daniel Spaude <daniel@spaude.de>
2022-06-23 13:31:15 +0200

    distro spots view/container, stories and mocked graphql (WIP)

--- react/src/views/DistroSpotsView/components/DistroSpots.stories.tsx
+++ react/src/views/DistroSpotsView/components/DistroSpots.stories.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import DistroSpots, { DistributionEventState, DistroSpot } from './DistroSpots';
-
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import DistroSpots, { DistributionEventState, DistroSpot } from "./DistroSpots";
 
 const mockedDistroSpots: DistroSpot[] = [
   {
     id: "1",
     name: "Horgosz",
-    distroEvents: []
+    distroEvents: [],
   },
   {
     id: "2",
@@ -16,47 +14,49 @@ const mockedDistroSpots: DistroSpot[] = [
     nextDistroEventDate: new Date("2022/08/22"),
     distroEvents: [
       {
-        eventDate: new Date("2022/08/22"),
+        date: new Date("2022/08/22"),
         state: DistributionEventState.PlanningDone,
-        id: "1"
+        id: "1",
       },
       {
-        eventDate: new Date("2022/02/13"),
+        date: new Date("2022/02/13"),
         state: DistributionEventState.Completed,
-        id: "2"
+        id: "2",
       },
-    ]
+    ],
   },
   {
     id: "3",
     name: "Bihac",
     nextDistroEventDate: new Date("2022/09/15"),
-    comment: "This distro spot is currently having issues regarding accessibility for our 2nd vehicle.",
+    comment:
+      "This distro spot is currently having issues regarding accessibility for our 2nd vehicle.",
     distroEvents: [
       {
-        eventDate: new Date("2022/09/15"),
+        date: new Date("2022/09/15"),
         state: DistributionEventState.Planning,
-        id: "3"
-      }
-    ]
-  }
+        id: "3",
+      },
+    ],
+  },
 ];
 
 export default {
-  title: 'Mobile Distro Events/Distro Spots',
+  title: "Mobile Distro Events/Distro Spots/Component",
   component: DistroSpots,
-  parameters: {
-  },
+  parameters: {},
 } as ComponentMeta<typeof DistroSpots>;
 
-const Template: ComponentStory<typeof DistroSpots> = (args) => <DistroSpots {...args} />;
+const Template: ComponentStory<typeof DistroSpots> = (args) => (
+  <DistroSpots {...args} />
+);
 
 export const Default = Template.bind({});
 Default.args = {
-    distroSpots: mockedDistroSpots
-}
+  distroSpots: mockedDistroSpots,
+};
 
 export const NoData = Template.bind({});
 NoData.args = {
   distroSpots: [],
-}
+};
