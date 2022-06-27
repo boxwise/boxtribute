bd5c992a455fee271577b73e36310c12b62df853
flisowna <ewa.agnieszka.flis@gmail.com>
2022-06-27 12:28:31 +0200

    distro event packing high level css

--- react/src/views/Distributions/DistroEventView/components/State4Packing/DistroEventPacking.tsx
+++ react/src/views/Distributions/DistroEventView/components/State4Packing/DistroEventPacking.tsx
@@ -5,139 +5,77 @@ import {
   AccordionButton,
   AccordionIcon,
   AccordionPanel,
-  VStack,
-  List,
-  ListItem,
   Text,
-  Button,
+  Box,
+  Checkbox,
+  Flex,
 } from "@chakra-ui/react";
 import { DistroEventStateLabel } from "views/Distributions/DistroSpotsView/components/DistroSpots";
 
-interface DistroEventPackingProps {
+export interface DistroEventPackingData {
   distroEventData: DistroEvent;
 }
 
-const DistroEventPacking = ({ distroEventData }: DistroEventPackingProps) => {
+interface DistroEventPackingProps {
+  distroEventDetailsData: DistroEventPackingData;
+  onCheckboxClick: () => void;
+}
+
+const DistroEventPacking = ({
+  distroEventDetailsData,
+  onCheckboxClick
+}: DistroEventPackingProps) => {
   return (
-    <VStack>
-      <Text fontSize="xl" mb={2}>
+    <Box textAlign='left'>
+        <Flex direction='column' mb={4}>
+      <Text fontSize="xl" mb={1}>
         Distro Event
       </Text>
-      <Text>
-        <strong>{distroEventData.eventDate?.toDateString()}</strong>
+      <Text lineHeight="normal">
+        <strong>{distroEventDetailsData.distroEventData.distroSpot}</strong>
+      </Text>
+      <Text lineHeight="normal">
+        <strong>
+          {distroEventDetailsData.distroEventData.eventDate?.toDateString()}
+        </strong>
       </Text>
       <Text>
-        <strong>{DistroEventStateLabel.get(distroEventData.status)}</strong>
+        {DistroEventStateLabel.get(
+          distroEventDetailsData.distroEventData.status
+        )}
       </Text>
-      <Accordion w={[300, 420, 500]} allowToggle mb={4}>
-          {distroEventData.itemsForPacking.map((item) => {
-            return (
-              <AccordionItem>
+      </Flex>
+      <Accordion w={[300, 420, 500]} allowToggle>
+        {distroEventDetailsData.distroEventData.itemsForPacking.map((item) => {
+          return (
+            <AccordionItem w={[300, 420, 500]} justifyItems="center">
+              <Flex justifyItems="center">
                 <AccordionButton>
+                  <Box flex="1" textAlign="center">
+                      <strong>
+                    {item.productName} {item.gender}
+                    </strong>
+                  </Box>
                   <AccordionIcon />
-                  <Text>{item.productName}</Text>
                 </AccordionButton>
-                <AccordionPanel>
-                  <List>
-                    <ListItem>
-                      <Text>{item.productName}</Text>
-                    </ListItem>
-                    <ListItem>
-                      <Text>{item.size}</Text>
-                    </ListItem>
-                    <ListItem>
-                      <Text>{item.items}</Text>
-                    </ListItem>
-                  </List>
-                  <Button
-                    onClick={() => {}}
-                    variantColor="blue"
-                    variant="outline"
-                    size="sm"
-                    mr={2}
-                  >
-                    Edit
-                  </Button>
-                  <Button
-                    onClick={() => {}}
-                    variantColor="red"
-                    variant="outline"
-                    size="sm"
-                  >
-                    Remove
-                  </Button>
-                </AccordionPanel>
-              </AccordionItem>
-            );
-          }, [])}
-        
-        </Accordion>
-    </VStack>
+              </Flex>
+              <AccordionPanel py={0}>
+                <Flex alignContent='center' justifyItems='center' py={2} borderTop="1px" borderColor="gray.300">
+                  <Text  flex="1" textAlign="center" >
+                    {item.items} x {item.size}
+                  </Text>
+                  {/* the checkbox fires 2 times on the onclick => it must be corrected */}
+                    <Flex alignItems="center" onClick={() => onCheckboxClick()}>
+                     <Checkbox/>
+                  </Flex>
+                </Flex>
+              </AccordionPanel>
+            </AccordionItem>
+          );
+        }, [])}
+      </Accordion>
+    </Box>
   );
 };
-
 export default DistroEventPacking;
-{/* 
-<Accordion w={[300, 420, 500]} allowToggle mb={4}>
-      {distroSpots.map((distroSpot) => {
-        return (
-          <AccordionItem>
-            <h2>
-              <AccordionButton>
-                <VStack flex="1" textAlign="left">
-                  <Box fontWeight="bold">{distroSpot.name}</Box>
-                  {distroSpot.nextDistroEventDate ? (
-                    <Box>
-                      Next distribution on:{" "}
-                      {distroSpot.nextDistroEventDate?.toDateString()}{" "}
-                    </Box>
-                  ) : null}
-                </VStack>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel pb={4}>
-              {distroSpot.comment ? (
-                <Box my={2}>
-                  <strong>Comment:</strong> {distroSpot.comment}
-                </Box>
-              ) : null}
-              {distroSpot.distroEvents?.length > 0 ? (
-                <Text>
-                  <strong>Distibution Events: </strong>
-                </Text>
-              ) : (
-                <Text>No Distribution Events</Text>
-              )}
 
-              <List>
-                {distroSpot.distroEvents.map((distroEvent, i) => {
-                  return (
-                    <>
-                      <ListItem
-                        border="1px"
-                        p={2}
-                        my={2}
-                        key={i}
-                        cursor="pointer"
-                        _hover={{
-                          color: "teal.500",
-                        }}
-                        onClick={() => onDistroEventClick(distroEvent.id)}
-                      >
-                        <Box>Date: {distroEvent.date?.toDateString()}</Box>
-                        <Box>
-                          Status:{" "}
-                          {DistroEventStateLabel.get(distroEvent.state)}
-                        </Box>
-                      </ListItem>
-                    </>
-                  );
-                })}
-              </List>
-              <Button onClick={() => onCreateNewDistroEventClick()}>Create New Event</Button>
-            </AccordionPanel>
-          </AccordionItem>
-        );
-      })}
-    </Accordion> */}
