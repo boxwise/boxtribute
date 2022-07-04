4d16712fe17d27db6cbc4a3e098142a437a725d2
Daniel Spaude <daniel@spaude.de>
2022-07-04 09:24:27 +0200

    add yup

--- react/src/views/Boxes/components/BoxesTable.tsx
+++ react/src/views/Boxes/components/BoxesTable.tsx
@@ -231,7 +231,12 @@ const ActualTable = ({
     nextPage,
     previousPage,
   } = useTable(
+  // TODO: remove this ts-ignore again and try to fix the type error properly
+  // was most likely caused by setting one of the following flags in .tsconfig:
+  // "strictNullChecks": true
+  // "strictFunctionTypes": false
     {
+  // @ts-ignore
       columns,
       data: tableData,
       initialState: {
@@ -316,7 +321,7 @@ const ActualTable = ({
               <Tr
                 cursor="pointer"
                 {...row.getRowProps()}
-                onClick={() => onBoxRowClick(row.original.labelIdentifier)}
+                onClick={() => onBoxRowClick(row.original['labelIdentifier'])}
                 key={i}
               >
                 {row.cells.map((cell, i) => {
