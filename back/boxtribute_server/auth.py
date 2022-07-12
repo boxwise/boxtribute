c745b717609ba38a791aaf87f9755167c34478ed
Philipp Metzner <beth.aleph@yahoo.de>
2022-07-12 13:11:22 +0200

    Use HTTP status code 401 as default for AuthenticationFailed

--- back/boxtribute_server/auth.py
+++ back/boxtribute_server/auth.py
@@ -18,7 +18,7 @@ def get_auth_string_from_header():
 
 def get_token_from_auth_header(header_string):
     """Obtain access token from the Authorization header. In case of parsing errors
-    return error information and HTTP status code 401.
+    return error information and relevant HTTP status code.
     """
     if not header_string:
         raise AuthenticationFailed(
@@ -26,7 +26,6 @@ def get_token_from_auth_header(header_string):
                 "code": "authorization_header_missing",
                 "description": "Authorization header is expected",
             },
-            401,
         )
 
     parts = header_string.split()
@@ -37,11 +36,10 @@ def get_token_from_auth_header(header_string):
                 "code": "invalid_header",
                 "description": "Authorization header must start with Bearer",
             },
-            401,
         )
     elif len(parts) == 1:
         raise AuthenticationFailed(
-            {"code": "invalid_header", "description": "Token not found"}, 401
+            {"code": "invalid_header", "description": "Token not found"}
         )
     elif len(parts) > 2:
         raise AuthenticationFailed(
@@ -49,7 +47,6 @@ def get_token_from_auth_header(header_string):
                 "code": "invalid_header",
                 "description": "Authorization header must be Bearer token",
             },
-            401,
         )
 
     token = parts[1]
@@ -83,7 +80,7 @@ def decode_jwt(*, token, public_key, domain, audience):
         )
     except jwt.ExpiredSignatureError:
         raise AuthenticationFailed(
-            {"code": "token_expired", "description": "token is expired"}, 401
+            {"code": "token_expired", "description": "token is expired"}
         )
     except jwt.JWTClaimsError:
         raise AuthenticationFailed(
@@ -92,7 +89,6 @@ def decode_jwt(*, token, public_key, domain, audience):
                 "description": "incorrect claims, "
                 "please check the audience and issuer",
             },
-            401,
         )
     except JOSEError as e:
         raise AuthenticationFailed(
@@ -101,7 +97,6 @@ def decode_jwt(*, token, public_key, domain, audience):
                 "description": "Unable to parse authentication token.",
                 "message": str(e),
             },
-            401,
         )
     except Exception:
         raise AuthenticationFailed(
