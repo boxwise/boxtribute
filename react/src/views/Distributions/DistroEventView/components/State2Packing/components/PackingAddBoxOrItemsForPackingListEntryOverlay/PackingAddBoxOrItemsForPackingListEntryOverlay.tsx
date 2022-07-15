e2bcd265d747f8fca16aec8201def15cf89f5b88
Daniel Spaude <daniel@spaude.de>
2022-07-15 15:56:44 +0200

    Mobile Distro: Packing state - fix 'reset overlay' issue - WIP

--- react/src/views/Distributions/DistroEventView/components/State2Packing/components/PackingAddBoxOrItemsForPackingListEntryOverlay/PackingAddBoxOrItemsForPackingListEntryOverlay.tsx
+++ react/src/views/Distributions/DistroEventView/components/State2Packing/components/PackingAddBoxOrItemsForPackingListEntryOverlay/PackingAddBoxOrItemsForPackingListEntryOverlay.tsx
@@ -1,15 +1,6 @@
-import { useApolloClient } from "@apollo/client";
-import {
-  Modal,
-  ModalOverlay,
-} from "@chakra-ui/react";
+import { Modal, ModalOverlay } from "@chakra-ui/react";
 import { useCallback, useState } from "react";
-import { BOX_DETAILS_FOR_MOBILE_DISTRO_QUERY } from "views/Distributions/queries";
 import { BoxData, IPackingListEntry } from "views/Distributions/types";
-import {
-  BoxDetailsQuery,
-  BoxDetailsQueryVariables,
-} from "types/generated/graphql";
 import PackingBoxDetailsOverlayContent from "./PackingBoxDetailsOverlayContent";
 import PackingScanBoxOrFindByLabelOverlayContent from "./PackingScanBoxOrFindByLabelOverlayContent";
 
@@ -19,47 +10,11 @@ interface PackingScanBoxOrFindByLabelOverlayProps {
   onClose: () => void;
   onAddBoxToDistributionEvent: (boxId: string) => void;
   // TODO: add correct signature / type here
-  onAddUnboxedItemsToDistributionEvent: (boxId: string, numberOfItemsToMove: number) => void;
+  onAddUnboxedItemsToDistributionEvent: (
+    boxId: string,
+    numberOfItemsToMove: number
+  ) => void;
 }
-
-type ValidateBoxByLabelForMatchingPackingListEntry = (
-  boxLabel: string
-) => Promise<{ isValid: boolean; boxData?: BoxData | null }>;
-const useValidateBoxByLabelMatchingPackingListEntry = (
-  packingListEntry: IPackingListEntry
-): ValidateBoxByLabelForMatchingPackingListEntry => {
-  const apolloClient = useApolloClient();
-  return (boxLabel: string) => {
-    return apolloClient
-      .query<BoxDetailsQuery, BoxDetailsQueryVariables>({
-        query: BOX_DETAILS_FOR_MOBILE_DISTRO_QUERY,
-        variables: {
-          labelIdentifier: boxLabel,
-        },
-      })
-      .then(({ data }) => {
-        const box = data?.box;
-        if (box != null) {
-          if (
-            box.product?.id === packingListEntry.product.id &&
-            box.size.id === packingListEntry.size?.id
-          ) {
-            return {
-              isValid: true,
-              boxData: {
-                ...box,
-                numberOfItems: box.items,
-              },
-            };
-          }
-        }
-        return {
-          isValid: false,
-          boxData: null,
-        };
-      });
-  };
-};
 
 const PackingAddBoxOrItemsForPackingListEntryOverlay = ({
   onAddBoxToDistributionEvent,
@@ -95,16 +50,23 @@ const PackingAddBoxOrItemsForPackingListEntryOverlay = ({
       {!showPackingBoxDetails && (
         <PackingScanBoxOrFindByLabelOverlayContent
           packingListEntry={packingListEntry}
-          onFoundMatchingBox={onFoundMatchingBox}
+          onFoundMatchingBox={(box: BoxData) => {
+            resetState();
+            onFoundMatchingBox(box);
+          }}
         />
       )}
 
       {showPackingBoxDetails && boxData != null && (
         <PackingBoxDetailsOverlayContent
-                  targetNumberOfItemsToPack={packingListEntry.numberOfItems}
-                  boxData={boxData}
-                  onAddBoxToDistributionEvent={onAddBoxToDistributionEvent}
-                  onAddIndividualItemsToDistribution={onAddUnboxedItemsToDistributionEvent}                  />
+          targetNumberOfItemsToPack={packingListEntry.numberOfItems}
+          boxData={boxData}
+          onAddBoxToDistributionEvent={onAddBoxToDistributionEvent}
+          onAddIndividualItemsToDistribution={(boxId: string, numberOfItemsToMove: number) => {
+            resetState();
+            onAddUnboxedItemsToDistributionEvent(boxId, numberOfItemsToMove);
+          }}
+        />
       )}
     </Modal>
   );
