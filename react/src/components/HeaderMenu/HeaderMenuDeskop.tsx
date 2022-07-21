d25f39e8ebbfdd3b4edeabb75bea7ee9bffc7d67
flisowna <ewa.agnieszka.flis@gmail.com>
2022-07-21 22:47:26 +0200

    small changes to menu elements

---
+++ react/src/components/HeaderMenu/HeaderMenuDeskop.tsx
@@ -0,0 +1,210 @@
+import { NavLink } from "react-router-dom";
+import {
+  Text,
+  Button,
+  Stack,
+  Flex,
+  Image,
+  IconButton,
+  Img,
+  Menu,
+  MenuButton,
+  useDisclosure,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
+
+import { AiOutlineQrcode } from "react-icons/ai";
+import BoxtributeLogo from "../../assets/images/boxtribute-logo.png";
+import {
+  HeaderMenuProps,
+  LoginOrUserMenuButtonProps,
+  MenuItemProps,
+  MenuLinksProps as MenuItemsProps,
+  UserMenuProps,
+} from "./HeaderMenu";
+
+const Logo = () => (
+  <NavLink to="/">
+    <Image src={BoxtributeLogo} maxH={"3.5em"} />
+  </NavLink>
+);
+
+// const BaseSwitcher = ({
+//   currentActiveBaseId,
+//   availableBases,
+// }: BaseSwitcherProps) => {
+//   return (
+//     <MenuGroup>
+//       {availableBases?.map((base, i) => (
+//         <MenuItem key={base.id}>
+//           <Link
+//             style={currentActiveBaseId === base.id ? { color: "orange" } : {}}
+//             to={`/bases/${base.id}/locations`}
+//           >
+//             {base.name}
+//           </Link>
+//         </MenuItem>
+//       ))}
+//     </MenuGroup>
+//   );
+// };
+
+const UserMenu = ({ logout, user }: UserMenuProps) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <Menu isOpen={isOpen}>
+      <MenuButton
+        as={IconButton}
+        icon={<Img src={user?.picture} width={10} height={10} />}
+        onMouseEnter={onOpen}
+        onMouseLeave={onClose}
+      />
+      <MenuList
+        onMouseEnter={onOpen}
+        onMouseLeave={onClose}
+        my={0}
+        border="2px"
+        borderRadius="0px"
+        py={0}
+      >
+        {/* <BaseSwitcher
+          currentActiveBaseId={currentActiveBaseId}
+          availableBases={availableBases}
+        /> */}
+
+        <MenuItem py={2}>Profile ({user?.email})</MenuItem>
+        <MenuItem py={2} onClick={() => logout()}>
+          Logout
+        </MenuItem>
+      </MenuList>
+    </Menu>
+  );
+};
+
+const LoginOrUserMenuButton = ({
+  isAuthenticated,
+  logout,
+  loginWithRedirect,
+  user,
+}: LoginOrUserMenuButtonProps) => {
+  return isAuthenticated ? (
+    <UserMenu user={user} logout={logout} />
+  ) : (
+    <Button
+      border="2px"
+      borderRadius="0px"
+      onClick={() => (isAuthenticated ? logout() : loginWithRedirect())}
+    >
+      Login
+    </Button>
+  );
+};
+
+const MenuItemDesktop = ({ ...props }: MenuItemProps) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <Menu isOpen={isOpen}>
+      <MenuButton
+        my={0}
+        variant="outline"
+        colorScheme="black"
+        borderRadius="0px"
+        as={Button}
+        border="2px"
+        onMouseEnter={onOpen}
+        onMouseLeave={onClose}
+      >
+        <Text display="block">{props.text}</Text>
+      </MenuButton>
+      <MenuList
+        onMouseEnter={onOpen}
+        onMouseLeave={onClose}
+        border="2px"
+        p={0}
+        borderRadius="0px"
+        my={0}
+      >
+        {props.links.map((link, i) => (
+          <MenuItem py={2} px={3} key={i}>
+            <NavLink to={link.link}>{link.name}</NavLink>
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+};
+
+const MenuItemsDesktop = ({ ...props }: MenuItemsProps) => {
+  return (
+    <Flex w="100%" flexBasis={{ base: "100%", md: "auto" }}>
+      <Stack
+        direction={["column", "row", "row", "row"]}
+        justifyItems={["center", "space-between", "flex-end", "flex-end"]}
+      >
+        {props.menuItems.map((item, i) => (
+          <MenuItemDesktop key={i} {...item} />
+        ))}
+
+        <LoginOrUserMenuButton
+          isAuthenticated={props.isAuthenticated}
+          logout={props.logout}
+          loginWithRedirect={props.loginWithRedirect}
+          user={props.user}
+        />
+      </Stack>
+    </Flex>
+  );
+};
+
+const HeaderMenuDesktopContainer = ({ children, ...props }) => {
+  return (
+    <Flex
+      as="nav"
+      alignItems="flex-start"
+      justify="space-between"
+      w="100%"
+      mb={8}
+      pt={4}
+      pb={4}
+      color={"black"}
+    >
+      {children}
+    </Flex>
+  );
+};
+
+const QrScannerButton = ({ onClick }: { onClick: () => void }) => (
+  <IconButton
+    h={20}
+    w={20}
+    fontSize="50px"
+    colorScheme="gray"
+    backgroundColor={"transparent"}
+    aria-label="Scan QR Code"
+    icon={<AiOutlineQrcode />}
+    onClick={onClick}
+  />
+);
+
+const HeaderMenuDeskop = (props: HeaderMenuProps) => {
+  return (
+    <HeaderMenuDesktopContainer>
+      <Flex w="100%" justifyContent="space-between" alignItems="center">
+        <Logo />
+        <Flex justifyItems="flex-end" alignItems="center">
+          <MenuItemsDesktop
+            user={props.user}
+            menuItems={props.menuItems}
+            isAuthenticated={props.isAuthenticated}
+            logout={props.logout}
+            loginWithRedirect={props.loginWithRedirect}
+          />
+          <QrScannerButton onClick={props.onClickScanQrCode} />
+        </Flex>
+      </Flex>
+    </HeaderMenuDesktopContainer>
+  );
+};
+
+export default HeaderMenuDeskop;
---
+++ react/src/components/HeaderMenu/HeaderMenuMobile.tsx
@@ -0,0 +1,247 @@
+import { Icon, ChevronDownIcon } from "@chakra-ui/icons";
+import {
+  Stack,
+  useDisclosure,
+  Flex,
+  Collapse,
+  Text,
+  Box,
+  Button,
+  IconButton,
+  Image,
+  Img,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import {
+  AiFillCloseCircle,
+  AiOutlineMenu,
+  AiOutlineQrcode,
+} from "react-icons/ai";
+import { Link, NavLink } from "react-router-dom";
+import {
+  HeaderMenuProps,
+  LoginOrUserMenuButtonProps,
+  MenuItemProps,
+  MenuLinksProps as MenuItemsProps,
+} from "./HeaderMenu";
+import BoxtributeLogo from "../../assets/images/boxtribute-logo.png";
+
+type MenuItemsMobileProps = MenuItemsProps & {
+  isMenuOpen: boolean;
+  setIsMenuOpen: (isOpen: boolean) => void;
+};
+
+const MenuToggle = ({ toggle, isOpen, ...props }) => (
+  <IconButton
+    onClick={toggle}
+    icon={isOpen ? <AiFillCloseCircle /> : <AiOutlineMenu />}
+    aria-label={isOpen ? "close menu" : "open menu"}
+  />
+);
+
+const Logo = () => (
+  <NavLink to="/">
+    <Image src={BoxtributeLogo} maxH={"3.5em"} />
+  </NavLink>
+);
+
+const LoginOrUserMenuButtonMobile = ({
+  isAuthenticated,
+  logout,
+  loginWithRedirect,
+  user,
+}: LoginOrUserMenuButtonProps) => {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return isAuthenticated ? (
+    <Stack spacing={4} onClick={onToggle}>
+      <Flex
+        px={4}
+        border="1px"
+        w="100%"
+        py={2}
+        justify={"space-between"}
+        align={"center"}
+        _hover={{
+          textDecoration: "none",
+          backgroundColor: "gray.100",
+        }}
+        as={Button}
+        borderRadius="0px"
+        backgroundColor={isOpen ? "gray.100" : "transparent"}
+      >
+        <Flex maxW="85%" align={"center"}>
+          {user?.picture ? (
+            <Img src={user?.picture} width={8} height={8} mr={2} />
+          ) : null}
+          <Text fontWeight={600} isTruncated>
+            {user?.email}
+          </Text>
+        </Flex>
+        <Icon
+          as={ChevronDownIcon}
+          transition={"all .25s ease-in-out"}
+          transform={isOpen ? "rotate(180deg)" : ""}
+          w={6}
+          h={6}
+        />
+      </Flex>
+
+      <Collapse in={isOpen} animateOpacity style={{ marginTop: "10px" }}>
+        <Stack pl={4} borderLeft={1} borderStyle={"solid"} align={"start"}>
+          <Box py={1} px={4}>
+            Profile
+          </Box>
+          <Box py={1} px={4} onClick={() => logout()}>
+            Logout
+          </Box>
+        </Stack>
+      </Collapse>
+    </Stack>
+  ) : (
+    <Button
+      py={2}
+      colorScheme="gray"
+      borderRadius="0px"
+      flex="1"
+      border="1px"
+      w="250px"
+      onClick={() => (isAuthenticated ? logout() : loginWithRedirect())}
+    >
+      Login
+    </Button>
+  );
+};
+
+const MenuItemsMobile = ({
+  isMenuOpen,
+  setIsMenuOpen,
+  ...props
+}: MenuItemsMobileProps) => {
+  return (
+    <Flex
+      w="100%"
+      flexBasis={{ base: "100%", md: "auto" }}
+      display={isMenuOpen ? "block" : "none"}
+    >
+      <Stack alignItems="start-end" direction="column">
+        {props.menuItems.map((item, i) => (
+          <MenuItemMobile key={i} {...item} setIsMenuOpen={setIsMenuOpen} />
+        ))}
+        <LoginOrUserMenuButtonMobile
+          isAuthenticated={props.isAuthenticated}
+          logout={props.logout}
+          loginWithRedirect={props.loginWithRedirect}
+          user={props.user}
+        />
+      </Stack>
+    </Flex>
+  );
+};
+
+const MenuItemMobile = ({
+  setIsMenuOpen,
+  links,
+  text,
+}: MenuItemProps & { setIsMenuOpen: (isOpen: boolean) => void }) => {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <Stack spacing={4} onClick={onToggle}>
+      <Flex
+        py={2}
+        justify={"space-between"}
+        align={"center"}
+        _hover={{
+          textDecoration: "none",
+          backgroundColor: "gray.100",
+        }}
+        px={4}
+        border="1px"
+        as={Button}
+        borderRadius="0px"
+        backgroundColor={isOpen ? "gray.100" : "transparent"}
+      >
+        <Text fontWeight={600}>{text}</Text>
+        <Icon
+          as={ChevronDownIcon}
+          transition={"all .25s ease-in-out"}
+          transform={isOpen ? "rotate(180deg)" : ""}
+          w={6}
+          h={6}
+        />
+      </Flex>
+      <Collapse in={isOpen} animateOpacity style={{ marginTop: "10px" }}>
+        <Stack pl={4} borderLeft={1} borderStyle={"solid"} align={"start"}>
+          {links.map((link) => (
+            <Box py={1} px={4} onClick={() => setIsMenuOpen(false)}>
+              <Link key={link.name} to={link.link}>
+                {link.name}
+              </Link>
+            </Box>
+          ))}
+        </Stack>
+      </Collapse>
+    </Stack>
+  );
+};
+
+const HeaderMenuMobileContainer = ({ children, ...props }) => {
+  return (
+    <Flex
+      as="nav"
+      wrap="wrap"
+      w="100%"
+      pt={4}
+      pb={4}
+      color={"black"}
+      zIndex="2"
+    >
+      {children}
+    </Flex>
+  );
+};
+
+// TODO: Extract this (because it's not mobile/desktop specific) out into a seperate component file
+const QrScannerButton = ({ onClick }: { onClick: () => void }) => (
+  <IconButton
+    h={20}
+    w={20}
+    fontSize="50px"
+    colorScheme="gray"
+    backgroundColor={"transparent"}
+    aria-label="Scan QR Code"
+    icon={<AiOutlineQrcode />}
+    onClick={onClick}
+  />
+);
+
+const HeaderMenuMobile = (props: HeaderMenuProps) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggle = () => setIsMenuOpen((curr) => !curr);
+  return (
+    <HeaderMenuMobileContainer>
+      <Flex justifyContent="space-between" w="100%" alignItems="center">
+        <Logo />
+        <QrScannerButton onClick={props.onClickScanQrCode} />
+        <MenuToggle
+          toggle={toggle}
+          isOpen={isMenuOpen}
+          display={{ base: "inline flex", md: "none" }}
+        />
+      </Flex>
+      <MenuItemsMobile
+        isMenuOpen={isMenuOpen}
+        setIsMenuOpen={setIsMenuOpen}
+        logout={props.logout}
+        loginWithRedirect={props.loginWithRedirect}
+        user={props.user}
+        isAuthenticated={props.isAuthenticated}
+        menuItems={props.menuItems}
+      />
+    </HeaderMenuMobileContainer>
+  );
+};
+
+export default HeaderMenuMobile;
