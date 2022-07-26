edf1f311a82c972ae5d2ad3ff23d472757dc4f90
Daniel Spaude <daniel@spaude.de>
2022-07-26 10:12:00 +0200

    Add BaseDashboardView skeleton and make it the base default route

---
+++ react/src/views/BaseDashboard/BaseDashboardView.tsx
@@ -0,0 +1,40 @@
+import { gql, useQuery } from "@apollo/client";
+import { Center, Heading } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+import { BaseDataQuery, BaseDataQueryVariables } from "types/generated/graphql";
+
+export const BASE_DATA = gql`
+  query BaseData($baseId: ID!) {
+    base(id: $baseId) {
+      name
+      locations {
+        name
+      }
+    }
+  }
+`;
+
+const BaseDashboardView = () => {
+  const baseId = useParams<{ baseId: string }>().baseId!;
+
+  const { loading, error, data } = useQuery<BaseDataQuery, BaseDataQueryVariables>(BASE_DATA, {
+    variables: {
+      baseId,
+    },
+  });
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    console.error(error);
+    return <div>Error!</div>;
+  }
+
+  return (
+    <Center>
+      <Heading>Welcome to the Base {data?.base?.name}</Heading>
+    </Center>
+  );
+};
+
+export default BaseDashboardView;
