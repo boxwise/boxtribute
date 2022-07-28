5eb38d310bc5992287ed3d3a3f451a25ba5c75f7
Daniel Spaude <daniel@spaude.de>
2022-07-28 21:43:39 +0200

    Mobile Distro: packing - WIP

--- react/src/views/Distributions/DistroEventView/components/State2Packing/components/PackedContentListOverlay.tsx
+++ react/src/views/Distributions/DistroEventView/components/State2Packing/components/PackedContentListOverlay.tsx
@@ -18,6 +18,14 @@ import {
   StatHelpText,
   StatLabel,
   StatNumber,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
 } from "@chakra-ui/react";
 import { useMemo } from "react";
 import {
@@ -60,7 +68,7 @@ const UnboxedItemsCollectionList = ({
             {/* <Text mr={4}>{box.labelIdentifier}</Text> */}
             <Text>
               {" "}
-              number of items: {unboxedItemsCollection.numberOfItems}
+              # of items: {unboxedItemsCollection.numberOfItems}
             </Text>
           </Flex>
           <Box>
@@ -89,38 +97,37 @@ const BoxesList = ({ boxesData }: { boxesData: BoxData[] }) => (
     <Heading as="h3" size="md">
       Boxes
     </Heading>
-    <Flex direction="column">
-      {boxesData.map((box) => (
-        <Flex
-          alignItems="center"
-          borderBottom="1px"
-          borderColor="gray.300"
-          my={2}
-          key={box.labelIdentifier}
-          justifyContent="space-between"
-        >
-          <Flex direction="row">
-            <Text mr={4}>{box.labelIdentifier}</Text>
-            <Text> number of items: {box.numberOfItems}</Text>
-          </Flex>
-          <Box>
-            <IconButton
-              _hover={{
-                backgroundColor: "transparent",
-                opacity: "0.5",
-              }}
-              backgroundColor="transparent"
-              aria-label="Delete"
-              color="teal"
-              icon={<DeleteIcon />}
-              onClick={() => {
-                // packingActionProps.onDeleteBoxFromDistribution(box.id)
-              }}
-            />
-          </Box>
-        </Flex>
-      ))}
-    </Flex>
+    <TableContainer mt={3}>
+      <Table size="sm">
+        <Thead>
+          <Tr>
+            <Th>Box Label</Th>
+            <Th isNumeric># of items</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {boxesData.map((box) => (
+            <Tr key={box.labelIdentifier}>
+              <Td>{box.labelIdentifier}</Td>
+              <Td isNumeric>{box.numberOfItems}</Td>
+            </Tr>
+            // <IconButton
+            //   _hover={{
+            //     backgroundColor: "transparent",
+            //     opacity: "0.5",
+            //   }}
+            //   backgroundColor="transparent"
+            //   aria-label="Delete"
+            //   color="teal"
+            //   icon={<DeleteIcon />}
+            //   onClick={() => {
+            //     // packingActionProps.onDeleteBoxFromDistribution(box.id)
+            //   }}
+            // />
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
   </>
 );
 
@@ -162,32 +169,39 @@ PackedContentListOverlayProps) => {
         </ModalHeader>
         <ModalCloseButton />
         <ModalBody mx={4}>
-          {boxesData.length > 0 && <BoxesList boxesData={boxesData} />}
-          {unboxedItemCollectionData.length > 0 && (
-            <UnboxedItemsCollectionList
-              unboxedItemCollectionData={unboxedItemCollectionData}
-            />
+          {boxesData.length > 0 && (
+            <Box my={5}>
+              <BoxesList boxesData={boxesData} />
+            </Box>
           )}
-            <StatGroup>
-              <Stat>
-                <StatLabel>Packed # of items</StatLabel>
-                <StatNumber>{totalNumberOfPackedItems}</StatNumber>
-              </Stat>
-              <Stat>
-                <StatLabel>Target # of items</StatLabel>
-                <StatNumber>{packingListEntry.numberOfItems}</StatNumber>
-              </Stat>
-            </StatGroup>
-            {targetNumberOfItemsReached && (
-              <Badge colorScheme="green">
-                {/* <CheckIcon /> Target number ({packingListEntry.numberOfItems}) fullfilled (with {totalNumberOfPackedItems} items) */}
-                <CheckIcon />{' '}
-                Enough items packed
-              </Badge>
-            )}
-            {!targetNumberOfItemsReached && (
-              <Badge colorScheme="red">Not yet enough items</Badge>
-            )}
+          {unboxedItemCollectionData.length > 0 && (
+            <Box my={5}>
+              <UnboxedItemsCollectionList
+                unboxedItemCollectionData={unboxedItemCollectionData}
+              />
+            </Box>
+          )}
+
+          <StatGroup my={5}>
+            <Stat>
+              <StatLabel>Packed # of items</StatLabel>
+              <StatNumber>{totalNumberOfPackedItems}</StatNumber>
+            </Stat>
+            <Stat>
+              <StatLabel>Target # of items</StatLabel>
+              <StatNumber>{packingListEntry.numberOfItems}</StatNumber>
+            </Stat>
+          </StatGroup>
+
+          {targetNumberOfItemsReached && (
+            <Badge colorScheme="green">
+              {/* <CheckIcon /> Target number ({packingListEntry.numberOfItems}) fullfilled (with {totalNumberOfPackedItems} items) */}
+              <CheckIcon /> Enough items packed
+            </Badge>
+          )}
+          {!targetNumberOfItemsReached && (
+            <Badge colorScheme="red">Not yet enough items</Badge>
+          )}
         </ModalBody>
         <ModalFooter />
       </ModalContent>
