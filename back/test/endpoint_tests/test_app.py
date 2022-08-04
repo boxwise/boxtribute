69bcd9dc6d61afb491a74053f0c085ff86baa350
Philipp Metzner <beth.aleph@yahoo.de>
2022-08-04 10:39:45 +0200

    Verify handling when non-existent tag is queried

--- back/test/endpoint_tests/test_app.py
+++ back/test/endpoint_tests/test_app.py
@@ -79,11 +79,12 @@ def test_invalid_pagination_input(read_only_client):
         "product",
         "productCategory",
         "shipment",
+        "tag",
         "transferAgreement",
     ],
 )
 def test_query_non_existent_resource(read_only_client, resource):
-    # Test cases 2.1.4, 3.1.3
+    # Test cases 2.1.4, 3.1.3, 4.1.3
     query = f"query {{ {resource}(id: 0) {{ id }} }}"
     response = assert_bad_user_input(read_only_client, query, field=resource)
     assert "SQL" not in response.json["errors"][0]["message"]
