59a5a9dd15a77acbd45bdb7281d1277ddfa9b92f
Daniel Spaude <daniel@spaude.de>
2022-08-08 21:35:49 +0200

    Mobile Distro: FE - iterate on planning views (WIP)

--- react/src/views/Distributions/DistroEventView/components/State1Planning/DistroEventDetailsForPlanningState.tsx
+++ react/src/views/Distributions/DistroEventView/components/State1Planning/DistroEventDetailsForPlanningState.tsx
@@ -1,43 +1,70 @@
 import {
   Box,
   Button,
+  Center,
   Flex,
   Heading,
   IconButton,
   SimpleGrid,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
   Text,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+  VStack,
 } from "@chakra-ui/react";
 import { CloseIcon, EditIcon } from "@chakra-ui/icons";
 import { IPackingListEntry } from "views/Distributions/types";
 import _ from "lodash";
 
-
-
 interface IPackingListEntrieGroupForProduct {
   productId: string;
   productName: string;
-  packingListEntries: IPackingListEntry[]
+  packingListEntries: IPackingListEntry[];
 }
 
-const PackingListEntrieGroupForProduct = ({data}: {data: IPackingListEntrieGroupForProduct}) => {
-  const {productId, productName, packingListEntries} = data;
+const PackingListEntrieGroupForProduct = ({
+  data,
+}: {
+  data: IPackingListEntrieGroupForProduct;
+}) => {
+  const { productId, productName, packingListEntries } = data;
   return (
     <Box>
-      <Heading as="h3" size="sm">{productName}</Heading>
-      <SimpleGrid columns={2} spacing={2}>
-        {packingListEntries.map((entry) => (
-          <Box key={entry.id}>
+      <Heading as="h3" size="sm">
+        {productName}
+      </Heading>
 
-            <Text>{entry.numberOfItems}</Text>
-            <Text>{entry.size?.label}</Text>
-          </Box>
-        ))}
-      </SimpleGrid>
+      {/* <SimpleGrid columns={2} spacing={2}> */}
+      {/* <Center> */}
+      <TableContainer>
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>Size</Th>
+              <Th>No. of items</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {packingListEntries.map((entry) => (
+              <Tr key={entry.id}>
+                <Td>{entry.size?.label}</Td>
+                <Td>{entry.numberOfItems}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+      {/* </SimpleGrid> */}
+      {/* </Center> */}
     </Box>
   );
-}
-
-
+};
 
 interface DistroEventDetailsForPlanningStateProps {
   packingListEntries: IPackingListEntry[];
@@ -54,14 +81,17 @@ const DistroEventDetailsForPlanningState = ({
   onRemoveItemFromPackingListClick,
   onEditItemOnPackingListClick,
 }: DistroEventDetailsForPlanningStateProps) => {
-
   // const packingListEntriesGroupedByProduct = _.groupBy(packingListEntries, (entry) => ({productId: entry.product.id, productName: entry.product.name}));
-  const packingListEntriesGroupedByProductId = _.groupBy(packingListEntries, (entry) => entry.product.id);
-  const packingListEntriesGroupedByProductIdAndName = Object.keys(packingListEntriesGroupedByProductId)
-  .map(k => ({
+  const packingListEntriesGroupedByProductId = _.groupBy(
+    packingListEntries,
+    (entry) => entry.product.id
+  );
+  const packingListEntriesGroupedByProductIdAndName = Object.keys(
+    packingListEntriesGroupedByProductId
+  ).map((k) => ({
     productId: k,
     productName: packingListEntriesGroupedByProductId[k]?.[0]?.product?.name,
-    packingListEntries: packingListEntriesGroupedByProductId[k]
+    packingListEntries: packingListEntriesGroupedByProductId[k],
   }));
 
   // console.log(packingListEntriesGroupedByProductIdAndName);
@@ -70,21 +100,25 @@ const DistroEventDetailsForPlanningState = ({
 
   return (
     <>
-    {/* packingListEntriesGroupedByProductIdAndName: {JSON.stringify(packingListEntriesGroupedByProductIdAndName)} */}
+      {/* packingListEntriesGroupedByProductIdAndName: {JSON.stringify(packingListEntriesGroupedByProductIdAndName)} */}
       <Flex w={[300, 400, 600]} direction="column" mb={4}>
         <Button onClick={() => onCopyPackingListFromPreviousEventsClick()}>
           Copy Packing List from Previous Event
         </Button>
         <Button my={2} onClick={() => onAddItemsClick()} colorScheme="blue">
-          Add New Packing List Entry
+          Select Products on List Entry
         </Button>
       </Flex>
-      <Heading size={"md"}>
-        Packing List:
-      </Heading>
+      <Heading size={"md"}>Packing List</Heading>
 
-     {packingListEntriesGroupedByProductIdAndName.map(packingListEntrieGroupForProduct => <PackingListEntrieGroupForProduct data={packingListEntrieGroupForProduct} />) }
-
+      {packingListEntriesGroupedByProductIdAndName.map(
+        (packingListEntrieGroupForProduct) => (
+          <PackingListEntrieGroupForProduct
+            key={packingListEntrieGroupForProduct.productId}
+            data={packingListEntrieGroupForProduct}
+          />
+        )
+      )}
 
       {/* {packingListEntries.map((item) => {
         return (
@@ -120,9 +154,6 @@ const DistroEventDetailsForPlanningState = ({
           </SimpleGrid>
         );
       })} */}
-
-
-
     </>
   );
 };
