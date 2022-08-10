1d0567712dafe6ffbdbcbed29ae99c80da998051
Daniel Spaude <daniel@spaude.de>
2022-08-10 16:26:05 +0200

    Add endpoint test for updateSelectedProductsForPackingListOfDistributionEvent

--- back/test/endpoint_tests/test_distribution_event.py
+++ back/test/endpoint_tests/test_distribution_event.py
@@ -17,3 +17,44 @@ def test_distribution_event_query(read_only_client, default_distribution_event):
     assert int(distribution_event["id"]) == expected_distribution_event["id"]
     assert distribution_event["name"] == expected_distribution_event["name"]
     assert distribution_event["state"] == DistributionEventState.Planning.name
+
+
+def test_update_selected_products_for_distribution_event_packing_list(
+    client, default_distribution_event, default_product, default_size, another_size
+):
+    mutation = f"""mutation {{
+    updateSelectedProductsForDistributionEventPackingList(
+      distributionEventId: {default_distribution_event['id']},
+      productIdsToAdd: [{default_product['id']}],
+      productIdsToRemove: []
+    )
+    }}"""
+
+    mutation_result = assert_successful_request(client, mutation)
+    assert mutation_result
+
+    query = f"""query {{
+                distributionEvent(id: {default_distribution_event['id']}) {{
+                    packingListEntries {{
+                        product {{ id }}
+                        size {{ id }}
+                        numberOfItems
+                    }}
+                }}
+            }}"""
+
+    queried_event = assert_successful_request(client, query)
+    assert queried_event == {
+        "packingListEntries": [
+            {
+                "product": {"id": str(default_product["id"])},
+                "size": {"id": str(default_size["id"])},
+                "numberOfItems": 0,
+            },
+            {
+                "product": {"id": str(default_product["id"])},
+                "size": {"id": str(another_size["id"])},
+                "numberOfItems": 0,
+            },
+        ],
+    }
