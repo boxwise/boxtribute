478a2b484c5705d406b797df1fd45d1d0bc5b065
Daniel Spaude <daniel@spaude.de>
2022-08-14 11:56:36 +0200

    Mobile Distro Prototyping: FE - Distro Event Return Tracking View - WIP

--- react/src/views/Distributions/DistrosReturnTrackingView/DistrosReturnTrackingView.tsx
+++ react/src/views/Distributions/DistrosReturnTrackingView/DistrosReturnTrackingView.tsx
@@ -2,9 +2,12 @@ import { Box } from "@chakra-ui/react";
 import { useSearchParams } from "react-router-dom";
 
 const DistrosReturnTrackingView = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
+  const distroEventIdsForReturnTracking = searchParams.getAll("distroEventIds[]");
+
   return (
     <Box>
+        {JSON.stringify(distroEventIdsForReturnTracking)}
       <h1>DistrosReturnTrackingView</h1>
     </Box>
   );
