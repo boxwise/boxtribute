6db65906375e1c04dc6aec80a80ad477c0e9a116
Daniel Spaude <daniel@spaude.de>
2022-08-14 11:49:41 +0200

    Mobile Distro Prototyping: FE - sync the current tab of DistroDashboard in url

--- react/src/views/Distributions/DistrosDashboardView/DistrosDashboardView.tsx
+++ react/src/views/Distributions/DistrosDashboardView/DistrosDashboardView.tsx
@@ -8,18 +8,27 @@ import {
   ModalContent,
   ModalFooter,
   ModalHeader,
-  ModalOverlay, Tab, TabList, TabPanel, TabPanels, Tabs, useDisclosure
+  ModalOverlay,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  useDisclosure,
 } from "@chakra-ui/react";
 import APILoadingIndicator from "components/APILoadingIndicator";
-import { useCallback, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useCallback, useEffect, useState } from "react";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import {
   DistributionEventsForBaseQuery,
-  DistributionEventsForBaseQueryVariables
+  DistributionEventsForBaseQueryVariables,
 } from "types/generated/graphql";
 import { z } from "zod";
 import { DISTRIBUTION_EVENTS_FOR_BASE_ID } from "../queries";
-import { DistributionEventDetails, DistributionEventDetailsSchema } from "../types";
+import {
+  DistributionEventDetails,
+  DistributionEventDetailsSchema,
+} from "../types";
 import DistroEventsCalendarContainer from "./components/DistroEventsCalendar/DistroEventsCalendarContainer";
 import DistroEventsStatistics from "./components/DistroEventsStatistics";
 import DistributionList from "./components/DistributionList";
@@ -27,6 +36,27 @@ import DistributionListForReturnTracking from "./components/DistributionListForR
 
 const DistrosDashboardView = () => {
   const baseId = useParams<{ baseId: string }>().baseId;
+
+  const navigate = useNavigate();
+  // TODO: consider to extract this out into custom hook (if it really makes sense!)
+  const [currentTabIndex, setCurrentTabIndex] = useState(0);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const tabIndexSearchParam = searchParams.get("tab");
+  const tabIndexSearchParamAsNumber = parseInt(tabIndexSearchParam || "0");
+  // searchParams.get("tab") && setCurrentTabIndex(parseInt(searchParams.get("tab")!));
+
+  // TODO: consider to flip the sync logic for the tab index search param:
+  // instead of setting the search url param and then register on changes on it,
+  // set the tab index and then sync the url search param based on it.
+  // Also then set the initial state value of currentTabIndex based on the search param.
+  // This way, even in (unlikely) cases of incompatible browsers etc, the switching of the tab
+  // will be supported via the UI.
+  useEffect(() => {
+    if (tabIndexSearchParamAsNumber !== currentTabIndex) {
+      setCurrentTabIndex(tabIndexSearchParamAsNumber!);
+    }
+  }, [currentTabIndex, tabIndexSearchParamAsNumber]);
 
   const { data, error, loading } = useQuery<
     DistributionEventsForBaseQuery,
@@ -38,20 +68,16 @@ const DistrosDashboardView = () => {
     },
   });
 
-  const navigate = useNavigate();
-
-
   const onGoToDistroEventViewHandler = useCallback(
     (distroEventId: string) =>
       navigate(`/bases/${baseId}/distributions/events/${distroEventId}`),
     [baseId, navigate]
   );
 
-
   const calendarEventDetailsModalState = useDisclosure();
-  const [selectedEvent, setSelectedEvent] = useState<DistributionEventDetails | undefined>()
-
-
+  const [selectedEvent, setSelectedEvent] = useState<
+    DistributionEventDetails | undefined
+  >();
 
   if (loading) return <APILoadingIndicator />;
   // TODO: add error logging here
@@ -65,7 +91,10 @@ const DistrosDashboardView = () => {
 
   return (
     <Box>
-      <Tabs>
+      <Tabs
+        index={currentTabIndex}
+        onChange={(param) => setSearchParams({ tab: param.toString() })}
+      >
         <TabList>
           <Tab>Distributions</Tab>
           <Tab>Track Returns</Tab>
@@ -74,39 +103,68 @@ const DistrosDashboardView = () => {
         </TabList>
 
         <TabPanels>
-        <TabPanel>
-            <DistributionList distributionEventsData={parsedDistributionEventsData} />
+          <TabPanel>
+            <DistributionList
+              distributionEventsData={parsedDistributionEventsData}
+            />
           </TabPanel>
           <TabPanel>
-            <DistributionListForReturnTracking distributionEventsData={parsedDistributionEventsData} />
+            <DistributionListForReturnTracking
+              distributionEventsData={parsedDistributionEventsData}
+            />
           </TabPanel>
           <TabPanel>
-            {selectedEvent &&
-            <Modal isOpen={calendarEventDetailsModalState.isOpen} onClose={calendarEventDetailsModalState.onClose}>
-              <ModalOverlay />
-              <ModalContent>
-                <ModalHeader><>{selectedEvent.distributionSpot.name} - {(new Date(selectedEvent.plannedStartDateTime)).toLocaleDateString('en-US')}</></ModalHeader>
-                <ModalCloseButton />
-                <ModalBody>
+            {selectedEvent && (
+              <Modal
+                isOpen={calendarEventDetailsModalState.isOpen}
+                onClose={calendarEventDetailsModalState.onClose}
+              >
+                <ModalOverlay />
+                <ModalContent>
+                  <ModalHeader>
+                    <>
+                      {selectedEvent.distributionSpot.name} -{" "}
+                      {new Date(
+                        selectedEvent.plannedStartDateTime
+                      ).toLocaleDateString("en-US")}
+                    </>
+                  </ModalHeader>
+                  <ModalCloseButton />
+                  <ModalBody>
                     <Box>Spot: {selectedEvent.distributionSpot.name}</Box>
                     <Box>State: {selectedEvent.state}</Box>
-                </ModalBody>
+                  </ModalBody>
 
-                <ModalFooter>
-                  <Button colorScheme="blue" mr={3} onClick={calendarEventDetailsModalState.onClose}>
-                    Close
-                  </Button>
-                  <Button variant="ghost" onClick={() => onGoToDistroEventViewHandler(selectedEvent.id)}>Go to Event Details</Button>
-                </ModalFooter>
-              </ModalContent>
-            </Modal>
-          }
+                  <ModalFooter>
+                    <Button
+                      colorScheme="blue"
+                      mr={3}
+                      onClick={calendarEventDetailsModalState.onClose}
+                    >
+                      Close
+                    </Button>
+                    <Button
+                      variant="ghost"
+                      onClick={() =>
+                        onGoToDistroEventViewHandler(selectedEvent.id)
+                      }
+                    >
+                      Go to Event Details
+                    </Button>
+                  </ModalFooter>
+                </ModalContent>
+              </Modal>
+            )}
 
             <DistroEventsCalendarContainer
               distributionEvents={parsedDistributionEventsData}
               onClickOnDistroEvent={function (distroEventId: string): void {
-                setSelectedEvent(parsedDistributionEventsData.find(distroEvent => distroEvent.id === distroEventId))
-                calendarEventDetailsModalState.onOpen()
+                setSelectedEvent(
+                  parsedDistributionEventsData.find(
+                    (distroEvent) => distroEvent.id === distroEventId
+                  )
+                );
+                calendarEventDetailsModalState.onOpen();
               }}
             />
           </TabPanel>
