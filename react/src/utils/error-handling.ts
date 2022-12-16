590dbe95fcae718d9caed02767670e138ba92e0b
HaGuesto <hans@boxtribute.org>
2022-12-16 13:22:07 +0100

    add Callback to triggerError

--- react/src/utils/error-handling.ts
+++ react/src/utils/error-handling.ts
@@ -1,31 +1,35 @@
-import { useToast, UseToastOptions, ToastPositionWithLogical } from "@chakra-ui/react";
+/* eslint-disable no-console */
+import { useToast, UseToastOptions } from "@chakra-ui/react";
+import { useCallback } from "react";
 
-export interface ITriggerErrorProps extends UseToastOptions{
-    message: string;
-    userMessage ?: string;
-    statusCode ?: string;
-  }
+export interface ITriggerErrorProps extends UseToastOptions {
+  message: string;
+  userMessage?: string;
+  statusCode?: string;
+}
 
 export const useErrorHandling = () => {
-    const toast = useToast();
+  const toast = useToast();
 
-    const triggerError = ({message, userMessage, statusCode}: ITriggerErrorProps) => {
-        if (statusCode) console.error(`[${statusCode}] ${message}`)
-        else console.error(`${message}`)
+  const triggerError = useCallback(
+    ({ message, userMessage, statusCode }: ITriggerErrorProps) => {
+      if (statusCode) console.error(`[${statusCode}] ${message}`);
+      else console.error(`${message}`);
 
-        toast({
-            duration: 5000,
-            isClosable: true,
-            position: "top",
-            variant: "subtle",
-            status: "error",
-            title: "Error",
-            description: userMessage || message,
-        });
-    }
+      toast({
+        duration: 5000,
+        isClosable: true,
+        position: "top",
+        variant: "subtle",
+        status: "error",
+        title: "Error",
+        description: userMessage || message,
+      });
+    },
+    [toast],
+  );
 
-        return {
-        triggerError,
-        };
-
+  return {
+    triggerError,
+  };
 };
