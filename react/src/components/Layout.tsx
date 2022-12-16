22056987cdf0ed4e62c22aad3a2b476bbd1a3a09
HaGuesto <hans@boxtribute.org>
2022-12-16 12:22:53 +0100

    fix format

--- react/src/components/Layout.tsx
+++ react/src/components/Layout.tsx
@@ -3,13 +3,13 @@ import { Container } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import HeaderMenuContainer from "./HeaderMenu/HeaderMenuContainer";
 
-const Layout = () => {
+function Layout() {
   return (
     <Container maxWidth="container.xl">
       <HeaderMenuContainer />
       <Outlet />
     </Container>
   );
-};
+}
 
 export default Layout;
