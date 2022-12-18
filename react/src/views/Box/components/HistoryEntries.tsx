b6d984d1a1010284fac2ada9ad87f9bdb0258d87
Vahid Bazzaz <vahidh@gmail.com>
2022-12-18 11:43:37 +0330

    show the last box history entry on the  box view

---
+++ react/src/views/Box/components/HistoryEntries.tsx
@@ -0,0 +1,60 @@
+import { ListItem, ListIcon, List, Stack, Flex } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+import { MdCheckCircle, MdSettings, MdHistory } from "react-icons/md";
+import { HistoryEntry } from "types/generated/graphql";
+
+interface IHistoryEntriesProps {
+  data: HistoryEntry[];
+  total: number | undefined;
+}
+function HistoryEntries({ data, total }: IHistoryEntriesProps) {
+  // sort history entries in the descending order
+  // this can be optimize in the BE by changing the defualt query order or adding sort option
+  const historyData = [...data].sort(
+    (a, b) => new Date(b.changeDate).getTime() - new Date(a.changeDate).getTime(),
+  );
+
+  // show different icons for changed / created records also default history for the rest
+  const getHistoryIcon = (changes: string): IconType => {
+    if (changes.includes("created")) {
+      return MdSettings;
+    }
+    if (changes.includes("changed")) {
+      return MdCheckCircle;
+    }
+    return MdHistory;
+  };
+
+  return (
+    <Stack py={2} px={2} alignContent="center">
+      <Flex alignContent="center" direction="column">
+        <List spacing={1}>
+          {historyData.slice(0, total).map((historyEntry) => (
+            <ListItem key={historyEntry.id}>
+              <ListIcon as={getHistoryIcon(historyEntry?.changes)} h={5} w={5} color="green.500" />
+              <b>{historyEntry?.user?.name}</b> on{" "}
+              <b>
+                {new Date(historyEntry?.changeDate).toLocaleDateString("en-GB", {
+                  weekday: "long",
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                })}
+                ,{" "}
+                {new Date(historyEntry?.changeDate).toLocaleTimeString("en-GB", {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}{" "}
+              </b>
+              {historyEntry?.changes.endsWith(";")
+                ? historyEntry?.changes.slice(0, -1)
+                : historyEntry?.changes}
+            </ListItem>
+          ))}
+        </List>
+      </Flex>
+    </Stack>
+  );
+}
+
+export default HistoryEntries;
