3f5a4571a5f10535b49b8ee2f795bf3519d462d4
Philipp Metzner <pmetzner@freenet.de>
2022-12-20 11:02:17 +0100

    Fix user organisation solver
    - remove returning current user's organisation instead of the requested
      user's one
    - if the requested user is different from the current one, we don't
      have any way to find out that user's organisation (we'd have to
      request that data from Auth0); hence return null for now
    - return null for god user's organisation: For the god user,
      CurrentUser.organisation_id is None, hence peewee throws an exception
      trying to find an organisation by that ID
    - allow any user to fetch other users' data through `user` query
      (albeit the email field is only visible to the user themselves and
      the god users)

--- back/test/endpoint_tests/test_user.py
+++ back/test/endpoint_tests/test_user.py
@@ -1,7 +1,8 @@
+from auth import create_jwt_payload
 from utils import assert_successful_request
 
 
-def test_user_query(read_only_client, another_user, default_organisation):
+def test_user_query(read_only_client, default_user, another_user, default_organisation):
     # Test case 10.1.2
     test_id = another_user["id"]
     query = f"""query User {{
@@ -26,6 +27,24 @@ def test_user_query(read_only_client, another_user, default_organisation):
     assert queried_user["lastLogin"][:-6] == another_user["last_login"].isoformat()
     assert [int(b["id"]) for b in queried_user["bases"]] == [1]
     assert int(queried_user["organisation"]["id"]) == default_organisation["id"]
+
+    user_id = default_user["id"]
+    query = f"""query {{
+                user(id: {user_id}) {{
+                    id
+                    organisation {{ id }} }} }}"""
+    queried_user = assert_successful_request(read_only_client, query)
+    assert queried_user == {"id": str(user_id), "organisation": None}
+
+
+def test_user_query_for_god_user(read_only_client, mocker, god_user):
+    user_id = god_user["id"]
+    mocker.patch("jose.jwt.decode").return_value = create_jwt_payload(
+        permissions=["*"], user_id=user_id, base_ids=[]
+    )
+    query = f"query {{ user (id: {user_id}) {{ organisation {{ id }} }} }}"
+    user = assert_successful_request(read_only_client, query)
+    assert user == {"organisation": None}
 
 
 def test_users_query(read_only_client):
