4d9c1717380358408b3d25669af96de0335cb189
HaGuesto <hans@boxtribute.org>
2023-01-05 23:41:05 +0100

    single out testing mocks

---
+++ react/src/mocks/boxes.ts
@@ -0,0 +1,15 @@
+import { product1 } from "./products";
+import { size1 } from "./sizeRanges";
+import { location1 } from "./locations";
+import { tag1 } from "./tags";
+
+export const box123 = {
+  labelIdentifier: "123",
+  product: product1,
+  size: size1,
+  numberOfItems: 62,
+  location: location1,
+  tags: [tag1],
+  comment: "Test",
+  __typename: "Box",
+};
---
+++ react/src/mocks/products.ts
@@ -0,0 +1,29 @@
+import { ProductGender } from "types/generated/graphql";
+import { sizeRange1, sizeRange2 } from "./sizeRanges";
+
+export const product1 = {
+  id: "1",
+  name: "Long Sleeves",
+  gender: ProductGender.Women,
+  category: {
+    name: "Tops",
+    __typename: "ProductCategory",
+  },
+  sizeRange: sizeRange1,
+  __typename: "Product",
+};
+
+export const products = [
+  product1,
+  {
+    id: "2",
+    name: "Winter Jackets",
+    gender: ProductGender.Men,
+    category: {
+      name: "Jackets / Outerwear",
+      __typename: "ProductCategory",
+    },
+    sizeRange: sizeRange2,
+    __typename: "Product",
+  },
+];
