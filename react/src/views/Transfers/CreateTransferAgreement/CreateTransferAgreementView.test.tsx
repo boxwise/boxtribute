8db88945ec7aadde2881b68ae96399b063ada794
Vahid Bazzaz <vahidh@gmail.com>
2023-01-26 00:31:17 +0330

    WIP: add more tests - complete test case 4.1.1 - complete test case 4.1.4 - add first draft test case 4.1.2 & 4.1.3

--- react/src/views/Transfers/CreateTransferAgreement/CreateTransferAgreementView.test.tsx
+++ react/src/views/Transfers/CreateTransferAgreement/CreateTransferAgreementView.test.tsx
@@ -1,13 +1,12 @@
-/* eslint-disable */
 import { GraphQLError } from "graphql";
 import "@testing-library/jest-dom";
-import { screen, render, waitFor } from "tests/test-utils";
+import { screen, render } from "tests/test-utils";
 import userEvent from "@testing-library/user-event";
+import { organisation1, organisations } from "mocks/oraganisations";
+import { assertOptionsInSelectField, selectOptionInSelectField } from "tests/helpers";
 import CreateTransferAgreementView, {
   ALL_ORGS_AND_BASES_QUERY,
 } from "./CreateTransferAgreementView";
-import { organisation1, organisations } from "mocks/oraganisations";
-import { assertOptionsInSelectField } from "tests/helpers";
 
 const initialQuery = {
   request: {
@@ -16,8 +15,18 @@ const initialQuery = {
   },
   result: {
     data: {
-      organisations: organisations,
+      organisations,
     },
+  },
+};
+
+const initialQueryNetworkError = {
+  request: {
+    query: ALL_ORGS_AND_BASES_QUERY,
+    variables: {},
+  },
+  result: {
+    errors: [new GraphQLError("Error!")],
   },
 };
 
@@ -46,6 +55,86 @@ it("4.1.1 - Initial load of Page", async () => {
   expect(screen.getByText(/boxaid bases/i)).toBeInTheDocument();
 
   // Test case 4.1.1.2 - Content: Displays Partner Orgs Select Options
-  await assertOptionsInSelectField(user, /Partner Organ/i, [/Care/i], title);
-  // await waitFor(() => expect(screen.getByText(/BoxCare/)).toBeInTheDocument());
+  await assertOptionsInSelectField(user, /partner organisation/i, [/boxcare/i], title);
+  // Test case 4.1.1.3	- Content: Displays Partner Bases Select Options When Partner Organisation Selected
+  await selectOptionInSelectField(user, /partner organisation/i, "BoxCare");
+  expect(await screen.findByText("BoxCare")).toBeInTheDocument();
+  const selectBaseDropDown = screen.getByRole("combobox", { name: /partner bases/i });
+  expect(selectBaseDropDown).toBeInTheDocument();
+  await user.click(selectBaseDropDown);
+  expect(await screen.findByText("Thessaloniki")).toBeInTheDocument();
+  expect(await screen.findByText("Samos")).toBeInTheDocument();
+  const selectedBase = screen.getByRole("button", { name: /samos/i });
+  expect(selectedBase).toBeInTheDocument();
+  await user.click(selectedBase);
+  // Test case 4.1.1.3	- Content: Display Source Organisation name on the label
+  expect(screen.getByText(/boxaid bases/i)).toBeInTheDocument();
+});
+
+// Test case 4.1.2
+it("4.1.2 - Input Validations", async () => {
+  // const user = userEvent.setup();
+  render(<CreateTransferAgreementView />, {
+    routePath: "/transfers/agreements/create",
+    initialUrl: "/transfers/agreements/create",
+    mocks: [initialQuery],
+    addTypename: true,
+    globalPreferences: {
+      dispatch: jest.fn(),
+      globalPreferences: {
+        selectedOrganisationId: organisation1.id,
+        availableBases: organisation1.bases,
+      },
+    },
+  });
+
+  expect(screen.getByTestId("loading-indicator")).toBeInTheDocument();
+
+  const title = await screen.findByRole("heading", { name: "New Transfer Agreement" });
+  expect(title).toBeInTheDocument();
+});
+
+// Test case 4.1.3
+it("4.1.3 - Click on Submit Button", async () => {
+  // const user = userEvent.setup();
+  render(<CreateTransferAgreementView />, {
+    routePath: "/transfers/agreements/create",
+    initialUrl: "/transfers/agreements/create",
+    mocks: [initialQuery],
+    addTypename: true,
+    globalPreferences: {
+      dispatch: jest.fn(),
+      globalPreferences: {
+        selectedOrganisationId: organisation1.id,
+        availableBases: organisation1.bases,
+      },
+    },
+  });
+
+  expect(screen.getByTestId("loading-indicator")).toBeInTheDocument();
+
+  const title = await screen.findByRole("heading", { name: "New Transfer Agreement" });
+  expect(title).toBeInTheDocument();
+});
+
+// Test case 4.1.4
+it("4.1.4 - Failed to Fetch Initial Data", async () => {
+  // const user = userEvent.setup();
+  render(<CreateTransferAgreementView />, {
+    routePath: "/transfers/agreements/create",
+    initialUrl: "/transfers/agreements/create",
+    mocks: [initialQueryNetworkError],
+    addTypename: true,
+    globalPreferences: {
+      dispatch: jest.fn(),
+      globalPreferences: {
+        selectedOrganisationId: organisation1.id,
+        availableBases: organisation1.bases,
+      },
+    },
+  });
+
+  // Test case 4.1.4.1 - No Partner Organisations and Bases Data
+  expect(await screen.findByText(/no partner organisation are available!/i)).toBeInTheDocument();
+  // screen.debug();
 });
