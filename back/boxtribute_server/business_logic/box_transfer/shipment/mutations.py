f191c553f7985c7f95e971d94fd18bd95698f196
Philipp Metzner <pmetzner@freenet.de>
2023-02-03 16:17:37 +0100

    Remove organisation-level authorization in shipment resolvers
    Use base-level authorization everywhere

--- back/boxtribute_server/business_logic/box_transfer/shipment/mutations.py
+++ back/boxtribute_server/business_logic/box_transfer/shipment/mutations.py
@@ -2,9 +2,7 @@ from ariadne import MutationType, convert_kwargs_to_snake_case
 from flask import g
 
 from ....authz import authorize
-from ....enums import TransferAgreementType
 from ....models.definitions.shipment import Shipment
-from ....models.definitions.transfer_agreement import TransferAgreement
 from .crud import cancel_shipment, create_shipment, send_shipment, update_shipment
 
 mutation = MutationType()
@@ -17,11 +15,6 @@ def resolve_create_shipment(*_, creation_input):
         permission="shipment:create",
         base_ids=[creation_input["source_base_id"], creation_input["target_base_id"]],
     )
-    agreement = TransferAgreement.get_by_id(creation_input["transfer_agreement_id"])
-    organisation_ids = [agreement.source_organisation_id]
-    if agreement.type == TransferAgreementType.Bidirectional:
-        organisation_ids.append(agreement.target_organisation_id)
-    authorize(organisation_ids=organisation_ids)
     return create_shipment(**creation_input, user=g.user)
 
 
@@ -29,11 +22,6 @@ def resolve_create_shipment(*_, creation_input):
 @convert_kwargs_to_snake_case
 def resolve_update_shipment(*_, update_input):
     shipment = Shipment.get_by_id(update_input["id"])
-    authorize(
-        permission="shipment:edit",
-        base_ids=[shipment.source_base_id, shipment.target_base_id],
-    )
-
     source_update_fields = [
         "prepared_box_label_identifiers",
         "removed_box_label_identifiers",
@@ -43,17 +31,22 @@ def resolve_update_shipment(*_, update_input):
         "received_shipment_detail_update_inputs",
         "lost_box_label_identifiers",
     ]
-    organisation_id = None
+    base_id = None
     if any([update_input.get(f) is not None for f in source_update_fields]):
-        # User must be member of organisation that created the shipment
-        organisation_id = shipment.source_base.organisation_id
+        # User must be member of base that serves as shipment source
+        base_id = shipment.source_base_id
     elif any([update_input.get(f) is not None for f in target_update_fields]):
-        # User must be member of organisation that is supposed to receive the shipment
-        organisation_id = shipment.target_base.organisation_id
+        # User must be member of base that is supposed to receive the shipment
+        base_id = shipment.target_base_id
 
-    if organisation_id is None:
+    if base_id is None:
+        authorize(
+            permission="shipment:edit",
+            base_ids=[shipment.source_base_id, shipment.target_base_id],
+        )
         return shipment  # no update arguments provided
-    authorize(organisation_id=organisation_id)
+
+    authorize(permission="shipment:edit", base_id=base_id)
 
     return update_shipment(**update_input, user=g.user)
 
@@ -65,21 +58,11 @@ def resolve_cancel_shipment(*_, id):
         permission="shipment:edit",
         base_ids=[shipment.source_base_id, shipment.target_base_id],
     )
-    authorize(
-        organisation_ids=[
-            shipment.transfer_agreement.source_organisation_id,
-            shipment.transfer_agreement.target_organisation_id,
-        ]
-    )
     return cancel_shipment(id=id, user=g.user)
 
 
 @mutation.field("sendShipment")
 def resolve_send_shipment(*_, id):
     shipment = Shipment.get_by_id(id)
-    authorize(
-        permission="shipment:edit",
-        base_ids=[shipment.source_base_id, shipment.target_base_id],
-    )
-    authorize(organisation_id=shipment.source_base.organisation_id)
+    authorize(permission="shipment:edit", base_id=shipment.source_base_id)
     return send_shipment(id=id, user=g.user)
