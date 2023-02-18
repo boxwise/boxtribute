e218a733e3b5c62fd80fb58ab9bb0a973a657b31
Vahid Bazzaz <vahidh@gmail.com>
2023-02-19 00:17:55 +0330

    WIP: first attempt to use react table
    Co-authored-by: HaGuesto <HaGuesto@users.noreply.github.com>

--- react/src/views/Transfers/ShipmentView/components/ShipmentTable.tsx
+++ react/src/views/Transfers/ShipmentView/components/ShipmentTable.tsx
@@ -1,4 +1,6 @@
 import { Table, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import { useMemo } from "react";
+import { useTable } from "react-table";
 import { Box } from "types/generated/graphql";
 
 interface IShipmentTablePros {
@@ -6,26 +8,69 @@ interface IShipmentTablePros {
 }
 
 function ShipmentTable({ boxes }: IShipmentTablePros) {
+  // Define columns
+  const columns = useMemo(
+    () => [
+      {
+        Header: "BOX #",
+        accessor: "labelIdentifier",
+      },
+      {
+        Header: "PRODUCT",
+        accessor: "product",
+      },
+      {
+        Header: "ITEMS",
+        accessor: "items",
+      },
+      {
+        Header: "",
+        accessor: "id",
+        // eslint-disable-next-line react/no-unstable-nested-components
+        // Cell: ({value}) => (<DeleteIcon onClick={handleDelete}  />)
+      },
+    ],
+    [],
+  );
+
+  const boxRows = boxes.map((box) => ({
+    labelIdentifier: box.labelIdentifier,
+    // eslint-disable-next-line max-len
+    product: `${`${box?.size?.label || ""} ` || ""}${`${box?.product?.gender || ""} ` || ""}${
+      box.product?.name
+    }`,
+    items: box.numberOfItems || 0,
+    id: parseInt(box.id, 10),
+  }));
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
+    columns,
+    data: boxRows,
+  });
+
   return (
     <TableContainer>
-      <Table size="sm" variant="simple">
+      <Table {...getTableProps()}>
         <Thead>
-          <Tr>
-            <Th>BOX #</Th>
-            <Th>PRODUCT</Th>
-            <Th isNumeric>ITEMS</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {boxes.map((box) => (
-            <Tr key={box.id}>
-              <Td>{box.labelIdentifier}</Td>
-              <Td>
-                {box.product?.sizeRange?.label} {box.product?.gender} {box.product?.name}
-              </Td>
-              <Td isNumeric>{box.numberOfItems}x</Td>
+          {headerGroups.map((headerGroup) => (
+            <Tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <Th {...column.getHeaderProps()}>{column.render("Header")}</Th>
+              ))}
             </Tr>
           ))}
+        </Thead>
+        <Tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <Tr {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <Td {...cell.getCellProps()}>{cell.render("Cell")}</Td>
+                ))}
+              </Tr>
+            );
+          })}
         </Tbody>
       </Table>
     </TableContainer>
