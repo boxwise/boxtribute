409ed61ad4bec80d679bb2c1ee9975bea117a074
HaGuesto <hans@boxtribute.org>
2023-02-23 23:53:53 +0100

    add QrReaderContainer to QrReaderView

--- react/src/views/NotFoundView/NotFoundView.tsx
+++ react/src/views/NotFoundView/NotFoundView.tsx
@@ -2,6 +2,8 @@ import { Navigate, useLocation } from "react-router-dom";
 
 function NotFoundView() {
   const location = useLocation();
+  // If the requested route was not found redirect to AutomaticBaseSwitcher
+  // to prepend /bases/:baseId
   return <Navigate to="/" replace state={{ origin: location.pathname }} />;
 }
 
