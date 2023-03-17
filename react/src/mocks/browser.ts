4656372b6e225acbfce69e6a12d8d95cc9b92196
HaGuesto <hans@boxtribute.org>
2023-03-17 08:22:13 +0100

    disable msw

--- react/src/mocks/browser.ts
+++ react/src/mocks/browser.ts
@@ -2,4 +2,4 @@
 import { setupWorker, SetupWorkerApi } from "msw";
 import { handlers } from "./handlers";
 // This configures a Service Worker with the given request handlers.
-export const worker: SetupWorkerApi = setupWorker(...handlers);
+// export const worker: SetupWorkerApi = setupWorker(...handlers);
