31b39bcf646295725ee1cf4217f8844137d461ee
Vahid Bazzaz <vahidh@gmail.com>
2023-03-24 18:34:50 +0330

    remove unused import

--- react/src/views/Transfers/ShipmentView/components/ShipmentReceivingCard.tsx
+++ react/src/views/Transfers/ShipmentView/components/ShipmentReceivingCard.tsx
@@ -1,15 +1,4 @@
-import {
-  Flex,
-  Box,
-  Stack,
-  Spacer,
-  Heading,
-  Wrap,
-  WrapItem,
-  VStack,
-  Text,
-  Center,
-} from "@chakra-ui/react";
+import { Flex, Box, Spacer, Heading, Wrap, WrapItem, VStack, Text, Center } from "@chakra-ui/react";
 import { BiPackage } from "react-icons/bi";
 import { BoxState, Shipment } from "types/generated/graphql";
 
