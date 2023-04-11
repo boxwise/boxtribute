3bcf5b837c9ae696b90ff44cbf04de8504ef9782
Philipp Metzner <pmetzner@freenet.de>
2023-04-11 19:17:10 +0200

    Flatten arguments into make_executable_schema
    Recommended usage acc. to note in
    https://ariadnegraphql.org/docs/0.18/resolvers#binding-resolvers

--- back/boxtribute_server/graph_ql/schema.py
+++ back/boxtribute_server/graph_ql/schema.py
@@ -13,29 +13,25 @@ from .scalars import date_scalar, datetime_scalar
 
 full_api_schema = make_executable_schema(
     definitions,
-    [
-        date_scalar,
-        datetime_scalar,
-        *query_types,
-        *mutation_types,
-        *object_types,
-        *enum_types,
-        *union_types,
-        *interface_types,
-    ],
+    date_scalar,
+    datetime_scalar,
+    *query_types,
+    *mutation_types,
+    *object_types,
+    *enum_types,
+    *union_types,
+    *interface_types,
     convert_names_case=True,
 )
 
 query_api_schema = make_executable_schema(
     query_api_definitions,
-    [
-        date_scalar,
-        datetime_scalar,
-        *query_types,
-        *object_types,
-        *enum_types,
-        *union_types,
-        *interface_types,
-    ],
+    date_scalar,
+    datetime_scalar,
+    *query_types,
+    *object_types,
+    *enum_types,
+    *union_types,
+    *interface_types,
     convert_names_case=True,
 )
