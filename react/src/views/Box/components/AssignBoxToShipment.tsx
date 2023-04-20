8dc1d487cf71c035838a369c9140ae38073e9d08
Vahid Bazzaz <vahidh@gmail.com>
2023-04-21 01:55:27 +0330

    make changes to assign, reassign and remove box

--- react/src/views/Box/components/AssignBoxToShipment.tsx
+++ react/src/views/Box/components/AssignBoxToShipment.tsx
@@ -6,19 +6,20 @@ import {
   IUnassignBoxToShipmentResult,
 } from "hooks/useAssignBoxesToShipment";
 import {
-  Stack,
   Text,
   FormControl,
   FormErrorMessage,
   Button,
   Flex,
-  Wrap,
-  WrapItem,
   Divider,
   Heading,
+  chakra,
+  IconButton,
+  Tooltip,
 } from "@chakra-ui/react";
 import { Select } from "chakra-react-select";
 import { IDropdownOption } from "components/Form/SelectField";
+import { AiFillInfoCircle } from "react-icons/ai";
 
 export interface IAssignBoxToShipmentProps {
   currentShipmentId: string | undefined;
@@ -39,8 +40,8 @@ function AssignBoxToShipment({
   const { createToast } = useNotification();
 
   const [selectedShipmentOption, setSelectedShipmentOption] = useState<IDropdownOption>({
-    value: currentShipmentId || "",
-    label: shipmentOptions.filter((opt) => opt.value === currentShipmentId)[0]?.label || "",
+    value: "",
+    label: "",
   });
 
   async function handleAssignBoxesToShipment() {
@@ -59,6 +60,7 @@ function AssignBoxToShipment({
             message: `Box has successfully assigned to the shipment ${selectedShipmentOption.value}.`,
             status: "success",
           });
+          setSelectedShipmentOption({ value: "", label: "" });
         }
       } else {
         const unassigneResult = await onUnassignBoxesToShipment(currentShipmentId);
@@ -81,6 +83,7 @@ function AssignBoxToShipment({
               message: `Box has successfully ressigned from shipment ${currentShipmentId} to the shipment ${selectedShipmentOption.value}`,
               status: "success",
             });
+            setSelectedShipmentOption({ value: "", label: "" });
           }
         }
       }
@@ -88,22 +91,20 @@ function AssignBoxToShipment({
   }
 
   async function handleUnassignBoxesToShipment() {
-    if (selectedShipmentOption) {
-      if (currentShipmentId) {
-        const unassigmentResult = await onUnassignBoxesToShipment(currentShipmentId);
-        if ((unassigmentResult?.error?.length || 0) > 0) {
-          triggerError({
-            // eslint-disable-next-line max-len
-            message: `Could not unassign the box from the shipment ${currentShipmentId}. Try again?`,
-            status: "error",
-          });
-        } else {
-          createToast({
-            message: `Box has successfully unassigned from the shipment ${currentShipmentId}`,
-            status: "success",
-          });
-          setSelectedShipmentOption({ value: "", label: "" });
-        }
+    if (currentShipmentId) {
+      const unassigmentResult = await onUnassignBoxesToShipment(currentShipmentId);
+      if ((unassigmentResult?.error?.length || 0) > 0) {
+        triggerError({
+          // eslint-disable-next-line max-len
+          message: `Could not unassign the box from the shipment ${currentShipmentId}. Try again?`,
+          status: "error",
+        });
+      } else {
+        createToast({
+          message: `Box has successfully unassigned from the shipment ${currentShipmentId}`,
+          status: "success",
+        });
+        setSelectedShipmentOption({ value: "", label: "" });
       }
     }
   }
@@ -115,72 +116,112 @@ function AssignBoxToShipment({
     return true;
   }, [selectedShipmentOption]);
 
+  const dropDownComponent = (
+    <FormControl isRequired mt={2}>
+      <Select
+        placeholder="Please select a shipment ..."
+        isSearchable
+        tagVariant="outline"
+        colorScheme="black"
+        useBasicStyles
+        focusBorderColor="blue.500"
+        chakraStyles={{
+          control: (provided) => ({
+            ...provided,
+            border: "2px",
+            borderRadius: "0",
+          }),
+        }}
+        options={shipmentOptions.filter((opt) => opt.value !== currentShipmentId)}
+        value={selectedShipmentOption}
+        onChange={setSelectedShipmentOption}
+      />
+      <FormErrorMessage>{false}</FormErrorMessage>
+    </FormControl>
+  );
+
+  const assignButton = (
+    <Button
+      isLoading={isAssignBoxesToShipmentLoading}
+      isDisabled={isSubmitButtonDisabled}
+      type="button"
+      colorScheme={!currentShipmentId ? "blue" : "green"}
+      mt={2}
+      size="md"
+      onClick={() => handleAssignBoxesToShipment()}
+    >
+      {currentShipmentId ? "Reassign" : "Assign"} Box
+    </Button>
+  );
+
   return (
-    <Flex alignItems="center" direction="column" alignContent="space-between">
-      <Stack direction="row" alignItems="flex-start" spacing={2}>
-        <Text>Assign this box to one of the following shipments under preparation:</Text>
-      </Stack>
-      <FormControl isRequired mt={2}>
-        <Select
-          placeholder="Please select a shipment ..."
-          isSearchable
-          tagVariant="outline"
-          colorScheme="black"
-          useBasicStyles
-          focusBorderColor="blue.500"
-          chakraStyles={{
-            control: (provided) => ({
-              ...provided,
-              border: "2px",
-              borderRadius: "0",
-            }),
-          }}
-          options={shipmentOptions}
-          value={selectedShipmentOption}
-          onChange={setSelectedShipmentOption}
-        />
-        <FormErrorMessage>{false}</FormErrorMessage>
-      </FormControl>
-
-      <Wrap>
-        <WrapItem>Box status will change to </WrapItem>
-        <WrapItem color="blackAlpha.500" fontWeight="bold">
-          Marked for Shipment
-        </WrapItem>
-      </Wrap>
-
-      {currentShipmentId && (
-        <Button
-          isLoading={isAssignBoxesToShipmentLoading}
-          isDisabled={isSubmitButtonDisabled}
-          type="button"
-          colorScheme="blue"
-          mt={2}
-          onClick={() => handleUnassignBoxesToShipment()}
-        >
-          Remove from Shipment
-        </Button>
-      )}
-      {currentShipmentId && (
-        <Flex alignItems="center" gap={4} width={["100%"]}>
-          <Divider border="2px" borderColor="black" borderRadius={0} />
-          <Heading as="h4" size="md" lineHeight="200%" sx={{ whiteSpace: "nowrap" }}>
-            OR
-          </Heading>
-          <Divider border="2px" borderColor="black" borderRadius={0} />
+    <>
+      {!currentShipmentId && (
+        <Flex alignItems="center" direction="column" alignContent="space-between">
+          <Text>Assign this box to one of the following shipments under preparation:</Text>
+          {dropDownComponent}
+          <Flex mt={3}>
+            <Text fontSize={16}>
+              Box status will change to{" "}
+              <chakra.span color="blue.500" fontWeight="bold">
+                Marked for Shipment
+              </chakra.span>
+            </Text>
+          </Flex>
+          {assignButton}
         </Flex>
       )}
-      <Button
-        isLoading={isAssignBoxesToShipmentLoading}
-        isDisabled={isSubmitButtonDisabled}
-        type="button"
-        colorScheme="blue"
-        mt={2}
-        onClick={() => handleAssignBoxesToShipment()}
-      >
-        {currentShipmentId ? "Reassign" : "Assign"} to Shipment
-      </Button>
-    </Flex>
+
+      {currentShipmentId && (
+        <Flex direction="column" alignContent="center" alignItems="center">
+          <Flex direction="row" gap={2} alignContent="center" alignItems="center" px={4}>
+            <Button
+              isLoading={isAssignBoxesToShipmentLoading}
+              type="button"
+              colorScheme="blue"
+              size="md"
+              mt={2}
+              aria-label="remove to shipment"
+              onClick={() => handleUnassignBoxesToShipment()}
+            >
+              Remove from Shipment
+            </Button>
+            <Tooltip
+              hasArrow
+              placement="bottom-start"
+              label="Remove a box from a shipment to return it to Instock status"
+              bg="black"
+            >
+              <IconButton
+                isRound
+                icon={<AiFillInfoCircle size={24} />}
+                style={{ background: "white", color: "grey" }}
+                aria-label="remove from shipment"
+              />
+            </Tooltip>
+          </Flex>
+
+          <Flex alignItems="center" gap={3} width={["100%"]}>
+            <Divider border="2px" borderColor="black" borderRadius={0} />
+            <Heading
+              as="h4"
+              fontSize={18}
+              fontWeight="bold"
+              lineHeight="200%"
+              sx={{ whiteSpace: "nowrap" }}
+            >
+              OR
+            </Heading>
+            <Divider border="2px" borderColor="black" borderRadius={0} />
+          </Flex>
+          <Flex direction="column" ml={4} mr={4}>
+            <Text>Assign box to a different shipment under preparation:</Text>
+            {dropDownComponent}
+            {assignButton}
+          </Flex>
+        </Flex>
+      )}
+    </>
   );
 }
 
