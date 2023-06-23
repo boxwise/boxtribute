e8822659f1949802d5d92d0a7f73d5edef790d68
HaGuesto <hans@boxtribute.org>
2023-06-23 08:40:23 +0200

    add submission tests

--- react/src/views/QrReader/QrReaderMultiBoxAssignToShipment.test.tsx
+++ react/src/views/QrReader/QrReaderMultiBoxAssignToShipment.test.tsx
@@ -1,7 +1,7 @@
 import { GraphQLError } from "graphql";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
-import { screen, render } from "tests/test-utils";
+import { screen, render, waitFor } from "tests/test-utils";
 import { useAuth0 } from "@auth0/auth0-react";
 import { QrReaderScanner } from "components/QrReader/components/QrReaderScanner";
 import { mockMatchMediaQuery } from "mocks/functions";
@@ -15,6 +15,7 @@ import { BoxState, ShipmentState } from "types/generated/graphql";
 import { cache } from "queries/cache";
 import { generateMockShipment } from "mocks/shipments";
 import { selectOptionInSelectField } from "tests/helpers";
+import { ASSIGN_BOX_TO_SHIPMENT } from "hooks/useAssignBoxesToShipment";
 import QrReaderView from "./QrReaderView";
 
 // extracting a cacheObject to reset the cache correctly later
@@ -60,6 +61,30 @@ const mockShipmentsQuery = ({
               shipments: [generateMockShipment({ state, iAmSource })],
             },
         errors: graphQlError ? [new GraphQLError("Error!")] : undefined,
+      },
+  error: networkError ? new Error() : undefined,
+});
+
+const mockAssignToShipmentMutation = ({
+  networkError = false,
+  graphQlError = false,
+  errorCode = "",
+  labelIdentifiers = ["123"],
+}) => ({
+  request: {
+    query: ASSIGN_BOX_TO_SHIPMENT,
+    variables: {
+      id: "1",
+      labelIdentifiers,
+    },
+  },
+  result: networkError
+    ? undefined
+    : {
+        data: graphQlError ? null : { updateShipmentWhenPreparing: generateMockShipment({}) },
+        errors: graphQlError
+          ? [new GraphQLError("Error!", { extensions: { code: errorCode } })]
+          : undefined,
       },
   error: networkError ? new Error() : undefined,
 });
@@ -191,6 +216,180 @@ it("3.4.5.1 - There are boxes in the list, but the state of some is not InStock"
 
   // click link to remove all not InStock boxes
   await user.click(screen.getByText(/click here to remove all non instock boxes from the list\./i));
+  expect(await screen.findByText(/boxes selected: 0/i)).toBeInTheDocument();
+  expect(screen.queryByRole("alert")).not.toBeInTheDocument();
+  expect(screen.queryByRole("button", { name: /assign all/i })).not.toBeInTheDocument();
+});
+
+const assignToShipmentMutationTests = [
+  {
+    name: "3.4.5.7 - Mutation to assign boxes to shipment returns Network Error",
+    mocks: [
+      mockSuccessfulQrQuery({ hash: "InStockBox", labelIdentifier: "123" }),
+      mockShipmentsQuery({}),
+      mockAssignToShipmentMutation({ networkError: true }),
+    ],
+    toast: { isError: true, message: /Could not assign boxes to shipment/i },
+  },
+  {
+    name: "3.4.5.8 - Mutation to assign boxes to shipment returns General Server Error",
+    mocks: [
+      mockSuccessfulQrQuery({ hash: "InStockBox", labelIdentifier: "123" }),
+      mockShipmentsQuery({}),
+      mockAssignToShipmentMutation({ graphQlError: true, errorCode: "INTERNAL_SERVER_ERROR" }),
+    ],
+    toast: { isError: true, message: /Could not assign boxes to shipment/i },
+  },
+  {
+    name: "3.4.5.9 - Mutation to assign boxes to shipment returns Authorization Error",
+    mocks: [
+      mockSuccessfulQrQuery({ hash: "InStockBox", labelIdentifier: "123" }),
+      mockShipmentsQuery({}),
+      mockAssignToShipmentMutation({ graphQlError: true, errorCode: "FORBIDDEN" }),
+    ],
+    toast: { isError: true, message: /have the permissions to assign boxes to this shipment/i },
+  },
+  {
+    name: "3.4.5.10 - Mutation to assign boxes to shipment fails since shipment is not in the Preparing State anymore",
+    mocks: [
+      mockSuccessfulQrQuery({ hash: "InStockBox", labelIdentifier: "123" }),
+      mockShipmentsQuery({}),
+      mockAssignToShipmentMutation({ graphQlError: true, errorCode: "BAD_USER_INPUT" }),
+    ],
+    toast: { isError: true, message: /The shipment is not in the Preparing state/i },
+  },
+  {
+    name: "3.4.5.12 - Boxes are successfully assigned to shipment",
+    mocks: [
+      mockSuccessfulQrQuery({ hash: "InStockBox", labelIdentifier: "123" }),
+      mockShipmentsQuery({}),
+      mockAssignToShipmentMutation({}),
+    ],
+    toast: { isError: false, message: /1 Boxes were successfully assigned to the shipment/i },
+  },
+];
+
+assignToShipmentMutationTests.forEach(({ name, mocks, toast }) => {
+  it(name, async () => {
+    const user = userEvent.setup();
+    mockImplementationOfQrReader(mockedQrReader, "InStockBox", true, true);
+    render(<QrReaderView />, {
+      routePath: "/bases/:baseId/qrreader",
+      initialUrl: "/bases/1/qrreader",
+      mocks,
+      cache,
+      additionalRoute: "/bases/1/transfers/shipments/1",
+    });
+
+    // go to the MultiBox Tab
+    const multiBoxTab = await screen.findByRole("tab", { name: /multi box/i });
+    expect(multiBoxTab).toBeInTheDocument();
+    user.click(multiBoxTab);
+
+    // 3.4.5.5 - Query for shipments returns one or more shipments in preparing state
+    const assignToShipmentOption = await screen.findByTestId("AssignShipment");
+    await user.click(assignToShipmentOption);
+    await selectOptionInSelectField(
+      user,
+      undefined,
+      /thessaloniki - BoxCare/i,
+      /please select a shipment/i,
+    );
+
+    // The submit button is not yet shown
+    expect(screen.queryByRole("button", { name: /assign all/i })).not.toBeInTheDocument();
+
+    // Click a button to trigger the event of scanning a QR-Code in mockImplementationOfQrReader
+    const scanButton = await screen.findByTestId("ReturnScannedQr");
+    await user.click(scanButton);
+
+    // The submit button is shown
+    const submitButton = await screen.findByRole("button", { name: /assign all/i });
+    expect(submitButton).not.toBeDisabled();
+    await user.click(submitButton);
+
+    // toast shown
+    await waitFor(() =>
+      expect(toast.isError ? mockedTriggerError : mockedCreateToast).toHaveBeenCalledWith(
+        expect.objectContaining({
+          message: expect.stringMatching(toast.message),
+        }),
+      ),
+    );
+
+    if (!toast.isError) {
+      // forward to shipmentView
+      expect(
+        await screen.findByRole("heading", { name: "/bases/1/transfers/shipments/1" }),
+      ).toBeInTheDocument();
+    }
+  });
+});
+
+it("3.4.5.11 - One Box of two or more Boxes fail for the Assign boxes to shipment Mutation", async () => {
+  const user = userEvent.setup();
+  mockImplementationOfQrReader(mockedQrReader, "InStockBox1", true, true);
+  const { rerender } = render(<QrReaderView />, {
+    routePath: "/bases/:baseId/qrreader",
+    initialUrl: "/bases/1/qrreader",
+    mocks: [
+      mockSuccessfulQrQuery({ hash: "InStockBox1", labelIdentifier: "123" }),
+      mockSuccessfulQrQuery({ hash: "InStockBox2", labelIdentifier: "678" }),
+      mockShipmentsQuery({}),
+      mockAssignToShipmentMutation({ labelIdentifiers: ["123", "678"] }),
+    ],
+    cache,
+  });
+
+  // go to the MultiBox Tab
+  user.click(await screen.findByRole("tab", { name: /multi box/i }));
+  expect(await screen.findByText(/boxes selected: 0/i)).toBeInTheDocument();
+
+  // scan box 123
+  await user.click(await screen.findByTestId("ReturnScannedQr"));
+  expect(await screen.findByText(/boxes selected: 1/i)).toBeInTheDocument();
+
+  // scan box 678
+  mockImplementationOfQrReader(mockedQrReader, "InStockBox2", true, true);
+  rerender(<QrReaderView />);
+  await user.click(await screen.findByTestId("ReturnScannedQr"));
+  expect(await screen.findByText(/boxes selected: 2/i)).toBeInTheDocument();
+
+  // 3.4.5.5 - Query for shipments returns one or more shipments in preparing state
+  const assignToShipmentOption = await screen.findByTestId("AssignShipment");
+  await user.click(assignToShipmentOption);
+  await selectOptionInSelectField(
+    user,
+    undefined,
+    /thessaloniki - BoxCare/i,
+    /please select a shipment/i,
+  );
+
+  // The submit button is shown
+  const submitButton = await screen.findByRole("button", { name: /assign all/i });
+  expect(submitButton).not.toBeDisabled();
+  await user.click(submitButton);
+
+  // selected boxes reduced by one
+  expect(await screen.findByText(/boxes selected: 1/i)).toBeInTheDocument();
+
+  // toast shown
+  await waitFor(() =>
+    expect(mockedCreateToast).toHaveBeenCalledWith(
+      expect.objectContaining({
+        message: expect.stringMatching(/1 Boxes were successfully assigned to the shipment/i),
+      }),
+    ),
+  );
+
+  // Alert appears
+  expect(
+    await screen.findByText(/The following boxes were not assigned to the shipment/i),
+  ).toBeInTheDocument();
+  expect(screen.getByText(/678/i)).toBeInTheDocument();
+
+  // click link to remove all not failed boxes
+  await user.click(screen.getByText(/Click here to remove all failed boxes from the list/i));
   expect(await screen.findByText(/boxes selected: 0/i)).toBeInTheDocument();
   expect(screen.queryByRole("alert")).not.toBeInTheDocument();
   expect(screen.queryByRole("button", { name: /assign all/i })).not.toBeInTheDocument();
