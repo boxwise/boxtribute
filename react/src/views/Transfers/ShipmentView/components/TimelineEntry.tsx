b628c25437080d3d419a4bebcacd631db18c3ba6
Vahid Bazzaz <vahidh@gmail.com>
2023-07-14 00:17:18 +0330

    add circle bullet in the history timeline

--- react/src/views/Transfers/ShipmentView/components/TimelineEntry.tsx
+++ react/src/views/Transfers/ShipmentView/components/TimelineEntry.tsx
@@ -1,4 +1,5 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
+import TimelineBullet from "./TimelineBullet";
 
 export interface ITimelineEntryProps {
   content: string | undefined;
@@ -10,7 +11,7 @@ function TimelineEntry({ content, time }: ITimelineEntryProps) {
     <Flex flexDirection="column">
       <Flex>
         <Flex direction="row" w="100%">
-          <Box borderLeftColor="blue.500" borderLeftWidth={2} ml={10} />
+          <TimelineBullet />
           <Flex w="100%" backgroundColor="gray.100" boxShadow="sm" margin={2} rounded={4}>
             <Text mt={1} ml={1} flex={1} padding={1}>
               {content}
