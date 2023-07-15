fd63d3a077292090f6a21ff85772fd0f149ad986
Philipp Metzner <pmetzner@freenet.de>
2023-07-15 12:29:13 +0200

    Enable accessing InTransit/Receiving boxes for members of target base
    - querying Box.product will still raise an authz error for the target
      side but instead ShipmentDetail.sourceProduct must be used

--- back/boxtribute_server/business_logic/warehouse/box/queries.py
+++ back/boxtribute_server/business_logic/warehouse/box/queries.py
@@ -1,8 +1,11 @@
 from ariadne import QueryType
 
 from ....authz import authorize
+from ....enums import BoxState
 from ....models.definitions.box import Box
 from ....models.definitions.location import Location
+from ....models.definitions.shipment import Shipment
+from ....models.definitions.shipment_detail import ShipmentDetail
 
 query = QueryType()
 
@@ -15,5 +18,25 @@ def resolve_box(*_, label_identifier):
         .where(Box.label_identifier == label_identifier)
         .get()
     )
-    authorize(permission="stock:read", base_id=box.location.base_id)
+
+    if box.state_id in [BoxState.InTransit, BoxState.Receiving]:
+        # Users of both source and target base of the underlying shipment are allowed to
+        # view InTransit or Receiving boxes
+        detail = (
+            ShipmentDetail.select(Shipment)
+            .join(Shipment)
+            .where(
+                ShipmentDetail.box_id == box.id,
+                ShipmentDetail.removed_on.is_null(),
+                ShipmentDetail.received_on.is_null(),
+            )
+        ).get()
+        authz_kwargs = {
+            "base_ids": [detail.shipment.source_base_id, detail.shipment.target_base_id]
+        }
+
+    else:
+        authz_kwargs = {"base_id": box.location.base_id}
+
+    authorize(permission="stock:read", **authz_kwargs)
     return box
