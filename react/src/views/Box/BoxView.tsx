80dcccd412f11fedaeed0481e1505c49cda137ba
Vahid Bazzaz <vahidh@gmail.com>
2023-07-21 00:16:18 +0330

    revert change in boxview

--- react/src/views/Box/BoxView.tsx
+++ react/src/views/Box/BoxView.tsx
@@ -525,7 +525,10 @@ function BTBox() {
   const shipmentOptions: IDropdownOption[] = useMemo(
     () =>
       shipmentsQueryResult
-        ?.filter((shipment) => shipment.sourceBase.id === currentBaseId)
+        ?.filter(
+          (shipment) =>
+            shipment.state === ShipmentState.Preparing && shipment.sourceBase.id === currentBaseId,
+        )
         ?.map((shipment) => ({
           // eslint-disable-next-line max-len
           label: `${shipment.targetBase.name} - ${shipment.targetBase.organisation.name} (Shipment ${shipment.id})`,
