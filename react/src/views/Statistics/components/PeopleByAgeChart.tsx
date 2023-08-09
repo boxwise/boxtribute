0f373932e7f9c2e69e69f419fe6fd845f87aa1cf
MaikNeubert <maik.neubert@belegebox.de>
2023-08-09 19:58:48 +0200

    use HumanGender, adjust field names

--- react/src/views/Statistics/components/PeopleByAgeChart.tsx
+++ react/src/views/Statistics/components/PeopleByAgeChart.tsx
@@ -1,24 +1,25 @@
 import BarChartCenterAxis, { IBarChartCenterAxis } from "components/graphs/BarChartCenterAxis";
 import { random, uniq } from "lodash";
+import { HumanGender } from "types/generated/graphql";
 
-const chartData: { age: number; gender: string; people: number }[] = [];
+const chartData: { age: number; gender: string; count: number }[] = [];
 
 for (let i = 1; i < 50; i += 1) {
-  let current = { age: i, gender: "F", people: random(80, false) };
+  let current = { age: i, gender: HumanGender.Male, count: random(80, false) };
   chartData.push(current);
-  current = { age: i, gender: "M", people: random(60, false) };
+  current = { age: i, gender: HumanGender.Female, count: random(60, false) };
   chartData.push(current);
 }
 
 const dataXr = chartData
-  .filter((value) => value.gender === "M")
-  .map((element) => ({ x: element.people, y: element.age }));
+  .filter((value) => value.gender === HumanGender.Male)
+  .map((element) => ({ x: element.count, y: element.age }));
 const dataXl = chartData
-  .filter((value) => value.gender === "F")
-  .map((element) => ({ x: element.people, y: element.age }));
+  .filter((value) => value.gender === HumanGender.Female)
+  .map((element) => ({ x: element.count, y: element.age }));
 
-const height = 500;
-const width = 400;
+const height = 600;
+const width = 800;
 
 const chart = {
   labelY: "Age",
