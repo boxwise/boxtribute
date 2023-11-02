54a3a5893b27b5a6caa8edcf75f656f30b67b893
Philipp Metzner <pmetzner@freenet.de>
2023-11-02 12:47:32 +0100

    Rename RBP to 'product_category'
    More precise naming matching the model name (ProductCategory)

--- back/boxtribute_server/business_logic/core/product_category/queries.py
+++ back/boxtribute_server/business_logic/core/product_category/queries.py
@@ -8,11 +8,11 @@ query = QueryType()
 
 @query.field("productCategory")
 def resolve_product_category(*_, id):
-    authorize(permission="category:read")
+    authorize(permission="product_category:read")
     return ProductCategory.get_by_id(id)
 
 
 @query.field("productCategories")
 def resolve_product_categories(*_):
-    authorize(permission="category:read")
+    authorize(permission="product_category:read")
     return ProductCategory.select()
