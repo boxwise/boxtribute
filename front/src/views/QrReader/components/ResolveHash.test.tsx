20b94e7e3311fbaed6b2645b47bf82222abfa90b
HaGuesto <hans@boxtribute.org>
2023-11-16 12:44:23 +0100

    add tests for fails

--- front/src/views/QrReader/components/ResolveHash.test.tsx
+++ front/src/views/QrReader/components/ResolveHash.test.tsx
@@ -1,6 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { QrReaderScanner } from "components/QrReader/components/QrReaderScanner";
-// import { GraphQLError } from "graphql";
+import { GraphQLError } from "graphql";
 import { useErrorHandling } from "hooks/useErrorHandling";
 import { generateMockBox } from "mocks/boxes";
 import { mockImplementationOfQrReader } from "mocks/components";
@@ -8,7 +8,7 @@ import { mockAuthenticatedUser } from "mocks/hooks";
 import { cache } from "queries/cache";
 import { GET_BOX_LABEL_IDENTIFIER_BY_QR_CODE } from "queries/queries";
 import { BoxState } from "types/generated/graphql";
-import { render, screen } from "tests/test-utils";
+import { render, screen, waitFor } from "tests/test-utils";
 import ResolveHash from "./ResolveHash";
 
 // extracting a cacheObject to reset the cache correctly later
@@ -86,37 +86,81 @@ SuccessfulQrScanningTests.forEach(({ name, hash, mocks, endRoute }) => {
       cache,
     });
 
+    expect(screen.queryByTestId("ReturnScannedQr")).not.toBeInTheDocument();
+
     expect(await screen.findByTestId("box-sections")).toBeInTheDocument();
+
+    expect(screen.queryByTestId("ReturnScannedQr")).not.toBeInTheDocument();
 
     expect(await screen.findByRole("heading", { name: endRoute })).toBeInTheDocument();
   });
 });
 
-// const mockFailedQrQuery = ({
-//   query = GET_BOX_LABEL_IDENTIFIER_BY_QR_CODE,
-//   hash = "",
-//   errorCode = "",
-//   resultQrCode = null as string | null | undefined,
-//   networkError = false,
-// }) => ({
-//   request: {
-//     query,
-//     variables: { qrCode: hash },
-//   },
-//   result: networkError
-//     ? undefined
-//     : {
-//         data:
-//           resultQrCode != null
-//             ? {
-//                 qrCode: {
-//                   _typename: "QrCode",
-//                   code: hash,
-//                   box: null,
-//                 },
-//               }
-//             : null,
-//         errors: [new GraphQLError("Error!", { extensions: { code: errorCode } })],
-//       },
-//   error: networkError ? new Error() : undefined,
-// });
+const mockFailedQrQuery = ({
+  query = GET_BOX_LABEL_IDENTIFIER_BY_QR_CODE,
+  hash = "",
+  errorCode = "",
+  networkError = false,
+}) => ({
+  request: {
+    query,
+    variables: { qrCode: hash },
+  },
+  result: networkError
+    ? undefined
+    : {
+        data: null,
+        errors: [new GraphQLError("Error!", { extensions: { code: errorCode } })],
+      },
+  error: networkError ? new Error() : undefined,
+});
+
+const FailedQrScanningTests = [
+  {
+    name: "3.4.8.4 - User scans QR code of different org with associated box",
+    hash: "QrWithBoxFromDifferentBase",
+    mocks: [mockFailedQrQuery({ hash: "QrWithBoxFromDifferentBase", errorCode: "FORBIDDEN" })],
+    toast: /You don't have permission to access this box/i,
+  },
+  {
+    name: "3.4.8.7 - User scans QR code where hash is not found in db",
+    hash: "NoBoxtributeQr",
+    mocks: [mockFailedQrQuery({ hash: "NoBoxtributeQr", errorCode: "BAD_USER_INPUT" })],
+    toast: /No box found for this QR code/i,
+  },
+  {
+    name: "3.4.8.8 - User scans QR code and server returns unexpected error",
+    hash: "QrServerFailure",
+    mocks: [mockFailedQrQuery({ hash: "QrServerFailure", errorCode: "SERVER_ERROR" })],
+    toast: /QR code lookup failed. Please wait a bit and try again./i,
+  },
+  {
+    name: "3.4.8.9 - User scans QR code and a network error is returned",
+    hash: "QrNetworkError",
+    mocks: [mockFailedQrQuery({ hash: "QrNetworkError", networkError: true })],
+    toast: /QR code lookup failed. Please wait a bit and try again./i,
+  },
+];
+
+FailedQrScanningTests.forEach(({ name, hash, mocks, toast }) => {
+  it(name, async () => {
+    mockImplementationOfQrReader(mockedQrReader, hash, true, true);
+    render(<ResolveHash />, {
+      routePath: "/bases/1/qrreader/:hash",
+      initialUrl: `/bases/1/qrreader/${hash}`,
+      mocks,
+      cache,
+    });
+
+    expect(await screen.findByTestId("ReturnScannedQr")).toBeInTheDocument();
+
+    // toast shown
+    await waitFor(() =>
+      expect(mockedTriggerError).toHaveBeenCalledWith(
+        expect.objectContaining({
+          message: expect.stringMatching(toast),
+        }),
+      ),
+    );
+  });
+});
