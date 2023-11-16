bbdebaee921d6d4dbd5bf25942e30b094c209c93
Vahid Bazzaz <vahidh@gmail.com>
2023-11-16 19:55:33 +0330

    add subtitle to action button

--- front/src/views/Boxes/components/ActionButtons.tsx
+++ front/src/views/Boxes/components/ActionButtons.tsx
@@ -1,9 +1,9 @@
-import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { Button, chakra, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { ChevronDownIcon } from "@chakra-ui/icons";
 
 interface ISelectButtonProps {
   label: string;
-  options: { label: string; value: string }[];
+  options: { label: string; value: string; subTitle?: string }[];
   onSelect: (value: string) => void;
 }
 
@@ -14,9 +14,14 @@ export function SelectButton({ label, options, onSelect }: ISelectButtonProps) {
         {label}
       </MenuButton>
       <MenuList>
-        {options.map(({ label: olabel, value }) => (
+        {options.map(({ label: olabel, value, subTitle }) => (
           <MenuItem key={`SelectButtonOption${value}`} onClick={() => onSelect(value)}>
-            {olabel}
+            <chakra.div>
+              <chakra.span>{olabel}</chakra.span>
+              {subTitle && (
+                <chakra.div style={{ fontSize: "0.8em", color: "gray" }}>ID: {subTitle}</chakra.div>
+              )}
+            </chakra.div>
           </MenuItem>
         ))}
       </MenuList>
