96165c469b1775cdcd7d38a6f015c3a966a84949
HaGuesto <hans@boxtribute.org>
2023-12-21 17:29:05 +0100

    move complete eslint from front to root

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,8 +6,36 @@ module.exports = {
   // ignore linting for these files
   ignorePatterns: ["dist", ".eslintrc.cjs", ".eslintrc"],
   // loads settings and rules from other eslint configs
-  extends: ["plugin:import/recommended", "plugin:import/typescript"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    // we generally want to follow the rules from airbnb as a base
+    // --> the extension of airbnb should be last so that it cannot be overriden by other configs
+    // airbnb uses the plugins eslint-plugin-import, eslint-plugin-react, eslint-plugin-react-hooks, eslint-plugin-jsx-a11y
+    // --> we need to install these plugins as devDependencies and load their recommended settings first
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "airbnb",
+    // put prettier config last so that it can override all formatting rules
+    "prettier",
+  ],
+  // regarding @typescript-eslint: https://typescript-eslint.io/linting/typed-linting/monorepos
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    // TODO add statviz to this
+    project: [
+      "./tsconfig.json",
+      "./front/tsconfig.json",
+      "./shared-components/tsconfig.json",
+    ],
+  },
+  plugins: ["@typescript-eslint"],
   settings: {
+    // --------- Import Plugin Settings ---------
     // This defines the parser to use for .ts and .tsx files
     "import/parsers": {
       "@typescript-eslint/parser": [".ts", ".tsx"],
@@ -24,19 +52,49 @@ module.exports = {
         // always try to resolve types
         alwaysTryTypes: true,
         // look here for tsconfig files
-        project: ["**/tsconfig*.json"],
+        project: [
+          "./tsconfig.json",
+          "./front/tsconfig.json",
+          "./shared-components/tsconfig.json",
+        ],
       },
     },
   },
   rules: {
+    // --------- typescipt-eslint Plugin Rules ---------
+    // enforce to have interfaces/type should start with "I".
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: "interface",
+        format: ["PascalCase"],
+        custom: {
+          regex: "^I[A-Z]",
+          match: true,
+        },
+      },
+    ],
+    // --------- React Plugin Rules ---------
+    // we do not need to import React in every file
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-props-no-spreading": "off",
+    // matter of preference (allows to use props.propName instead of deconstructing props first)
+    "react/destructuring-assignment": "off",
+    // allow other than jsx extensions
+    "react/jsx-filename-extension": [
+      1,
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+    ],
+    // --------- Import Plugin Rules ---------
     // ensure that all modules that are imported are actually declared in a package.json file
     "import/no-extraneous-dependencies": [
       "error",
       {
         // look for package.json files also in the parent directory
-        packageDir: ["./", "../"],
+        packageDir: ["./", "./front", "./shared-components"],
         // importing devDependencies in these files is ok
         devDependencies: [
+          "**/mocks/**",
           "**/tests/**",
           "**/*.test.{ts,tsx}", // tests where the extension or filename suffix denotes that it is a test
           "**/jest.config.ts", // jest config
@@ -65,4 +123,13 @@ module.exports = {
       },
     ],
   },
+  overrides: [
+    {
+      files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+      extends: ["plugin:testing-library/react"],
+      rules: {
+        "no-undef": "off",
+      },
+    },
+  ],
 };
--- statviz/.eslintrc.cjs
+++ statviz/.eslintrc.cjs
@@ -1,11 +1,12 @@
 module.exports = {
-  // the order in extends matters. Latter extends can overwrite former ones.
+  root: true,
+  env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
-    "../.eslintrc.cjs",
   ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
   rules: {
