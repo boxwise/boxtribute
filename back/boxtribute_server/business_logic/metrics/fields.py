63403cc2077498e7334098d500495d7f335cbbea
Philipp Metzner <pmetzner@freenet.de>
2024-02-20 10:35:59 +0100

    Remove redundant Metrics.movedStockOverview field
    - superseded by movedBoxes query
    - was incorrect anyways (no change of box state taken into account,
      only last_modified_on date)
    - box IDs had to be added to test case whenever a new test box was added

--- back/boxtribute_server/business_logic/metrics/fields.py
+++ back/boxtribute_server/business_logic/metrics/fields.py
@@ -1,7 +1,6 @@
 from ariadne import ObjectType
 
 from .crud import (
-    compute_moved_stock_overview,
     compute_number_of_beneficiaries_served,
     compute_number_of_families_served,
     compute_number_of_sales,
@@ -37,10 +36,3 @@ def resolve_metrics_number_of_sales(metrics_obj, _, after=None, before=None):
 @metrics.field("stockOverview")
 def resolve_metrics_stock_overview(metrics_obj, _):
     return compute_stock_overview(organisation_id=metrics_obj["organisation_id"])
-
-
-@metrics.field("movedStockOverview")
-def resolve_metrics_moved_stock_overview(metrics_obj, _, after=None, before=None):
-    return compute_moved_stock_overview(
-        organisation_id=metrics_obj["organisation_id"], after=after, before=before
-    )
