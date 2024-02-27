4b509b5dc13ecff0e6be6b7cb4efa9e1f3b2b218
Philipp Metzner <pmetzner@freenet.de>
2024-02-27 11:29:24 +0100

    Move setup_wizard script to dedicated cli module

--- back/setup.py
+++ back/setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=REQUIREMENTS,
     entry_points={
         "console_scripts": [
-            "bwiz = boxtribute_server.setup_wizard:main",
+            "bwiz = boxtribute_server.cli.main:main",
         ],
     },
 )
