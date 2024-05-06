4fdb74008e23cc9a75629c75894f843aa9039ef6
Philipp Metzner <pmetzner@freenet.de>
2024-05-06 09:40:13 +0200

    Add location CRUD functions

--- back/test/endpoint_tests/test_classic_location.py
+++ back/test/endpoint_tests/test_classic_location.py
@@ -1,4 +1,7 @@
+from boxtribute_server.auth import CurrentUser
+from boxtribute_server.db import db
 from boxtribute_server.enums import BoxState
+from boxtribute_server.business_logic.warehouse.location.crud import create_location
 from utils import assert_successful_request
 
 
@@ -50,3 +53,35 @@ def test_locations_query(read_only_client, base1_classic_locations):
     query = """query { locations { name } }"""
     locations = assert_successful_request(read_only_client, query)
     assert locations == [{"name": loc["name"]} for loc in base1_classic_locations]
+
+
+def test_crud(client, default_base):
+    from flask import g
+
+    g.user = CurrentUser(id=8)
+    name = "test location"
+    base_id = default_base["id"]
+    location = create_location(
+        name=name,
+        base_id=base_id,
+        user_id=8,
+    )
+    # assert location.box_state == BoxState.InStock
+    assert location.box_state_id == BoxState.InStock.value
+    db.database.close()
+
+    query = f"""query {{ location(id: {location.id}) {{
+                    name
+                    base {{ id }}
+                    defaultBoxState
+                    isShop
+                    createdBy {{ id }}
+                }} }}"""
+    location = assert_successful_request(client, query)
+    assert location == {
+        "name": name,
+        "base": {"id": str(base_id)},
+        "defaultBoxState": BoxState.InStock.name,
+        "isShop": False,
+        "createdBy": {"id": "8"},
+    }
