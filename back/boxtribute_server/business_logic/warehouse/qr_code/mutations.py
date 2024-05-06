cb3f87775d2b69ff2bc67ea94c4b7f3ee0ed4221
Philipp Metzner <pmetzner@freenet.de>
2024-05-06 11:43:32 +0200

    Save QR code creation in history table

--- back/boxtribute_server/business_logic/warehouse/qr_code/mutations.py
+++ back/boxtribute_server/business_logic/warehouse/qr_code/mutations.py
@@ -1,4 +1,5 @@
 from ariadne import MutationType
+from flask import g
 
 from ....authz import authorize
 from .crud import create_qr_code
@@ -9,4 +10,4 @@ mutation = MutationType()
 @mutation.field("createQrCode")
 def resolve_create_qr_code(*_, box_label_identifier=None):
     authorize(permission="qr:create")
-    return create_qr_code(box_label_identifier=box_label_identifier)
+    return create_qr_code(user_id=g.user.id, box_label_identifier=box_label_identifier)
