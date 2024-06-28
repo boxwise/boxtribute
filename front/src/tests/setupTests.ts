a2f6438f5423d099444412ef770d855f61ba1cfa
Felipe Henrich <henrich.felipe@gmail.com>
2024-06-27 22:37:34 -0300

    docs: comment to document the global mock for navigator.mediaDevices.getUserMedia

--- front/src/tests/setupTests.ts
+++ front/src/tests/setupTests.ts
@@ -30,7 +30,8 @@ const mockedUseNotification = vi.mocked(useNotification);
 mockedUseErrorHandling.mockReturnValue({ triggerError: mockedTriggerError });
 mockedUseNotification.mockReturnValue({ createToast: mockedCreateToast });
 
-// This is needed to mock the `navigator.mediaDevices.getUserMedia` function used to ask users for camera permissions in order to scan QR codes.
+// This is needed to mock the `navigator.mediaDevices.getUserMedia` function,
+// used to ask users for camera permissions in order to scan QR codes.
 const mockGetUserMedia = vi.fn(async () => {
   return new Promise<void>(resolve => {
     resolve()
