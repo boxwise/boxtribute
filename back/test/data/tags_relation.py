83b34c3e92222ee4c8f90fa91603a3d11eccba92
Philipp Metzner <pmetzner@freenet.de>
2024-09-03 15:28:31 +0200

    Update tag assignment/removal in shipments

--- back/test/data/tags_relation.py
+++ back/test/data/tags_relation.py
@@ -4,7 +4,7 @@ from boxtribute_server.enums import TaggableObjectType
 from boxtribute_server.models.definitions.tags_relation import TagsRelation
 
 from .beneficiary import default_beneficiary_data
-from .box import box_without_qr_code_data, default_box_data
+from .box import box_without_qr_code_data, default_box_data, in_transit_box_data
 from .tag import data as tag_data
 from .user import default_user_data
 
@@ -71,6 +71,15 @@ def data():
             "tag": tag_data()[2]["id"],
             "created_on": None,
             "created_by": None,
+            "deleted_on": None,
+            "deleted_by": None,
+        },
+        {
+            "object_id": in_transit_box_data()["id"],
+            "object_type": TaggableObjectType.Box,
+            "tag": tag_data()[2]["id"],
+            "created_on": date(2024, 1, 1),
+            "created_by": default_user_data()["id"],
             "deleted_on": None,
             "deleted_by": None,
         },
