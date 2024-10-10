77f796699a0fcc485b061d97f87f7b4f706ef7b9
Philipp Metzner <pmetzner@freenet.de>
2024-10-10 10:47:22 +0200

    Enable google-cloud-profiler
    - downgrade to Python 3.11
    - try to deploy directly to v2-staging

--- back/boxtribute_server/main.py
+++ back/boxtribute_server/main.py
@@ -1,5 +1,14 @@
 """Main entry point for web application"""
 
+import os
+
+from .utils import in_staging_environment
+
+if in_staging_environment():
+    import googlecloudprofiler as profiler  # type: ignore
+
+    profiler.start(verbose=2, project_id=os.environ["GOOGLE_PROJECT_ID"])
+
 from .app import main
 from .routes import app_bp
 
