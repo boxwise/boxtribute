8bb0f00a3bf438aad3368d38f15035d16445d149
Jia Lin <leeway95@gmail.com>
2024-10-30 00:51:44 +0100

    Add mocked data

--- shared-components/statviz/components/visualizations/stock/StockDataFilter.test.tsx
+++ shared-components/statviz/components/visualizations/stock/StockDataFilter.test.tsx
@@ -1,22 +1,91 @@
 import { it, expect } from "vitest";
-import StockDataFilter from "./StockDataFilter";
 import { render, screen } from "../../../../tests/testUtils";
+import { FakeGraphQLError, FakeGraphQLNetworkError } from "../movedBoxes/MovedBoxes.test";
+import { STOCK_QUERY } from "./StockDataContainer";
+import StockDataFilter from "./StockDataFilter";
+import { BoxState, ProductGender } from "../../../../types/generated/graphql";
+
+const mockFailedStockQuery = ({ baseId = "1", networkError = false }) => ({
+  request: {
+    query: STOCK_QUERY,
+    variables: { baseId },
+  },
+  result: networkError
+    ? undefined
+    : {
+        data: null,
+        errors: [new FakeGraphQLError()],
+      },
+  error: networkError ? new FakeGraphQLNetworkError() : undefined,
+});
 
 const stockDataFilterTests = [
   {
     name: "x.x.x.x - user scans wants to see stock data filter viz, but a network error is returned",
-    // mocks: [mockFailedMovedBoxesQuery({ networkError: true })],
+    mocks: [mockFailedStockQuery({ networkError: true })],
     alert: /An unexpected error happened/i,
   },
 ];
 
-stockDataFilterTests.forEach(({ name, alert }) => {
+stockDataFilterTests.forEach(({ name }) => {
   it(name, async () => {
     render(
       <StockDataFilter
-        //   TODO: Get STOCK_QUERY data to use in tests
         stockOverview={{
-          facts: [],
+          facts: [
+            {
+              productName: "underwear",
+              categoryId: 1,
+              gender: ProductGender.Women,
+              boxesCount: 1,
+              itemsCount: 806,
+              sizeId: 52,
+              tagIds: [46],
+              boxState: BoxState.InStock,
+              locationId: 100000036,
+            },
+            {
+              productName: "underwear",
+              categoryId: 1,
+              gender: ProductGender.Men,
+              boxesCount: 1,
+              itemsCount: 54,
+              sizeId: 68,
+              tagIds: [],
+              boxState: BoxState.InStock,
+              locationId: 100000036,
+            },
+            {
+              productName: "underwear",
+              categoryId: 1,
+              gender: ProductGender.Men,
+              boxesCount: 1,
+              itemsCount: 378,
+              sizeId: 4,
+              tagIds: [],
+              boxState: BoxState.InStock,
+              locationId: 100000036,
+            },
+          ],
+
+          dimensions: {
+            category: [
+              { id: 1, name: "Underwear / Nightwear" },
+              { id: 2, name: "Bottoms" },
+            ],
+            size: [
+              { id: 1, name: "S" },
+              { id: 2, name: "M" },
+            ],
+            tag: [
+              { id: 45, name: "west", color: "#7973e2" },
+              { id: 46, name: "seek", color: "#ce4869" },
+            ],
+            location: [
+              { id: 100000036, name: "Stockroom" },
+              { id: 100000037, name: "WH" },
+            ],
+          },
         }}
       />,
       {
@@ -25,6 +94,6 @@ stockDataFilterTests.forEach(({ name, alert }) => {
       },
     );
 
-    expect(await screen.findByText(alert)).toBeInTheDocument();
+    expect(await screen.findByText(/Drilldown Chart of Instock Boxes/)).toBeInTheDocument();
   });
 });
