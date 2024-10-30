e898b434f37f1db2f0cd89cddcaa5422d45a7c18
Felipe Henrich <henrich.felipe@gmail.com>
2024-10-30 12:14:55 -0300

    feat: add menu icon component

---
+++ front/src/components/HeaderMenu/MenuIcons.tsx
@@ -0,0 +1,44 @@
+import { BiSolidStore } from "react-icons/bi";
+import { BsBox2HeartFill, BsClipboardHeartFill } from "react-icons/bs";
+import { FaShieldAlt } from "react-icons/fa";
+import { FaTruckPlane } from "react-icons/fa6";
+import { RiAccountCircleFill, RiBarChart2Fill, RiQrCodeLine } from "react-icons/ri";
+
+export type Icon =
+  | "QRCode"
+  | "Statistics"
+  | "Aid Inventory"
+  | "Aid Transfers"
+  | "Beneficiares"
+  | "Free Shop"
+  | "Admin"
+  | "Account";
+
+type props = {
+  icon: Icon;
+};
+
+function MenuIcons({ icon }: props) {
+  switch (icon) {
+    case "QRCode":
+      return <RiQrCodeLine />;
+    case "Statistics":
+      return <RiBarChart2Fill />;
+    case "Aid Inventory":
+      return <BsBox2HeartFill />;
+    case "Aid Transfers":
+      return <FaTruckPlane />;
+    case "Beneficiares":
+      return <BsClipboardHeartFill />;
+    case "Free Shop":
+      return <BiSolidStore />;
+    case "Admin":
+      return <FaShieldAlt />;
+    case "Account":
+      return <RiAccountCircleFill />;
+    default:
+      return <></>;
+  }
+}
+
+export default MenuIcons;
