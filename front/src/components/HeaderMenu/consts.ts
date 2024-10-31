7ed9e2c050ece8bca3d7bc989d154131bb1021ce
Felipe Henrich <henrich.felipe@gmail.com>
2024-10-31 09:20:25 -0300

    refactor: split components, consts

---
+++ front/src/components/HeaderMenu/consts.ts
@@ -0,0 +1,4 @@
+export const LARGE_DESKTOP_SCREEN_MEDIA_QUERY = "(min-width: 1600px)";
+export const SMALL_DESKTOP_OR_TABLET_SCREEN_MEDIA_QUERY = "(min-width: 1024px)";
+export const ACCOUNT_SETTINGS_URL =
+    `${import.meta.env.FRONT_OLD_APP_BASE_URL}/?action=cms_profile` as const;
