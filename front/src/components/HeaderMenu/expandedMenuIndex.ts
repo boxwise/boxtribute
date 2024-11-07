8fa61f1dffcc038f3f6ad34d47af6d6d07eb1d1f
Felipe Henrich <henrich.felipe@gmail.com>
2024-11-07 10:20:56 -0300

    chore: prettier

--- front/src/components/HeaderMenu/expandedMenuIndex.ts
+++ front/src/components/HeaderMenu/expandedMenuIndex.ts
@@ -6,17 +6,17 @@
  * https://v2.chakra-ui.com/docs/components/accordion/usage#expand-multiple-items-at-once
  */
 export function expandedMenuIndex() {
-	const path = document.location.pathname.split("/").at(-1)
+  const path = document.location.pathname.split("/").at(-1)
 
-	switch (path) {
-		case "statviz":
-			return [0]
-		case "boxes":
-			return [1]
-		case "shipments":
-		case "agreements":
-			return [2]
-		default:
-			return undefined
-	}
+  switch (path) {
+    case "statviz":
+      return [0]
+    case "boxes":
+      return [1]
+    case "shipments":
+    case "agreements":
+      return [2]
+    default:
+      return undefined
+  }
 }
