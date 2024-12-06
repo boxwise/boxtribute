8d15663aec6ad16367902df8cb1fe9860ca281e9
Felipe Henrich <henrich.felipe@gmail.com>
2024-12-06 17:13:06 -0300

    infra: update graphql-gen script, fixtures

--- graphql-gen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cat ./back/boxtribute_server/graph_ql/definitions/basic/*.graphql ./back/boxtribute_server/graph_ql/definitions/protected/*.graphql ./back/boxtribute_server/graph_ql/definitions/public/types.graphql > ./graphql/generated/schema.graphql
-
-pnpm gql-tada generate output
-
-pnpm graphql-codegen -c graphql/codegen.yml
