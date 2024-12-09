d546af407dc381d7fe5ee5868956fbedd807fc0a
Philipp Metzner <pmetzner@freenet.de>
2024-12-09 17:23:31 +0100

    More config

--- front/src/i18n.js
+++ front/src/i18n.js
@@ -9,7 +9,11 @@ i18n
     lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
-
+    debug: true,
+    fallbackLng: "en",
+    saveMissing: true,
+    backend: { addPath: 'http://localhost:3000/public/locales/{{lng}}/{{ns}}.missing.json'},
+    // backend: { addPath: '/public/locales/{{lng}}/{{ns}}.json'},
     interpolation: {
       escapeValue: false // react already safes from xss
     }
