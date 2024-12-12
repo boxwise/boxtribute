f7781ee0d0de202df413995dcf64d2486ff3a185
HaGuesto <hans@boxtribute.org>
2024-12-12 20:39:29 +0100

    implement different fallback depending on screensize

--- front/src/views/NotFoundView/NotFoundView.tsx
+++ front/src/views/NotFoundView/NotFoundView.tsx
@@ -1,4 +1,5 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { DESKTOP_OR_TABLET_SCREEN_MEDIA_QUERY } from "components/HeaderMenu/consts";
 import { useErrorHandling } from "hooks/useErrorHandling";
 import { Navigate, useLocation } from "react-router-dom";
 
@@ -6,6 +7,10 @@ function NotFoundView() {
   const { user } = useAuth0();
   const location = useLocation();
   const { triggerError } = useErrorHandling();
+  const isLargeScreen = window.matchMedia(DESKTOP_OR_TABLET_SCREEN_MEDIA_QUERY);
+  // somehow useMediaQuery always returns false, most likely because the width is measured somewhere during the rendering.
+  // TODO: create a global state using useMediaQuery higher up in the DOM tree and use it here.
+  // const [isLargeScreen] = useMediaQuery(DESKTOP_OR_TABLET_SCREEN_MEDIA_QUERY);
 
   if (!user || !user["https://www.boxtribute.com/base_ids"]) {
     triggerError({ message: "This requested page does not exist." });
@@ -14,7 +19,14 @@ function NotFoundView() {
 
   // If the requested route was not found redirect to statviz
   const baseId = user["https://www.boxtribute.com/base_ids"][0];
-  return <Navigate to={`/bases/${baseId}/statviz`} replace state={{ origin: location.pathname }} />;
+  if (isLargeScreen.matches) {
+    return (
+      <Navigate to={`/bases/${baseId}/statviz`} replace state={{ origin: location.pathname }} />
+    );
+  }
+  return (
+    <Navigate to={`/bases/${baseId}/qrreader`} replace state={{ origin: location.pathname }} />
+  );
 }
 
 export default NotFoundView;
