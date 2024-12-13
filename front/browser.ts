7e169c1078d73d8750976b454eef7f6ba433f4a0
Felipe Henrich <henrich.felipe@gmail.com>
2024-12-13 12:06:38 -0300

    feat: msw handlers (except shipments, transfer agreements, box history)

--- front/browser.ts
+++ front/browser.ts
@@ -1,4 +1,4 @@
-import { setupWorker } from 'msw/browser'
-import { handlers } from '../tests/mswHandlers';
+import { setupWorker } from "msw/browser";
+import { handlers } from "../tests/mswHandlers";
 
 export const worker = setupWorker(...handlers);
--- tests/mswHandlers.ts
+++ tests/mswHandlers.ts
@@ -1,16 +1,18 @@
-import { delay, graphql, GraphQLResponseResolver, HttpResponse, RequestHandlerOptions } from 'msw'
-import { ResultOf, TadaDocumentNode, VariablesOf } from 'gql.tada';
-import { devCoordinator } from './fixtures';
+import { delay, graphql, GraphQLResponseResolver, HttpResponse, RequestHandlerOptions } from "msw"
+import { ResultOf, TadaDocumentNode, VariablesOf } from "gql.tada";
+import { devCoordinator } from "./fixtures";
 
 import { worker } from "../front/browser"
-import { ORGANISATION_AND_BASES_QUERY, BOX_BY_LABEL_IDENTIFIER_AND_ALL_SHIPMENTS_QUERY } from "../front/src/queries/queries"
+import { ORGANISATION_AND_BASES_QUERY, BOX_BY_LABEL_IDENTIFIER_AND_ALL_SHIPMENTS_QUERY, GET_BOX_LABEL_IDENTIFIER_BY_QR_CODE, CHECK_IF_QR_EXISTS_IN_DB, MULTI_BOX_ACTION_OPTIONS_FOR_LOCATIONS_TAGS_AND_SHIPMENTS_QUERY } from "../front/src/queries/queries"
 import { BOXES_FOR_BOXESVIEW_QUERY, ACTION_OPTIONS_FOR_BOXESVIEW_QUERY } from "../front/src/views/Boxes/BoxesView"
 import { UPDATE_BOX_MUTATION, UPDATE_NUMBER_OF_ITEMS_IN_BOX_MUTATION, UPDATE_STATE_IN_BOX_MUTATION } from "../front/src/views/Box/BoxView"
-import { BOX_BY_LABEL_IDENTIFIER_AND_ALL_PRODUCTS_WITH_BASEID_QUERY, UPDATE_CONTENT_OF_BOX_MUTATION } from '../front/src/views/BoxEdit/BoxEditView';
-import { CREATED_BOXES_QUERY } from '../shared-components/statviz/components/visualizations/createdBoxes/CreatedBoxesDataContainer';
-import { MOVED_BOXES_QUERY } from '../shared-components/statviz/components/visualizations/movedBoxes/MovedBoxesDataContainer';
-import { STOCK_QUERY } from '../shared-components/statviz/components/visualizations/stock/StockDataContainer';
-import { DEMOGRAPHIC_QUERY } from '../shared-components/statviz/components/visualizations/demographic/DemographicDataContainer';
+import { BOX_BY_LABEL_IDENTIFIER_AND_ALL_PRODUCTS_WITH_BASEID_QUERY, UPDATE_CONTENT_OF_BOX_MUTATION } from "../front/src/views/BoxEdit/BoxEditView";
+import { ASSIGN_BOXES_TO_SHIPMENT, UNASSIGN_BOX_FROM_SHIPMENT } from "../front/src/hooks/useAssignBoxesToShipment";
+import { ALL_PRODUCTS_AND_LOCATIONS_FOR_BASE_QUERY, CREATE_BOX_MUTATION } from "../front/src/views/BoxCreate/BoxCreateView";
+import { CREATED_BOXES_QUERY } from "../shared-components/statviz/components/visualizations/createdBoxes/CreatedBoxesDataContainer";
+import { MOVED_BOXES_QUERY } from "../shared-components/statviz/components/visualizations/movedBoxes/MovedBoxesDataContainer";
+import { STOCK_QUERY } from "../shared-components/statviz/components/visualizations/stock/StockDataContainer";
+import { DEMOGRAPHIC_QUERY } from "../shared-components/statviz/components/visualizations/demographic/DemographicDataContainer";
 
 // Utilities
 
@@ -24,7 +26,7 @@ async function baseQueryHandler<T extends TadaDocumentNode>(
   /** MSW handler options. */
   options?: RequestHandlerOptions
 ) {
-  await delay(100);
+  await delay(300);
 
   return graphql.query<ResultOf<T>, VariablesOf<T>>(
     operationName,
@@ -43,7 +45,7 @@ async function baseMutationHandler<T extends TadaDocumentNode>(
   /** MSW handler options. */
   options?: RequestHandlerOptions
 ) {
-  await delay(100);
+  await delay(300);
 
   return graphql.mutation<ResultOf<T>, VariablesOf<T>>(
     operationName,
@@ -99,11 +101,83 @@ const mockOrganisationsAndBasesQueryHandler = baseQueryHandler(ORGANISATION_AND_
   return HttpResponse.json(devCoordinator.OrganisationAndBases);
 });
 
+const mockGetBoxLabelIdentifierForQrCodeHandler = baseQueryHandler(GET_BOX_LABEL_IDENTIFIER_BY_QR_CODE, "GetBoxLabelIdentifierForQrCode", ({ variables }) => {
+  const { qrCode } = variables;
+
+  const result = devCoordinator.GetBoxLabelIdentifierForQrCode[qrCode];
+
+  if (!result) return HttpResponse.json({ errors: [{ message: "Mock doesn't exist for this QRCode." }] });
+
+  // @ts-ignore
+  return HttpResponse.json(result);
+});
+
+const mockCheckIfQrExistsInDbHandler = baseQueryHandler(CHECK_IF_QR_EXISTS_IN_DB, "CheckIfQrExistsInDb", ({ variables }) => {
+  const { qrCode } = variables;
+
+  const result = devCoordinator.CheckIfQrExistsInDb[qrCode];
+
+  if (!result) return HttpResponse.json({ errors: [{ message: "Mock doesn't exist for this QRCode." }] });
+
+  // @ts-ignore
+  return HttpResponse.json(result);
+});
+
+const mockMultiBoxActionOptionsForLocationsTagsAndShipmentseHandler = baseQueryHandler(
+  MULTI_BOX_ACTION_OPTIONS_FOR_LOCATIONS_TAGS_AND_SHIPMENTS_QUERY, "MultiBoxActionOptionsForLocationsTagsAndShipments", ({ variables }) => {
+    const { baseId } = variables;
+
+    return HttpResponse.json(devCoordinator.MultiBoxActionOptionsForLocationsTagsAndShipments.baseId[baseId]);
+  });
+
+const mockAllProductsAndLocationsForBaseHandler = baseQueryHandler(ALL_PRODUCTS_AND_LOCATIONS_FOR_BASE_QUERY, "AllProductsAndLocationsForBase", ({ variables }) => {
+  const { baseId } = variables;
+
+  return HttpResponse.json(devCoordinator.AllProductsAndLocationsForBase.baseId[baseId]);
+});
+
+// TODO: include box in BoxesForBoxesViewQuery?
+const mockCreateBoxHandler = baseMutationHandler(CREATE_BOX_MUTATION, "CreateBox", async ({ variables }) => {
+  const { locationId, numberOfItems, productId, comment, tagIds, sizeId } = variables;
+
+  const allProducts = Object.values(devCoordinator.AllProductsAndLocationsForBase.baseId)
+    .flatMap(res => res.data.base.products);
+  const size = allProducts.find(product => product.id === "" + productId)?.sizeRange.sizes.find(size => size.id === "" + sizeId)!;
+
+  const box = {
+    ...Object.values(devCoordinator.BoxesForBoxesViewQuery.baseId).flatMap(res => res.data.boxes.elements)[0]
+  }
+
+  box.numberOfItems = numberOfItems;
+  box.location = findLocation("" + locationId);
+  box.product = findProduct("" + productId);
+  box.size = size;
+  if (comment) box.comment = comment;
+  if (tagIds?.length) box.tags = findTags(tagIds);
+
+  worker.use(await baseQueryHandler(BOX_BY_LABEL_IDENTIFIER_AND_ALL_SHIPMENTS_QUERY, "BoxByLabelIdentifier", ({ variables: { labelIdentifier } }) =>
+    HttpResponse.json({
+      data: {
+        box: {
+          // @ts-expect-error
+          ...box, labelIdentifier, location: { ...box.location, ...boxByLabelIdentifierLocation, defaultBoxState: "InStock" },
+          history: boxByLabelIdentifierHistory
+        },
+        // @ts-expect-error
+        shipments: boxByLabelIdentifierShipments
+      }
+    }))
+  );
+
+  // @ts-expect-error
+  return HttpResponse.json(devCoordinator.CreateBox);
+})
+
 const mockBoxesForBoxesViewHandler = baseQueryHandler(BOXES_FOR_BOXESVIEW_QUERY, "BoxesForBoxesView", ({ variables }) => {
   const { baseId } = variables;
 
   worker.resetHandlers();
-  // TODO: update box in elements?
+  // TODO: update box in BoxesForBoxesViewQuery elements?
 
   return HttpResponse.json(devCoordinator.BoxesForBoxesViewQuery.baseId[baseId]);
 });
@@ -197,19 +271,20 @@ const mockUpdateNumberOfItemsHandler = baseMutationHandler(UPDATE_NUMBER_OF_ITEM
   return HttpResponse.json({ data: { updateBox: box } });
 })
 
-const mockBoxByLabelIdentifierAndAllProductsWithBaseIdHandler = baseQueryHandler(BOX_BY_LABEL_IDENTIFIER_AND_ALL_PRODUCTS_WITH_BASEID_QUERY, "BoxByLabelIdentifierAndAllProductsWithBaseId", ({ variables }) => {
-  const { baseId, labelIdentifier } = variables;
+const mockBoxByLabelIdentifierAndAllProductsWithBaseIdHandler = baseQueryHandler(
+  BOX_BY_LABEL_IDENTIFIER_AND_ALL_PRODUCTS_WITH_BASEID_QUERY, "BoxByLabelIdentifierAndAllProductsWithBaseId", ({ variables }) => {
+    const { baseId, labelIdentifier } = variables;
 
-  const box = findBox(labelIdentifier);
-  const base = devCoordinator.BoxByLabelIdentifierAndAllProductsWithBaseId.baseId[baseId].data.base;
+    const box = findBox(labelIdentifier);
+    const base = devCoordinator.BoxByLabelIdentifierAndAllProductsWithBaseId.baseId[baseId].data.base;
 
-  return HttpResponse.json({
-    data: {
-      // @ts-expect-error
-      base, box: { ...box, id: labelIdentifier, history: boxByLabelIdentifierHistory },
-    }
-  });
-})
+    return HttpResponse.json({
+      data: {
+        // @ts-expect-error
+        base, box: { ...box, id: labelIdentifier, history: boxByLabelIdentifierHistory },
+      }
+    });
+  })
 
 const mockUpdateContentOfBoxHandler = baseMutationHandler(UPDATE_CONTENT_OF_BOX_MUTATION, "UpdateContentOfBox", async ({ variables }) => {
   const { boxLabelIdentifier, locationId, numberOfItems, productId, sizeId, comment, tagIds } = variables;
@@ -236,6 +311,70 @@ const mockUpdateContentOfBoxHandler = baseMutationHandler(UPDATE_CONTENT_OF_BOX_
   return HttpResponse.json({ data: { updateBox: box } });
 })
 
+const mockAssignBoxesToShipmentHandler = baseMutationHandler(ASSIGN_BOXES_TO_SHIPMENT, "AssignBoxesToShipment", async ({ variables }) => {
+  const { labelIdentifiers, id: resId } = variables;
+
+  const result = devCoordinator.AssignBoxesToShipment;
+  const box = findBox(labelIdentifiers![0]);
+  const lastShipmentDetailCopy = result.data.updateShipmentWhenPreparing.details.at(-1)!;
+
+  box.state = "MarkedForShipment";
+  // @ts-expect-error
+  lastShipmentDetailCopy.box = box;
+  lastShipmentDetailCopy.box.shipmentDetail = { __typename: "", id: "", shipment: { __typename: "", id: resId } };
+  result.data.updateShipmentWhenPreparing.details.push(lastShipmentDetailCopy);
+
+  worker.use(await baseQueryHandler(BOX_BY_LABEL_IDENTIFIER_AND_ALL_SHIPMENTS_QUERY, "BoxByLabelIdentifier", () =>
+    HttpResponse.json({
+      data: {
+        box: {
+          ...box,
+          // @ts-expect-error
+          location: { ...box.location, ...boxByLabelIdentifierLocation },
+          shipmentDetail: {
+            // @ts-expect-error
+            ...lastShipmentDetailCopy.box.shipmentDetail, shipment: { targetBase: result.data.updateShipmentWhenPreparing.targetBase, id: resId }
+          },
+          history: boxByLabelIdentifierHistory
+        },
+        // @ts-expect-error
+        shipments: boxByLabelIdentifierShipments
+      }
+    }))
+  );
+
+  // @ts-expect-error
+  return HttpResponse.json(result);
+});
+
+const mockUnassignBoxesFromShipmentHandler = baseMutationHandler(UNASSIGN_BOX_FROM_SHIPMENT, "UnassignBoxesFromShipment", async ({ variables }) => {
+  const { labelIdentifiers } = variables;
+
+  const result = devCoordinator.UnassignBoxesFromShipment;
+  const box = findBox(labelIdentifiers![0]);
+  const lastShipmentDetailCopy = result.data.updateShipmentWhenPreparing.details.at(-1)!;
+
+  box.state = "InStock";
+  // @ts-expect-error
+  lastShipmentDetailCopy.box = box;
+  lastShipmentDetailCopy.box.shipmentDetail = null;
+  result.data.updateShipmentWhenPreparing.details.push(lastShipmentDetailCopy);
+
+  worker.use(await baseQueryHandler(BOX_BY_LABEL_IDENTIFIER_AND_ALL_SHIPMENTS_QUERY, "BoxByLabelIdentifier", () =>
+    HttpResponse.json({
+      data: {
+        // @ts-expect-error
+        box: { ...box, location: { ...box.location, ...boxByLabelIdentifierLocation }, history: boxByLabelIdentifierHistory },
+        // @ts-expect-error
+        shipments: boxByLabelIdentifierShipments
+      }
+    }))
+  );
+
+  // @ts-expect-error
+  return HttpResponse.json(result);
+});
+
 // Exported handlers to be consumed by MSW
 export const handlers = [
   await mockOrganisationsAndBasesQueryHandler,
@@ -243,6 +382,11 @@ export const handlers = [
   await mockMovedBoxesHandler,
   await mockBeneficiaryDemographicsHandler,
   await mockStockOverviewHandler,
+  await mockGetBoxLabelIdentifierForQrCodeHandler,
+  await mockCheckIfQrExistsInDbHandler,
+  await mockAllProductsAndLocationsForBaseHandler,
+  await mockMultiBoxActionOptionsForLocationsTagsAndShipmentseHandler,
+  await mockCreateBoxHandler,
   await mockBoxesForBoxesViewHandler,
   await mockActionOptionsForBoxesViewHandler,
   await mockBoxByLabelIdentifierHandler,
@@ -251,4 +395,6 @@ export const handlers = [
   await mockUpdateNumberOfItemsHandler,
   await mockBoxByLabelIdentifierAndAllProductsWithBaseIdHandler,
   await mockUpdateContentOfBoxHandler,
+  await mockAssignBoxesToShipmentHandler,
+  await mockUnassignBoxesFromShipmentHandler,
 ];
