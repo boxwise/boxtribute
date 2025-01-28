f98fc1b5da153ac4fdf69d09726f4422daee3548
Philipp Metzner <pmetzner@freenet.de>
2025-01-28 11:39:53 +0100

    Update comments

--- back/boxtribute_server/graph_ql/scalars.py
+++ back/boxtribute_server/graph_ql/scalars.py
@@ -6,17 +6,21 @@ datetime_scalar = ScalarType("Datetime")
 date_scalar = ScalarType("Date")
 
 
+# Serializing date(time) object in application layer to string for API
 @datetime_scalar.serializer
 def serialize_datetime(value):
+    # Return datetime value as UTC in format YYYY-MM-DDTHH:MM:SS+00:00
     value = value.replace(tzinfo=timezone.utc)
     return value.isoformat()
 
 
 @date_scalar.serializer
 def serialize_date(value):
+    # Return date value in format YYYY-MM-DD
     return value.isoformat()
 
 
+# Parsing string from API into date(time) object in application layer
 @date_scalar.value_parser
 def parse_date(value):
     # Allowed formats: YYYY-MM-DD and YYYYMMDD
@@ -25,6 +29,7 @@ def parse_date(value):
 
 @datetime_scalar.value_parser
 def parse_datetime(value):
-    # Datetime string must be of format '2022-08-30T14:00:00'.
-    # With Python 3.11 more formats will be supported
+    # Allowed formats cf.
+    # https://docs.python.org/3/library/datetime.html#datetime.datetime.fromisoformat
+    # This returns a offset-naive datetime if value without "+XX:XX" suffix
     return datetime.fromisoformat(value)
