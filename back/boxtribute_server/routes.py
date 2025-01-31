542777541d93bad421fd9da15bc2c53a0e947195
Philipp Metzner <pmetzner@freenet.de>
2025-01-31 17:45:36 +0100

    Add route for housekeeping cron job

--- back/boxtribute_server/routes.py
+++ back/boxtribute_server/routes.py
@@ -132,15 +132,18 @@ def cron(job_name):
         )
         return jsonify({"message": "unauthorized"}), 401
 
-    permitted_databases = ["dropapp_dev", "dropapp_staging", "dropapp_demo"]
-    if (db_name := os.environ["MYSQL_DB"]) not in permitted_databases:
-        return jsonify({"message": f"Reset of '{db_name}' not permitted"}), 400
-
     if job_name == "reseed-db":
+        permitted_databases = ["dropapp_dev", "dropapp_staging", "dropapp_demo"]
+        if (db_name := os.environ["MYSQL_DB"]) not in permitted_databases:
+            return jsonify({"message": f"Reset of '{db_name}' not permitted"}), 400
+
         from .cron.reseed_db import reseed_db
 
         # Any error will be reported as 500 response by Flask, and logged in Sentry
         reseed_db()
         return jsonify({"message": "reseed-db job executed"}), 200
+
+    if job_name == "housekeeping":
+        pass
 
     return jsonify({"message": f"unknown job '{job_name}'"}), 400
