ed3835ba58461e67b99b0297724eb0ed401abe9b
Felipe Henrich <henrich.felipe@gmail.com>
2025-05-15 09:36:03 -0300

    feat: add custom product edit view and form

---
+++ front/src/views/EditCustomProduct/EditCustomProductView.tsx
@@ -0,0 +1,228 @@
+import { Box, Center, Heading } from "@chakra-ui/react";
+import { ErrorBoundary } from "@sentry/react";
+import { AlertWithoutAction } from "components/Alerts";
+import { MobileBreadcrumbButton } from "components/BreadcrumbNavigation";
+import { FormSkeleton } from "components/Skeletons";
+import { Suspense, useCallback } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useAtomValue } from "jotai";
+import { selectedBaseIdAtom } from "stores/globalPreferenceStore";
+import { useErrorHandling } from "hooks/useErrorHandling";
+import { useNotification } from "hooks/useNotification";
+import { useMutation, useSuspenseQuery } from "@apollo/client";
+import { ResultOf } from "gql.tada";
+import { graphql } from "../../../../graphql/graphql";
+import { PRODUCTS_QUERY } from "views/Products/components/ProductsContainer";
+import { NonNullProductGender } from "../../../../graphql/types";
+// TODO: move this to common folder in components?
+import {
+  customProductRawToFormOptionsTransformer,
+  findDefaultValues,
+} from "views/CreateCustomProduct/components/transformer";
+import EditCustomProductForm, {
+  EditCustomProductFormOutput,
+} from "./components/EditCustomProductForm";
+
+const editCustomProductQueryErrorText = "Something went wrong! Please try reloading the page.";
+
+const CUSTOM_PRODUCT_FORM_OPTIONS_QUERY = graphql(
+  `
+    query CustomProductFormOptions {
+      productCategories {
+        id
+        name
+      }
+      sizeRanges {
+        id
+        label
+      }
+    }
+  `,
+  [],
+);
+
+export type ICustomProductFormQueryResult = ResultOf<typeof CUSTOM_PRODUCT_FORM_OPTIONS_QUERY>;
+
+const EDIT_CUSTOM_PRODUCT_MUTATION = graphql(
+  `
+    mutation EditCustomProduct(
+      $id: ID!
+      $name: String!
+      $categoryId: Int!
+      $sizeRangeId: Int!
+      $gender: ProductGender!
+      $price: Int
+      $inShop: Boolean
+      $comment: String
+    ) {
+      editCustomProduct(
+        editInput: {
+          id: $id
+          name: $name
+          categoryId: $categoryId
+          sizeRangeId: $sizeRangeId
+          gender: $gender
+          price: $price
+          inShop: $inShop
+          comment: $comment
+        }
+      ) {
+        __typename
+        ... on Product {
+          id
+        }
+      }
+    }
+  `,
+  [],
+);
+
+const editCustomProductErrorToastText = "Could not edit this custom product! Try again?";
+
+function EditCustomProductFormContainer() {
+  const navigate = useNavigate();
+  const baseId = useAtomValue(selectedBaseIdAtom);
+  const customProductId = useParams<{ customProductId: string }>().customProductId;
+  const { createToast } = useNotification();
+  const { triggerError } = useErrorHandling();
+  const { data: customProductFormRawOptions, error } = useSuspenseQuery(
+    CUSTOM_PRODUCT_FORM_OPTIONS_QUERY,
+  );
+
+  const { data: productsRawData, error: prodError } = useSuspenseQuery(PRODUCTS_QUERY, {
+    variables: {
+      baseId,
+    },
+  });
+
+  const [editCustomProduct, { loading: isEditCustomProductLoading }] = useMutation(
+    EDIT_CUSTOM_PRODUCT_MUTATION,
+  );
+
+  const onSubmit = useCallback(
+    (editProductFormOutput: EditCustomProductFormOutput) => {
+      editCustomProduct({
+        variables: {
+          id: customProductId!,
+          name: editProductFormOutput.name,
+          categoryId: parseInt(editProductFormOutput.category?.value!),
+          sizeRangeId: parseInt(editProductFormOutput.sizeRange?.value!),
+          gender: editProductFormOutput.gender?.value as NonNullProductGender,
+          price: editProductFormOutput.price,
+          inShop: editProductFormOutput.inShop,
+          comment: editProductFormOutput.comment,
+        },
+        refetchQueries: [
+          {
+            query: PRODUCTS_QUERY,
+            variables: {
+              baseId,
+            },
+          },
+        ],
+      })
+        .then(({ data }) => {
+          const result = data?.editCustomProduct;
+          if (!result) {
+            triggerError({
+              message: editCustomProductErrorToastText,
+            });
+            return;
+          }
+
+          switch (result.__typename) {
+            case "Product":
+              createToast({
+                message: `The custom product was successfully edited.`,
+              });
+              navigate(`../..`);
+
+              break;
+            case "InsufficientPermissionError":
+            case "UnauthorizedForBaseError":
+              triggerError({
+                message: "You don't have permission to edit a custom product!",
+              });
+              break;
+            case "InvalidPriceError":
+              triggerError({
+                message: "Price must be a positive integer number.",
+              });
+              break;
+            case "EmptyNameError":
+              triggerError({
+                message: "The name of the product cannot be empty.",
+              });
+              break;
+            case "ResourceDoesNotExistError":
+              triggerError({
+                message: "The selected options do not exist.",
+              });
+              break;
+
+            default:
+              triggerError({
+                message: editCustomProductErrorToastText,
+              });
+              break;
+          }
+        })
+        .catch(() => {
+          // Handle network or other errors
+          triggerError({
+            message: editCustomProductErrorToastText,
+          });
+        });
+    },
+    [editCustomProduct, customProductId, baseId, triggerError, createToast, navigate],
+  );
+
+  // If Apollo encountered an error (like network error), throw it
+  if (error) throw error;
+  if (prodError) throw prodError;
+
+  const { categoryOptions, sizeRangeOptions, genderOptions } =
+    customProductRawToFormOptionsTransformer(customProductFormRawOptions);
+
+  const defaultValues = findDefaultValues(productsRawData, customProductId);
+
+  return (
+    <EditCustomProductForm
+      categoryOptions={categoryOptions}
+      sizeRangeOptions={sizeRangeOptions}
+      genderOptions={genderOptions}
+      onSubmit={onSubmit}
+      isLoading={isEditCustomProductLoading}
+      defaultValues={defaultValues}
+    />
+  );
+}
+
+function EditCustomProductView() {
+  return (
+    <>
+      <MobileBreadcrumbButton label="Back to Manage Products" linkPath="../.." />
+      <Center>
+        <Box w={["100%", "100%", "60%", "40%"]}>
+          <Heading fontWeight="bold" mb={8} as="h1">
+            Edit Custom Product
+          </Heading>
+          <ErrorBoundary
+            fallback={({ error }) => (
+              <AlertWithoutAction
+                type="error"
+                alertText={error?.toString() || editCustomProductQueryErrorText}
+              />
+            )}
+          >
+            <Suspense fallback={<FormSkeleton />}>
+              <EditCustomProductFormContainer />
+            </Suspense>
+          </ErrorBoundary>
+        </Box>
+      </Center>
+    </>
+  );
+}
+
+export default EditCustomProductView;
---
+++ front/src/views/EditCustomProduct/components/EditCustomProductForm.tsx
@@ -0,0 +1,186 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  HStack,
+  Input,
+  Stack,
+  // Switch,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { NewNumberField } from "components/Form/NumberField";
+import SelectField from "components/Form/SelectField";
+import SwitchField from "components/Form/SwitchField";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { z } from "zod";
+
+const SingleSelectOptionSchema = z.object({
+  label: z.string(),
+  value: z.string(),
+});
+
+const EditCustomProductFormSchema = z.object({
+  name: z
+    .string()
+    .trim()
+    .refine((name) => !!name, {
+      message: "Please enter a product name.",
+    }),
+  category: SingleSelectOptionSchema.optional(),
+  gender: SingleSelectOptionSchema.optional(),
+  sizeRange: SingleSelectOptionSchema.optional(),
+  comment: z
+    .string()
+    .optional()
+    .transform((value) => (value === "" ? undefined : value)),
+  inShop: z.boolean().optional(),
+  price: z
+    .number({
+      invalid_type_error: "Please enter a positive integer number.",
+    })
+    .int()
+    .nonnegative()
+    .optional(),
+});
+
+export type EditCustomProductFormInput = z.input<typeof EditCustomProductFormSchema>;
+export type EditCustomProductFormOutput = z.output<typeof EditCustomProductFormSchema>;
+
+export type IEditCustomProductFormProps = {
+  isLoading: boolean;
+  categoryOptions: z.infer<typeof SingleSelectOptionSchema>[];
+  sizeRangeOptions: z.infer<typeof SingleSelectOptionSchema>[];
+  genderOptions: z.infer<typeof SingleSelectOptionSchema>[];
+  defaultValues: EditCustomProductFormInput | undefined;
+  onSubmit: (EditCustomProductFormOutput: EditCustomProductFormOutput) => void;
+};
+
+function EditCustomProductForm({
+  isLoading,
+  categoryOptions,
+  sizeRangeOptions,
+  genderOptions,
+  defaultValues,
+  onSubmit,
+}: IEditCustomProductFormProps) {
+  console.log("defaultValues", defaultValues);
+  const navigate = useNavigate();
+  const {
+    handleSubmit,
+    control,
+    register,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(EditCustomProductFormSchema),
+    ...(defaultValues ? { defaultValues } : {}),
+  });
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Box border="2px" mb={4} p={2}>
+        <HStack borderBottom="2px" pb={2} px={2}>
+          <Text fontWeight="bold" fontSize="md">
+            PRODUCT SETUP
+          </Text>
+        </HStack>
+        <VStack spacing={4} p={2} mt={2}>
+          <FormControl>
+            <HStack>
+              {/* <Switch id="type-switch" mr={2} isChecked onChange={() => navigate("../enable")} /> */}
+              <Text fontWeight="medium" fontSize="md">
+                Custom Product (Base Specific)
+              </Text>
+            </HStack>
+          </FormControl>
+          <FormControl isInvalid={!!errors.name}>
+            <FormLabel htmlFor="name">
+              Name{" "}
+              <Text as="span" color="red.500">
+                *
+              </Text>
+            </FormLabel>
+            <Input
+              borderColor="black"
+              border="2px"
+              borderRadius={0}
+              _focus={{ borderColor: "blue.500" }}
+              _hover={{ borderColor: "gray.300" }}
+              placeholder="Please enter a product name."
+              type="string"
+              {...register("name")}
+            />
+            {!!errors.name && <FormErrorMessage>{errors.name.message}</FormErrorMessage>}
+          </FormControl>
+          <SelectField
+            fieldId="category"
+            fieldLabel="Category"
+            placeholder="Please select a category."
+            options={categoryOptions}
+            errors={errors}
+            control={control}
+          />
+          <SelectField
+            fieldId="gender"
+            fieldLabel="Gender"
+            placeholder="Please select a gender."
+            options={genderOptions}
+            errors={errors}
+            control={control}
+          />
+          <SelectField
+            fieldId="sizeRange"
+            fieldLabel="Size Range"
+            placeholder="Please select a size range."
+            options={sizeRangeOptions}
+            errors={errors}
+            control={control}
+          />
+
+          <FormControl>
+            <FormLabel htmlFor="comment">Description</FormLabel>
+            <Input
+              borderColor="black"
+              border="2px"
+              borderRadius={0}
+              _focus={{ borderColor: "blue.500" }}
+              _hover={{ borderColor: "gray.300" }}
+              size="lg"
+              type="string"
+              {...register("comment")}
+            />
+          </FormControl>
+        </VStack>
+      </Box>
+      <Box border="2px" mb={4} p={2}>
+        <Box borderBottom="2px" pb={2} px={2} mb={2}>
+          <Text fontWeight="bold" fontSize="md">
+            FREE SHOP SETTINGS
+          </Text>
+        </Box>
+        <SwitchField fieldId="inShop" fieldLabel="Always Show in Stockroom?" control={control} />
+        <NewNumberField
+          fieldId="price"
+          fieldLabel="Token Price"
+          errors={errors}
+          control={control}
+          p={2}
+        />
+      </Box>
+      <Stack spacing={2} my={4}>
+        <Button isLoading={isLoading} disabled={isLoading} type="submit" w="full" variant="submit">
+          Edit Product
+        </Button>
+        <Button size="md" type="button" w="full" variant="cancel" onClick={() => navigate("../..")}>
+          Nevermind
+        </Button>
+      </Stack>
+    </form>
+  );
+}
+
+export default EditCustomProductForm;
