747e71aa178c6c7cabb1f5d8e59caae12b5ba33e
HaGuesto <hans@boxtribute.org>
2025-06-05 14:11:40 +0200

    add custom cache function

--- front/src/queries/cache.ts
+++ front/src/queries/cache.ts
@@ -1,12 +1,50 @@
-import { InMemoryCache, makeVar } from "@apollo/client";
+import { InMemoryCache, makeVar, Reference } from "@apollo/client";
 import { IScannedBoxesData } from "types/graphql-local-only";
 import { ITableConfig } from "hooks/hooks";
 import { GET_SCANNED_BOXES } from "./local-only";
 
 export const cache = new InMemoryCache({
   typePolicies: {
+    Query: {
+      fields: {
+        boxes: {
+          // Only use baseId for cache key generation for this field.
+          // filterInput and paginationInput will be ignored in cache key generation.
+          keyArgs: ["baseId"],
+          merge(existing, incoming, { readField }) {
+            const existingElements = existing?.elements ?? [];
+            const incomingElements = incoming.elements ?? [];
+            // Use a Map to store elements by labelIdentifier, ensuring uniqueness and easy updates.
+            // Assumption: all boxes have a labelIdentifier field!
+            // Incoming elements will overwrite existing ones with the same labelIdentifier.
+            const elementsMap = new Map<string, Reference | Record<string, any>>();
+            existingElements.forEach((element: Reference | Record<string, any>) => {
+              const labelIdentifier = readField<string>("labelIdentifier", element);
+              if (labelIdentifier != null) {
+                elementsMap.set(labelIdentifier, element);
+              }
+            });
+            incomingElements.forEach((element: Reference | Record<string, any>) => {
+              const labelIdentifier = readField<string>("labelIdentifier", element);
+              if (labelIdentifier != null) {
+                elementsMap.set(labelIdentifier, element); // Add or update element
+              }
+            });
+            const mergedElements = Array.from(elementsMap.values());
+            return {
+              ...incoming, // Use totalCount, pageInfo, etc., from the latest fetch
+              elements: mergedElements,
+            };
+          },
+        },
+      },
+    },
     Box: {
+      // Boxes should be normalized by labelIdentifier
       keyFields: ["labelIdentifier"],
+      // If in two separate queries the same Box with the same labelIdentifier is returned,
+      // the incoming Box is overwriting the fields overlapping with the existing Box and
+      // the rest of the incoming Box is appended to the existing Box.
       merge: true,
     },
     QrCode: {
