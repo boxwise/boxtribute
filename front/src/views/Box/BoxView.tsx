ae014ae84b181975e46e156db724fbc7e455e466
Roanna K <6267028+aerinsol@users.noreply.github.com>
2025-09-10 02:57:11 +0800

    Update BoxView.tsx
    Text change to "Details displayed show historical information of the box prior to deletion. New actions cannot be performed on the box."
    
    Signed-off-by: Roanna K <6267028+aerinsol@users.noreply.github.com>

--- front/src/views/Box/BoxView.tsx
+++ front/src/views/Box/BoxView.tsx
@@ -622,8 +622,7 @@ function BTBox() {
               })}
             </AlertTitle>
             <AlertDescription>
-              Details displayed show the historical information of the box prior to its deletion,
-              however, new actions cannot be performed on the box.
+              Details displayed show historical information of the box prior to deletion. New actions cannot be performed on the box.
             </AlertDescription>
           </Box>
         </Alert>
