dcbece437bbe6451515c324ebbacf6778d04344d
Philipp Metzner <10617122+pylipp@users.noreply.github.com>
2025-10-15 18:14:37 +0200

    Fix placeholder type in QrReader component
    Signed-off-by: Philipp Metzner <10617122+pylipp@users.noreply.github.com>

--- front/src/components/QrReader/components/QrReader.tsx
+++ front/src/components/QrReader/components/QrReader.tsx
@@ -95,7 +95,7 @@ function QrReader({
                   onChange={(e) => onBoxLabelInputChange(e.currentTarget.value)}
                   isDisabled={findBoxByLabelIsLoading}
                   value={boxLabelInputValue}
-                  placeholder={12345678}
+                  placeholder={"12345678"}
                   borderRadius={0}
                 />
                 <InputRightElement>
