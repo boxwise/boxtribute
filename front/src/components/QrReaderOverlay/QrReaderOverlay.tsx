cde3fd317ba58c61acae583c374061b397a0ba7d
Maurovic Cachia <maurovic.cachia@gmail.com>
2025-11-16 11:45:50 +0100

    QR Overlay Updates

--- front/src/components/QrReaderOverlay/QrReaderOverlay.tsx
+++ front/src/components/QrReaderOverlay/QrReaderOverlay.tsx
@@ -2,6 +2,7 @@ import {
   Button,
   Modal,
   ModalBody,
+  ModalCloseButton,
   ModalContent,
   ModalFooter,
   ModalHeader,
@@ -16,16 +17,19 @@ export interface IQrReaderOverlayProps {
 
 function QrReaderOverlay({ isOpen, onClose }: IQrReaderOverlayProps) {
   return (
-    <Modal isOpen={isOpen} closeOnOverlayClick closeOnEsc onClose={onClose}>
+    <Modal isOpen={isOpen} closeOnOverlayClick={false} closeOnEsc onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>QR Scanner</ModalHeader>
+        <div className="flex flex-row justify-end">
+          <ModalHeader>Boxtribute QR Scanner</ModalHeader>
+          <ModalCloseButton />
+        </div>
         <ModalBody>
           <QrReaderContainer onSuccess={onClose} />
         </ModalBody>
         <ModalFooter>
           <Button mr={3} onClick={onClose}>
-            Cancel
+            Close QR Scanner
           </Button>
         </ModalFooter>
       </ModalContent>
