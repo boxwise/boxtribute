3500a2994739bdf54f4133c935c82e1ca9df0893
Philipp Metzner <pmetzner@freenet.de>
2025-12-22 13:45:33 +0100

    Renaming for consistency

--- back/boxtribute_server/business_logic/metrics/queries.py
+++ back/boxtribute_server/business_logic/metrics/queries.py
@@ -4,9 +4,9 @@ from flask import g
 from ...authz import authorize
 from .crud import (
     get_time_span,
+    number_of_beneficiaries_reached_between,
     number_of_beneficiaries_registered_between,
     number_of_boxes_created_between,
-    reached_beneficiaries_numbers,
 )
 
 query = QueryType()
@@ -46,6 +46,6 @@ def resolve_newly_created_box_numbers(*_, start=None, end=None, duration=None):
 @public_query.field("reachedBeneficiariesNumbers")
 def resolve_reached_beneficiaries_numbers(*_, start=None, end=None, duration=None):
     time_span = get_time_span(start_date=start, end_date=end, duration_days=duration)
-    result = reached_beneficiaries_numbers(*time_span)
+    result = number_of_beneficiaries_reached_between(*time_span)
     # Sum up the numbers grouped by organisation and base into a single number
     return sum([element.number for element in result])
