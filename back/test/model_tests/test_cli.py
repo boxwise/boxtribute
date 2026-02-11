30b321742781f5b1abccf91334057d40c545096b
Philipp Metzner <pmetzner@freenet.de>
2026-02-11 08:50:18 +0100

    Remove products CLI
    - never used (use GraphQL API instead)
    - incorrect (no history entries created)

--- back/test/model_tests/test_cli.py
+++ back/test/model_tests/test_cli.py
@@ -1,143 +1,20 @@
-import csv
-import pathlib
-import tempfile
 from datetime import date
 from unittest.mock import MagicMock, call
 
 import peewee
 import pytest
 from auth0 import Auth0Error
-from boxtribute_server.cli.main import _create_db_interface, _parse_options
-from boxtribute_server.cli.products import (
-    PRODUCT_COLUMN_NAMES,
-    clone_products,
-    import_products,
-)
+from boxtribute_server.cli.main import _create_db_interface
 from boxtribute_server.cli.remove_base_access import remove_base_access
 from boxtribute_server.cli.service import Auth0Service, _user_data_without_base_id
 from boxtribute_server.db import db
 from boxtribute_server.exceptions import ServiceError
 from boxtribute_server.models.definitions.base import Base
 from boxtribute_server.models.definitions.organisation import Organisation
-from boxtribute_server.models.definitions.product import Product
 from boxtribute_server.models.definitions.user import User
 
 
-@pytest.fixture
-def valid_data():
-    return [
-        {
-            "name": "coats",
-            "category": 6,
-            "gender": 1,
-            "size_range": 1,
-            "base": 1,
-            "price": 20,
-            "in_shop": 0,
-            "comment": "",
-        },
-        {
-            "name": "umbrellas",
-            "category": 13,
-            "gender": 1,
-            "size_range": 1,
-            "base": 2,
-            "price": 10,
-            "in_shop": 0,
-            "comment": "yellow color",
-        },
-    ]
-
-
-def write_to_csv(*, filepath, data, fieldnames):
-    with open(filepath, mode="w", newline="") as data_file:
-        writer = csv.DictWriter(data_file, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(data)
-
-
-# Note that yielding the filepath prevents from exiting the tempfile context manager
-# which would otherwise result in automatic deletion of the temporary directory before
-# it is even used by the test
-@pytest.fixture
-def valid_data_filepath(valid_data):
-    with tempfile.TemporaryDirectory() as tmpdir:
-        filepath = pathlib.Path(tmpdir) / "valid_data.csv"
-        write_to_csv(
-            filepath=filepath, data=valid_data, fieldnames=PRODUCT_COLUMN_NAMES
-        )
-        yield filepath
-
-
-@pytest.fixture
-def empty_filepath():
-    with tempfile.NamedTemporaryFile(mode="w", newline="", suffix=".csv") as tmpfile:
-        yield tmpfile.name
-
-
-@pytest.fixture
-def only_header_filepath():
-    with tempfile.TemporaryDirectory() as tmpdir:
-        filepath = pathlib.Path(tmpdir) / "only_header.csv"
-        write_to_csv(filepath=filepath, data=[], fieldnames=PRODUCT_COLUMN_NAMES)
-        yield filepath
-
-
-@pytest.fixture
-def invalid_data_filepath():
-    with tempfile.TemporaryDirectory() as tmpdir:
-        filepath = pathlib.Path(tmpdir) / "invalid_data.csv"
-        write_to_csv(filepath=filepath, data=[{"invalid": 0}], fieldnames=["invalid"])
-        yield filepath
-
-
-@pytest.fixture
-def invalid_typed_data_filepath():
-    with tempfile.TemporaryDirectory() as tmpdir:
-        filepath = pathlib.Path(tmpdir) / "invalid_data.csv"
-        write_to_csv(
-            filepath=filepath,
-            data=[
-                {
-                    "name": "coats",
-                    "category": "Clothing",  # should be valid int
-                    "gender": 1,
-                    "size_range": 1,
-                    "base": 1,
-                    "price": 20,
-                    "in_shop": 0,
-                    "comment": "",
-                }
-            ],
-            fieldnames=PRODUCT_COLUMN_NAMES,
-        )
-        yield filepath
-
-
 def test_parse_options():
-    assert _parse_options("import-products -f data.csv".split()) == {
-        "command": "import-products",
-        "data_filepath": "data.csv",
-        "database": None,
-        "password": None,
-        "user": None,
-        "host": "127.0.0.1",
-        "port": 3386,
-        "verbose": False,
-    }
-
-    assert _parse_options("clone-products -s 1 -t 2".split()) == {
-        "command": "clone-products",
-        "source_base_id": 1,
-        "target_base_id": 2,
-        "database": None,
-        "password": None,
-        "user": None,
-        "host": "127.0.0.1",
-        "port": 3386,
-        "verbose": False,
-    }
-
     assert isinstance(
         _create_db_interface(
             password="dropapp_root",
@@ -148,58 +25,6 @@ def test_parse_options():
         ),
         peewee.MySQLDatabase,
     )
-
-
-def test_import_products(
-    valid_data_filepath,
-    valid_data,
-    empty_filepath,
-    only_header_filepath,
-    invalid_data_filepath,
-    invalid_typed_data_filepath,
-):
-    import_products(data_filepath=valid_data_filepath)
-    products = list(Product.select().dicts())
-
-    # Verify that result is superset of original test data
-    assert products[-2].items() >= valid_data[0].items()
-    assert products[-1].items() >= valid_data[1].items()
-
-    with pytest.raises(RuntimeError):
-        import_products(data_filepath=empty_filepath)
-
-    with pytest.raises(RuntimeError):
-        import_products(data_filepath=only_header_filepath)
-
-    with pytest.raises(ValueError):
-        import_products(data_filepath=invalid_data_filepath)
-
-    with pytest.raises(ValueError) as exc_info:
-        import_products(data_filepath=invalid_typed_data_filepath)
-    assert exc_info.value.args[0] == "Invalid fields:\nRow   1: category"
-
-
-def test_clone_products(default_product):
-    target_base_id = 2
-    clone_products(source_base_id=1, target_base_id=target_base_id)
-
-    # Verify that source and target product are identical apart from ID, base, and price
-    products = list(Product.select().dicts())
-    cloned_products = products[-4:-3]
-    original_products = products[:1]
-    for cloned_product, original_product in zip(cloned_products, original_products):
-        cloned_product.pop("id")
-        cloned_product.pop("created_by")
-        for field in ["id", "base", "price", "created_by"]:
-            original_product.pop(field)
-        assert cloned_product.pop("base") == target_base_id
-        assert cloned_product.pop("price") == 0
-        assert cloned_product == original_product
-
-    with pytest.raises(ValueError):
-        clone_products(source_base_id=0, target_base_id=1)
-    with pytest.raises(ValueError):
-        clone_products(source_base_id=1, target_base_id=0)
 
 
 @pytest.fixture
