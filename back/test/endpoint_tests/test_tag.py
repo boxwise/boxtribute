d758cfe3b984a94e8f53b5b58d5b8ef1fb0421d0
Philipp Metzner <pmetzner@freenet.de>
2026-02-19 18:33:49 +0100

    Add Tag.lastUsedOn field

--- back/test/endpoint_tests/test_tag.py
+++ back/test/endpoint_tests/test_tag.py
@@ -1,5 +1,5 @@
 import time
-from datetime import date
+from datetime import date, datetime
 
 import pytest
 from boxtribute_server.enums import TagType
@@ -27,6 +27,7 @@ def test_tag_query(client, tags):
             createdBy {{ id }}
             lastModifiedOn
             lastModifiedBy {{ id }}
+            lastUsedOn
         }} }}"""
     queried_tag = assert_successful_request(client, query)
     assert queried_tag == {
@@ -37,6 +38,7 @@ def test_tag_query(client, tags):
         "createdBy": {"id": str(tag["created_by"])},
         "lastModifiedOn": None,
         "lastModifiedBy": None,
+        "lastUsedOn": datetime(2023, 1, 1).isoformat() + "+00:00",
     }
 
 
@@ -176,6 +178,7 @@ def test_tags_mutations(client, tags, base1_active_tags, another_beneficiary, lo
                     createdBy {{ id }}
                     lastModifiedOn
                     lastModifiedBy {{ id }}
+                    lastUsedOn
                     base {{ id }}
                     taggedResources {{
                         ...on Beneficiary {{ id }}
@@ -198,6 +201,7 @@ def test_tags_mutations(client, tags, base1_active_tags, another_beneficiary, lo
         "createdBy": {"id": "8"},
         "lastModifiedOn": None,
         "lastModifiedBy": None,
+        "lastUsedOn": None,
     }
 
     # Test case 4.2.3
@@ -261,8 +265,10 @@ def test_tags_mutations(client, tags, base1_active_tags, another_beneficiary, lo
                     ...on Beneficiary {{ id }}
                     ...on Box {{ id }}
                 }}
+                lastUsedOn
     }} }}"""
     tag = assert_successful_request(client, query)
+    assert tag.pop("lastUsedOn").startswith(today)
     assert tag == {"taggedResources": [{"id": i} for i in [beneficiary_id, box_id]]}
 
     # Test case 4.2.27
@@ -290,6 +296,7 @@ def test_tags_mutations(client, tags, base1_active_tags, another_beneficiary, lo
     assert beneficiary == {"tags": []}
 
     tag = assert_successful_request(client, query)
+    assert tag.pop("lastUsedOn").startswith(today)
     assert tag == {"taggedResources": []}
 
     history_entries = list(
