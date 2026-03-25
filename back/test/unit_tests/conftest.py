a215f5940abc38c6e20aeddd13c0a8427e55fe99
Philipp Metzner <pmetzner@freenet.de>
2026-03-25 16:24:46 +0100

    Revert "fix"
    This reverts commit 21740bbdb9bfcd4f7c98def94699a283b93cd1da.

--- back/test/unit_tests/conftest.py
+++ back/test/unit_tests/conftest.py
@@ -1,6 +1,5 @@
 import pytest
 from boxtribute_server.app import create_app
-from boxtribute_server.models import MODELS
 from boxtribute_server.routes import app_bp
 
 
@@ -8,8 +7,5 @@ from boxtribute_server.routes import app_bp
 def no_db_client():
     app = create_app()
     app.register_blueprint(app_bp)
-    # Reset possible interference from other (endpoint/integration) tests
-    for model in MODELS:
-        model.bind(None)
     with app.app_context():
         yield app.test_client()
