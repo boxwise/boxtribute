3bcabf47f1909b9badec38afdc214e1c2743362d
Philipp Metzner <pmetzner@freenet.de>
2026-05-12 12:00:29 +0200

    Use MobileBreadcrumbButton

--- front/src/views/Products/StandardProductsView.tsx
+++ front/src/views/Products/StandardProductsView.tsx
@@ -1,26 +1,13 @@
-import { Button, Heading } from "@chakra-ui/react";
-import { useNavigate } from "react-router-dom";
-import { useAtomValue } from "jotai";
-import { selectedBaseIdAtom } from "stores/globalPreferenceStore";
-import { ChevronLeftIcon } from "@chakra-ui/icons";
+import { Heading } from "@chakra-ui/react";
 import StandardProductsContainer from "./components/StandardProductsContainer";
+import { MobileBreadcrumbButton } from "components/BreadcrumbNavigation";
 import { ErrorBoundary } from "@sentry/react";
 import { AlertWithoutAction } from "components/Alerts";
 
 function StandardProductsView() {
-  const navigate = useNavigate();
-  const baseId = useAtomValue(selectedBaseIdAtom);
-
   return (
     <>
-      <Button
-        leftIcon={<ChevronLeftIcon />}
-        variant="link"
-        mb={4}
-        onClick={() => navigate(`/bases/${baseId}/products`)}
-      >
-        Back to Manage Products
-      </Button>
+      <MobileBreadcrumbButton label="Back to Manage Products" linkPath={".."} />
       <Heading fontWeight="bold" mb={4} as="h2">
         ASSORT Standard Products
       </Heading>
