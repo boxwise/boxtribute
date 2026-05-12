667d920d72f29111ed0ffc16e34ac31fe9f4c56c
Philipp Metzner <pmetzner@freenet.de>
2026-05-12 12:33:26 +0200

    add-text

--- front/src/views/Products/StandardProductsView.tsx
+++ front/src/views/Products/StandardProductsView.tsx
@@ -1,4 +1,4 @@
-import { Heading } from "@chakra-ui/react";
+import { Heading, Text } from "@chakra-ui/react";
 import StandardProductsContainer from "./components/StandardProductsContainer";
 import { MobileBreadcrumbButton } from "components/BreadcrumbNavigation";
 import { ErrorBoundary } from "@sentry/react";
@@ -9,8 +9,13 @@ function StandardProductsView() {
     <>
       <MobileBreadcrumbButton label="Back to Manage Products" linkPath={".."} />
       <Heading fontWeight="bold" mb={4} as="h2">
-        ASSORT Standard Products
+        Explore ASSORT
       </Heading>
+      <Text>
+        ASSORT is a standardized inventory classification system developed in partnership with IHA,
+        HERMINE, and DistributeAid in full compliance with SPHERE and CHS standards for easy and
+        effective use even by small volunteer teams.
+      </Text>
       <ErrorBoundary
         fallback={
           <AlertWithoutAction alertText="Could not fetch standard products data! Please try reloading the page." />
