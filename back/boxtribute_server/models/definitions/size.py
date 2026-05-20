55726652360a645120462c4c3b3e629a765d2207
Philipp Metzner <pmetzner@freenet.de>
2026-05-20 10:13:07 +0200

    update-size-model

--- back/boxtribute_server/models/definitions/size.py
+++ back/boxtribute_server/models/definitions/size.py
@@ -1,30 +1,10 @@
-from peewee import CharField, DateTimeField
+from peewee import CharField
 
-from ..fields import UIntForeignKeyField
 from . import Model
-from .user import User
 
 
 class Size(Model):
-    created = DateTimeField(null=True)
-    created_by = UIntForeignKeyField(
-        column_name="created_by",
-        field="id",
-        model=User,
-        null=True,
-        on_delete="SET NULL",
-        on_update="CASCADE",
-    )
     label = CharField()
-    modified = DateTimeField(null=True)
-    modified_by = UIntForeignKeyField(
-        column_name="modified_by",
-        field="id",
-        model=User,
-        null=True,
-        on_delete="SET NULL",
-        on_update="CASCADE",
-    )
 
     class Meta:
         table_name = "sizes"
