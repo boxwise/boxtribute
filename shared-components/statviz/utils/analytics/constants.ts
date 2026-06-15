dfd81d664c564c97b07747859770e0509275c5f6
Philipp Metzner <pmetzner@freenet.de>
2026-06-15 16:09:43 +0200

    Remove unneeded components

--- shared-components/statviz/utils/analytics/constants.ts
+++ shared-components/statviz/utils/analytics/constants.ts
@@ -1,6 +1,5 @@
 const GRAPH_TYPES = {
   CREATED_BOXES: "CreatedBoxes",
-  TOP_CREATED_PRODUCTS: "TopCreatedProducts",
   OUTGOING_BOXES: "OutGoingBoxes",
   DEMOGRAPHICS_BENEFICIARIES_REGISTERED: "BeneficiariesRegistered",
   STOCK_OVERVIEW: "StockOverview",
