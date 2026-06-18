6bc3e92454fdd81cefd140ea366aaa8b0f0962e1
Philipp Metzner <10617122+pylipp@users.noreply.github.com>
2026-06-18 18:17:11 +0200

    GraphQL API for incoming shipment stats (#2782)

--- shared-components/statviz/components/visualizations/movedBoxes/BoxFlowSankey.tsx
+++ shared-components/statviz/components/visualizations/movedBoxes/BoxFlowSankey.tsx
@@ -89,7 +89,7 @@ export default function BoxFlowSankey({ width, height, data, boxesOrItems }: IBo
             isNegative: target.isNegative,
           };
         }
-        if (target.type === "Shipment") {
+        if (target.type === "OutgoingShipment") {
           return {
             source: outgoingNode.id,
             target: shipmentNode.id,
@@ -100,50 +100,54 @@ export default function BoxFlowSankey({ width, height, data, boxesOrItems }: IBo
         return undefined;
       })
       .filter((e) => e !== undefined),
-    ...movedBoxes.map((movedBox) => {
-      if (movedBox.type === "OutgoingLocation") {
+    ...movedBoxes
+      .filter((e) => e.type !== "IncomingShipment")
+      .map((movedBox) => {
+        if (movedBox.type === "OutgoingLocation") {
+          return {
+            source: selfReportedNode.id,
+            target: movedBox.targetId,
+            value: movedBox.count,
+            isNegative: movedBox.isNegative,
+          };
+        }
+        if (movedBox.type === "OutgoingShipment") {
+          return {
+            source: shipmentNode.id,
+            target: movedBox.targetId,
+            value: movedBox.count,
+            isNegative: movedBox.isNegative,
+          };
+        }
         return {
-          source: selfReportedNode.id,
+          source: outgoingNode.id,
           target: movedBox.targetId,
           value: movedBox.count,
           isNegative: movedBox.isNegative,
         };
-      }
-      if (movedBox.type === "Shipment") {
-        return {
-          source: shipmentNode.id,
-          target: movedBox.targetId,
-          value: movedBox.count,
-          isNegative: movedBox.isNegative,
-        };
-      }
-      return {
-        source: outgoingNode.id,
-        target: movedBox.targetId,
-        value: movedBox.count,
-        isNegative: movedBox.isNegative,
-      };
-    }),
+      }),
   ];
 
   const nodes = [
     outgoingNode,
-    ...movedBoxes.map((movedBox) => {
-      const getName = () => {
-        if (movedBox.organisationName) {
-          return `${movedBox.name} | ${movedBox.organisationName} `;
-        }
-        return movedBox.name;
-      };
+    ...movedBoxes
+      .filter((e) => e.type !== "IncomingShipment")
+      .map((movedBox) => {
+        const getName = () => {
+          if (movedBox.organisationName) {
+            return `${movedBox.name} | ${movedBox.organisationName} `;
+          }
+          return movedBox.name;
+        };
 
-      return {
-        id: movedBox.targetId,
-        name: movedBox.isNegative ? `${getName()} removed` : getName(),
-        nodeColor: movedBox.isNegative
-          ? "red"
-          : sample(["#9467bd", "#e377c2", "#7f7f7f", "#bcbd22", "#51bd22", "#2287bd"]),
-      };
-    }),
+        return {
+          id: movedBox.targetId,
+          name: movedBox.isNegative ? `${getName()} removed` : getName(),
+          nodeColor: movedBox.isNegative
+            ? "red"
+            : sample(["#9467bd", "#e377c2", "#7f7f7f", "#bcbd22", "#51bd22", "#2287bd"]),
+        };
+      }),
   ];
 
   const nodeIsTargetedByLink = (node) => links.findIndex((link) => link?.target === node.id) !== -1;
