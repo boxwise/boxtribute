f3984680872437298dbe63dd4b4515475c28fa22
Philipp Metzner <pmetzner@freenet.de>
2026-08-14 15:28:44 +0200

    Avoid permission error when logging in as label-creation user
    https://trello.com/c/biEWwI6E

--- front/src/views/NotFoundView/NotFoundView.tsx
+++ front/src/views/NotFoundView/NotFoundView.tsx
@@ -1,18 +1,21 @@
 import { DESKTOP_OR_TABLET_SCREEN_MEDIA_QUERY } from "components/HeaderMenu/consts";
 import { useAtomValue } from "jotai";
 import { Navigate, useLocation } from "react-router-dom";
+import { useHasPermission } from "hooks/hooks";
 import { selectedBaseIdAtom } from "stores/globalPreferenceStore";
 
 function NotFoundView() {
   const selectedBaseId = useAtomValue(selectedBaseIdAtom);
+  const hasViewInventoryPermission = useHasPermission("view_inventory");
   const location = useLocation();
   const isLargeScreen = window.matchMedia(DESKTOP_OR_TABLET_SCREEN_MEDIA_QUERY);
   // somehow useMediaQuery always returns false, most likely because the width is measured somewhere during the rendering.
   // TODO: create a global state using useMediaQuery higher up in the DOM tree and use it here.
   // const [isLargeScreen] = useMediaQuery(DESKTOP_OR_TABLET_SCREEN_MEDIA_QUERY);
 
-  // If the requested route was not found redirect to statviz
-  if (isLargeScreen.matches) {
+  // On desktop, if the requested route was not found and the user has sufficient permissions,
+  // redirect to statviz
+  if (isLargeScreen.matches && hasViewInventoryPermission) {
     return (
       <Navigate
         to={`/bases/${selectedBaseId}/statviz`}
@@ -21,6 +24,7 @@ function NotFoundView() {
       />
     );
   }
+  // On mobile, and/or no view_inventory permission
   return (
     <Navigate
       to={`/bases/${selectedBaseId}/qrreader`}
