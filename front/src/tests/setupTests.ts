f03533aa790a85527bdc5708c397c70c4d9e3b38
MomoRazor <Maurovic.Cachia@gmail.com>
2026-09-03 15:26:50 +0200

    Hotfix for localstorage mocking (#2890)

--- front/src/tests/setupTests.ts
+++ front/src/tests/setupTests.ts
@@ -70,6 +70,33 @@ Object.defineProperty(navigator, "userAgent", {
   // configurable: true
 });
 
+// needed to avoid "running clear() on undefined" when executing `pnpm test` outside docker or CI
+const mockLocalStorage = (() => {
+  let store = {} as Storage;
+
+  return {
+    getItem(key: string) {
+      return store[key];
+    },
+
+    setItem(key: string, value: string) {
+      store[key] = value;
+    },
+
+    removeItem(key: string) {
+      delete store[key];
+    },
+
+    clear() {
+      store = {} as Storage;
+    },
+  };
+})();
+
+Object.defineProperty(window, "localStorage", {
+  value: mockLocalStorage,
+});
+
 beforeEach(() => {
   localStorage.clear();
   // Reset the cache before each test
