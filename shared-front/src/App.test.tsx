b4f1aa9585f6cbb2276860579e4087f27073c038
Philipp Metzner <pmetzner@freenet.de>
2026-09-10 16:08:05 +0200

    Fixup

--- shared-front/src/App.test.tsx
+++ shared-front/src/App.test.tsx
@@ -86,9 +86,7 @@ const stockOverviewData = {
           { id: 1, name: "Size A" },
           { id: 2, name: "Size B" },
         ],
-        tag: [
-          { id: 1, name: "Tag A", color: "#abcdef" },
-        ],
+        tag: [{ id: 1, name: "Tag A", color: "#abcdef" }],
         location: [
           { id: 1, name: "Location A" },
           { id: 2, name: "Location B" },
